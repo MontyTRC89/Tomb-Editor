7ef5b7a2015a3f906f7497051a1d449aed998616
MontyTRC89 <montyhammet@hotmail.it>
2017-09-05 23:03:42 +0200

    Added conversion of SFX and SAM to Wad2 format

---
+++ TombLib/Wad/WadSound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TombLib.Utils;
+using System.IO;
+
+namespace TombLib.Wad
+{
+    public class WadSound : IEquatable<WadSound>
+    {
+        public byte[] WaveData { get { return _wave; } }
+        public Hash Hash { get { return _hash; } }
+
+        private byte[] _wave;
+        private Hash _hash;
+
+        public WadSound(byte[] data)
+        {
+            _wave = data;
+            UpdateHash();
+        }
+
+        public Hash UpdateHash()
+        {
+            _hash = Hash.FromByteArray(_wave);
+            return _hash;
+        }
+
+        public bool Equals(WadSound other)
+        {
+            return (Hash == other.Hash);
+        }
+    }
+}
