d41bcaaff42767fbf28d99cfccf41dc2f6f8d48d
MontyTRC89 <montyhammet@hotmail.it>
2017-09-16 16:22:28 +0200

    Added initial code for NG triggers

---
+++ TombLib/NG/NGExtraParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TombLib.NG
+{
+    public class NGExtraParameter
+    {
+        public int Id { get; internal set; }
+        public string Description { get; internal set; }
+
+        public NGExtraParameter(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+    }
+}
---
+++ TombLib/NG/NGTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TombLib.NG
+{
+    public enum NGParameterType
+    {
+        None,
+        Timer,
+        Object,
+        Extra,
+        Button
+    }
+
+    public class NGTrigger
+    {
+        public int Id { get; internal set; }
+        public string Description { get; internal set; }
+        public Dictionary<int, NGTimerParameter> TimerParameters { get; private set; }
+        public Dictionary<int, NGExtraParameter> ExtraParameters { get; private set; }
+        
+        public NGTrigger(int id, string description)
+        {
+            Id = id;
+            Description = description;
+            TimerParameters = new Dictionary<int, NGTimerParameter>();
+            ExtraParameters = new Dictionary<int, NGExtraParameter>();
+        }
+    }
+}
