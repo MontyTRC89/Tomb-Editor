b677744d16f22525be5f1ae1cae35e393487ef29
Lwmte <initiator@nm.ru>
2017-09-21 13:55:02 +0300

    Fix some bugs in object browser

--- TombEditor/ToolWindows/ObjectBrowser.Designer.cs
+++ TombEditor/ToolWindows/ObjectBrowser.Designer.cs
@@ -105,7 +105,7 @@
             this.panelItem.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelItem.Location = new System.Drawing.Point(2, 2);
             this.panelItem.Name = "panelItem";
-            this.panelItem.Size = new System.Drawing.Size(826, 568);
+            this.panelItem.Size = new System.Drawing.Size(229, 217);
             this.panelItem.TabIndex = 62;
             // 
             // colorDialog
@@ -121,7 +121,8 @@
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 25);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(897, 27);
+            this.panelHeader.Padding = new System.Windows.Forms.Padding(1, 2, 0, 0);
+            this.panelHeader.Size = new System.Drawing.Size(300, 27);
             this.panelHeader.TabIndex = 72;
             // 
             // comboItems
@@ -134,9 +135,9 @@
             this.comboItems.ForeColor = System.Drawing.Color.White;
             this.comboItems.FormattingEnabled = true;
             this.comboItems.ItemHeight = 18;
-            this.comboItems.Location = new System.Drawing.Point(31, 0);
+            this.comboItems.Location = new System.Drawing.Point(31, 2);
             this.comboItems.Name = "comboItems";
-            this.comboItems.Size = new System.Drawing.Size(805, 24);
+            this.comboItems.Size = new System.Drawing.Size(208, 24);
             this.comboItems.TabIndex = 66;
             this.comboItems.SelectedIndexChanged += new System.EventHandler(this.comboItems_SelectedIndexChanged);
             // 
@@ -145,9 +146,9 @@
             this.panelHeaderRight.Controls.Add(this.butItemsNext);
             this.panelHeaderRight.Controls.Add(this.butAddItem);
             this.panelHeaderRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelHeaderRight.Location = new System.Drawing.Point(836, 0);
+            this.panelHeaderRight.Location = new System.Drawing.Point(239, 2);
             this.panelHeaderRight.Name = "panelHeaderRight";
-            this.panelHeaderRight.Size = new System.Drawing.Size(61, 27);
+            this.panelHeaderRight.Size = new System.Drawing.Size(61, 25);
             this.panelHeaderRight.TabIndex = 76;
             // 
             // butItemsNext
@@ -158,6 +159,7 @@
             this.butItemsNext.Padding = new System.Windows.Forms.Padding(5);
             this.butItemsNext.Size = new System.Drawing.Size(24, 24);
             this.butItemsNext.TabIndex = 67;
+            this.butItemsNext.Click += new System.EventHandler(this.butItemsNext_Click);
             // 
             // butAddItem
             // 
@@ -167,14 +169,15 @@
             this.butAddItem.Padding = new System.Windows.Forms.Padding(5);
             this.butAddItem.Size = new System.Drawing.Size(24, 24);
             this.butAddItem.TabIndex = 66;
+            this.butAddItem.Click += new System.EventHandler(this.butAddItem_Click);
             // 
             // panelHeaderLeft
             // 
             this.panelHeaderLeft.Controls.Add(this.butItemsBack);
             this.panelHeaderLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelHeaderLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelHeaderLeft.Location = new System.Drawing.Point(1, 2);
             this.panelHeaderLeft.Name = "panelHeaderLeft";
-            this.panelHeaderLeft.Size = new System.Drawing.Size(31, 27);
+            this.panelHeaderLeft.Size = new System.Drawing.Size(30, 25);
             this.panelHeaderLeft.TabIndex = 75;
             // 
             // panelRight
@@ -182,9 +185,9 @@
             this.panelRight.Controls.Add(this.panelRightTop);
             this.panelRight.Controls.Add(this.panelRightBottom);
             this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelRight.Location = new System.Drawing.Point(830, 52);
+            this.panelRight.Location = new System.Drawing.Point(233, 52);
             this.panelRight.Name = "panelRight";
-            this.panelRight.Size = new System.Drawing.Size(67, 572);
+            this.panelRight.Size = new System.Drawing.Size(67, 221);
             this.panelRight.TabIndex = 73;
             // 
             // panelRightTop
@@ -202,7 +205,7 @@
             this.panelRightBottom.Controls.Add(this.butFindItem);
             this.panelRightBottom.Controls.Add(this.butResetSearch);
             this.panelRightBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelRightBottom.Location = new System.Drawing.Point(0, 516);
+            this.panelRightBottom.Location = new System.Drawing.Point(0, 165);
             this.panelRightBottom.Name = "panelRightBottom";
             this.panelRightBottom.Size = new System.Drawing.Size(67, 56);
             this.panelRightBottom.TabIndex = 1;
@@ -216,7 +219,7 @@
             this.panelViewer.Location = new System.Drawing.Point(0, 52);
             this.panelViewer.Name = "panelViewer";
             this.panelViewer.Padding = new System.Windows.Forms.Padding(2);
-            this.panelViewer.Size = new System.Drawing.Size(830, 572);
+            this.panelViewer.Size = new System.Drawing.Size(233, 221);
             this.panelViewer.TabIndex = 74;
             // 
             // lblLoadHelper
@@ -225,7 +228,7 @@
             this.lblLoadHelper.ForeColor = System.Drawing.Color.DarkGray;
             this.lblLoadHelper.Location = new System.Drawing.Point(2, 2);
             this.lblLoadHelper.Name = "lblLoadHelper";
-            this.lblLoadHelper.Size = new System.Drawing.Size(826, 568);
+            this.lblLoadHelper.Size = new System.Drawing.Size(229, 217);
             this.lblLoadHelper.TabIndex = 0;
             this.lblLoadHelper.Text = "Click here to load WAD";
             this.lblLoadHelper.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -244,7 +247,7 @@
             this.MinimumSize = new System.Drawing.Size(284, 264);
             this.Name = "ObjectBrowser";
             this.SerializationKey = "ObjectBrowser";
-            this.Size = new System.Drawing.Size(897, 624);
+            this.Size = new System.Drawing.Size(300, 273);
             this.panelHeader.ResumeLayout(false);
             this.panelHeaderRight.ResumeLayout(false);
             this.panelHeaderLeft.ResumeLayout(false);
