f8c671dab581f0fbbf14c08c05cc3b3cb4fdc154
MontyTRC89 <montyhammet@hotmail.it>
2017-09-25 23:32:57 +0200

    Added missing files

---
+++ WadTool/FormStaticMeshEditor.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WadTool
+{
+    partial class FormStaticMeshEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.darkStatusStrip1 = new DarkUI.Controls.DarkStatusStrip();
+            this.SuspendLayout();
+            // 
+            // darkStatusStrip1
+            // 
+            this.darkStatusStrip1.AutoSize = false;
+            this.darkStatusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkStatusStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkStatusStrip1.Location = new System.Drawing.Point(0, 580);
+            this.darkStatusStrip1.Name = "darkStatusStrip1";
+            this.darkStatusStrip1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 3);
+            this.darkStatusStrip1.Size = new System.Drawing.Size(768, 24);
+            this.darkStatusStrip1.SizingGrip = false;
+            this.darkStatusStrip1.TabIndex = 0;
+            this.darkStatusStrip1.Text = "darkStatusStrip1";
+            // 
+            // FormStaticMeshEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 604);
+            this.Controls.Add(this.darkStatusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormStaticMeshEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Static mesh editor";
+            this.Load += new System.EventHandler(this.FormStaticMeshEditor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkStatusStrip darkStatusStrip1;
+    }
+}
