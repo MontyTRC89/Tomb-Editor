0e432409d378606065c29facee2a0f3a19596561
MontyTRC89 <montyhammet@hotmail.it>
2017-09-25 23:32:57 +0200

    Added missing files

---
+++ WadTool/FormStaticMeshEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WadTool
+{
+    public partial class FormStaticMeshEditor : DarkUI.Forms.DarkForm
+    {
+        public FormStaticMeshEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void FormStaticMeshEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
