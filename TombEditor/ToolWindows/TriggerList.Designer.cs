9eccd40995de73470d1601f39de32fb944ebdab0
Lwmte <initiator@nm.ru>
2017-09-28 21:50:53 +0300

    Reduce room options tool size, DarkUI bottom tab size fix.

--- TombEditor/ToolWindows/TriggerList.Designer.cs
+++ TombEditor/ToolWindows/TriggerList.Designer.cs
@@ -34,7 +34,7 @@
             this.panelTriggerTools.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panelTriggerTools.Location = new System.Drawing.Point(0, 143);
             this.panelTriggerTools.Name = "panelTriggerTools";
-            this.panelTriggerTools.Size = new System.Drawing.Size(279, 31);
+            this.panelTriggerTools.Size = new System.Drawing.Size(284, 31);
             this.panelTriggerTools.TabIndex = 57;
             // 
             // butAddTrigger
@@ -74,22 +74,22 @@
             this.panelTriggerList.Location = new System.Drawing.Point(0, 25);
             this.panelTriggerList.Name = "panelTriggerList";
             this.panelTriggerList.Padding = new System.Windows.Forms.Padding(2);
-            this.panelTriggerList.Size = new System.Drawing.Size(279, 118);
+            this.panelTriggerList.Size = new System.Drawing.Size(284, 118);
             this.panelTriggerList.TabIndex = 58;
             // 
             // lstTriggers
             // 
             this.lstTriggers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.lstTriggers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lstTriggers.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstTriggers.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.lstTriggers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lstTriggers.ForeColor = System.Drawing.Color.White;
             this.lstTriggers.FormattingEnabled = true;
             this.lstTriggers.IntegralHeight = false;
             this.lstTriggers.ItemHeight = 18;
             this.lstTriggers.Location = new System.Drawing.Point(2, 2);
             this.lstTriggers.Name = "lstTriggers";
-            this.lstTriggers.Size = new System.Drawing.Size(275, 114);
+            this.lstTriggers.Size = new System.Drawing.Size(280, 114);
             this.lstTriggers.TabIndex = 56;
             this.lstTriggers.SelectedIndexChanged += new System.EventHandler(this.lstTriggers_SelectedIndexChanged);
             // 
@@ -100,13 +100,12 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Controls.Add(this.panelTriggerList);
             this.Controls.Add(this.panelTriggerTools);
-            this.DefaultDockArea = DarkUI.Docking.DarkDockArea.Left;
             this.DockText = "Triggers";
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MinimumSize = new System.Drawing.Size(100, 100);
             this.Name = "TriggerList";
             this.SerializationKey = "TriggerList";
-            this.Size = new System.Drawing.Size(279, 174);
+            this.Size = new System.Drawing.Size(284, 174);
             this.panelTriggerTools.ResumeLayout(false);
             this.panelTriggerList.ResumeLayout(false);
             this.ResumeLayout(false);
