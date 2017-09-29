f1b8ea235198952065895efa5574236c654fb8a0
ActuallyaDeviloper <ActuallyaDeviloper@users.noreply.github.com>
2017-09-29 15:40:48 +0200

    Fix importedGeometryForm win forms settings

--- TombEditor/FormImportedGeometry.Designer.cs
+++ TombEditor/FormImportedGeometry.Designer.cs
@@ -48,16 +48,16 @@
             this.importedGeometryManager.Location = new System.Drawing.Point(23, 47);
             this.importedGeometryManager.Name = "importedGeometryManager";
             this.importedGeometryManager.SelectedImportedGeometry = null;
-            this.importedGeometryManager.Size = new System.Drawing.Size(686, 378);
+            this.importedGeometryManager.Size = new System.Drawing.Size(713, 354);
             this.importedGeometryManager.TabIndex = 0;
             // 
             // butCancel
             // 
             this.butCancel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.butCancel.Location = new System.Drawing.Point(359, 3);
+            this.butCancel.Location = new System.Drawing.Point(373, 3);
             this.butCancel.Name = "butCancel";
             this.butCancel.Padding = new System.Windows.Forms.Padding(5);
-            this.butCancel.Size = new System.Drawing.Size(351, 24);
+            this.butCancel.Size = new System.Drawing.Size(364, 24);
             this.butCancel.TabIndex = 1;
             this.butCancel.Text = "Cancel";
             this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
@@ -68,7 +68,7 @@
             this.butOk.Location = new System.Drawing.Point(3, 3);
             this.butOk.Name = "butOk";
             this.butOk.Padding = new System.Windows.Forms.Padding(5);
-            this.butOk.Size = new System.Drawing.Size(350, 24);
+            this.butOk.Size = new System.Drawing.Size(364, 24);
             this.butOk.TabIndex = 1;
             this.butOk.Text = "Ok";
             this.butOk.Click += new System.EventHandler(this.butOk_Click);
@@ -80,7 +80,7 @@
             this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
             this.darkLabel1.Location = new System.Drawing.Point(4, 9);
             this.darkLabel1.Name = "darkLabel1";
-            this.darkLabel1.Size = new System.Drawing.Size(705, 35);
+            this.darkLabel1.Size = new System.Drawing.Size(732, 35);
             this.darkLabel1.TabIndex = 2;
             this.darkLabel1.Text = "Select a model from the following list or add a new one by pressing \'Create from " +
     "file\'.\r\nPress \'Assign\' afterwards to assign the loaded file to this imported geo" +
@@ -90,7 +90,7 @@
             // 
             this.darkLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.darkLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-            this.darkLabel2.Location = new System.Drawing.Point(221, 434);
+            this.darkLabel2.Location = new System.Drawing.Point(221, 410);
             this.darkLabel2.Name = "darkLabel2";
             this.darkLabel2.Size = new System.Drawing.Size(144, 17);
             this.darkLabel2.TabIndex = 2;
@@ -99,7 +99,7 @@
             // butAssign
             // 
             this.butAssign.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.butAssign.Location = new System.Drawing.Point(23, 431);
+            this.butAssign.Location = new System.Drawing.Point(23, 407);
             this.butAssign.Name = "butAssign";
             this.butAssign.Padding = new System.Windows.Forms.Padding(5);
             this.butAssign.Size = new System.Drawing.Size(185, 20);
@@ -112,9 +112,9 @@
             this.importedGeometryLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.importedGeometryLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-            this.importedGeometryLabel.Location = new System.Drawing.Point(371, 434);
+            this.importedGeometryLabel.Location = new System.Drawing.Point(371, 410);
             this.importedGeometryLabel.Name = "importedGeometryLabel";
-            this.importedGeometryLabel.Size = new System.Drawing.Size(338, 17);
+            this.importedGeometryLabel.Size = new System.Drawing.Size(365, 17);
             this.importedGeometryLabel.TabIndex = 2;
             this.importedGeometryLabel.Text = "None ☹";
             // 
@@ -126,25 +126,29 @@
             this.tableLayoutPanel1.Controls.Add(this.butOk, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.butCancel, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 458);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 434);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(713, 30);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(740, 30);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
             // FormImportedGeometry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(713, 488);
+            this.ClientSize = new System.Drawing.Size(740, 464);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.importedGeometryLabel);
             this.Controls.Add(this.darkLabel2);
             this.Controls.Add(this.darkLabel1);
             this.Controls.Add(this.butAssign);
             this.Controls.Add(this.importedGeometryManager);
+            this.MinimizeBox = false;
             this.Name = "FormImportedGeometry";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Imported Geometry Settings";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
