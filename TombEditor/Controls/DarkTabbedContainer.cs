f357f0fcb63bfbaa237d02f5f50e91eff157855a
Anatoly <initiator@nm.ru>
2017-10-09 20:23:56 +0300

    Group level settings to occupy less screen space (#129)
    * Initial commit
    
    * Restore missing sound gridview buttons
    
    * Fix up control position
    
    * Remove empty autogenerated event
    
    * Remove one more empty event
    
    * Also re-enable sound gridview move controls
    
    * Override ctrl+tab combination, move tab hiding code into constructor
    
    * Make tabbed container's list linking self-contained
    
    * Hide LinkedListView property from designer

---
+++ TombEditor/Controls/DarkTabbedContainer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using DarkUI.Config;
+using DarkUI.Controls;
+
+namespace TombEditor.Controls
+{
+    class DarkTabbedContainer : TabControl
+    {
+        private DarkListView _linkedListView;
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public DarkListView LinkedListView
+        {
+            get { return _linkedListView; }
+            set
+            {
+                // Unlink previous list view, if any
+                if (value != null)
+                {
+                    value.SelectedIndicesChanged -= linkedListView_SelectionChanged;
+                    value.Items.Clear();
+                }
+
+                _linkedListView = value;
+
+                if (value != null)
+                {
+                    value.Items.Clear();
+
+                    // Populate options list and select first entry
+                    foreach (TabPage tab in TabPages)
+                        value.Items.Add(new DarkListItem(tab.Text));
+                    value.SelectItem(0);
+
+                    // Link list selection event to container
+                    value.SelectedIndicesChanged += linkedListView_SelectionChanged;
+                }
+            }
+        }
+
+        public DarkTabbedContainer()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                     ControlStyles.SupportsTransparentBackColor |
+                     ControlStyles.ResizeRedraw, true);
+
+            if (LicenseManager.UsageMode == LicenseUsageMode.Runtime)
+            {
+                SetStyle(ControlStyles.UserPaint, true);
+
+                Appearance = TabAppearance.FlatButtons;
+                ItemSize = new Size(0, 1);
+                SizeMode = TabSizeMode.Fixed;
+            }
+        }
+
+        private void linkedListView_SelectionChanged(object sender, EventArgs e)
+        {
+            SelectedIndex = _linkedListView.SelectedIndices.First();
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == (Keys.Tab | Keys.Control))
+                return true;
+
+            return base.ProcessDialogKey(keyData);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            using (var b = new SolidBrush(Colors.GreyBackground))
+                e.Graphics.FillRectangle(b, ClientRectangle);
+        }
+    }
+}
