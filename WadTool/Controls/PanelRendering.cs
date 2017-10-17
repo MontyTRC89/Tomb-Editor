907ea53151d479c38d16fdbfa742c5835f509761
MontyTRC89 <montyhammet@hotmail.it>
2017-10-17 20:05:54 +0200

    Fixed Wad2 I/O; Added configuration XML file to Wad Tool;

--- WadTool/Controls/PanelRendering.cs
+++ WadTool/Controls/PanelRendering.cs
@@ -189,7 +189,7 @@ namespace WadTool.Controls
         {
             base.OnMouseWheel(e);
 
-            Camera.Zoom(-e.Delta * 46000 /*_.Configuration.RenderingItem_NavigationSpeedMouseWheelZoom*/);
+            Camera.Zoom(-e.Delta * _tool.Configuration.RenderingItem_NavigationSpeedMouseWheelZoom);
             Invalidate();
         }
 
@@ -215,12 +215,12 @@ namespace WadTool.Controls
                 _lastY = e.Y;
 
                 if ((Control.ModifierKeys & Keys.Control) == Keys.Control)
-                    Camera.Zoom(-deltaY * 46000f /*_editor.Configuration.RenderingItem_NavigationSpeedMouseZoom*/);
+                    Camera.Zoom(-deltaY * _tool.Configuration.RenderingItem_NavigationSpeedMouseZoom);
                 else if ((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
-                    Camera.MoveCameraPlane(new Vector3(-deltaX, -deltaY, 0) * 22000f /* _editor.Configuration.RenderingItem_NavigationSpeedMouseTranslate*/);
+                    Camera.MoveCameraPlane(new Vector3(-deltaX, -deltaY, 0) * _tool.Configuration.RenderingItem_NavigationSpeedMouseTranslate);
                 else
-                    Camera.Rotate(deltaX * 2.2f /*_editor.Configuration.RenderingItem_NavigationSpeedMouseRotate*/, 
-                                  -deltaY * 2.2f /*_editor.Configuration.RenderingItem_NavigationSpeedMouseRotate*/);
+                    Camera.Rotate(deltaX * _tool.Configuration.RenderingItem_NavigationSpeedMouseRotate, 
+                                  -deltaY * _tool.Configuration.RenderingItem_NavigationSpeedMouseRotate);
                 Invalidate();
             }
         }
