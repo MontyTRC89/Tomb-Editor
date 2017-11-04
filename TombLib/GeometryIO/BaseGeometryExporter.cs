f39e3437cdf91d691860924d01d426f5d87b9d91
MontyTRC89 <montyhammet@hotmail.it>
2017-11-04 17:15:26 +0100

    Refactored geometry importer system;

---
+++ TombLib/GeometryIO/BaseGeometryExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TombLib.GeometryIO
+{
+    public abstract class BaseGeometryExporter
+    {
+        public abstract bool ExportToFile(IOMesh mesh, string filename);
+    }
+}
---
+++ TombLib/GeometryIO/IOTexture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TombLib.Utils;
+
+namespace TombLib.GeometryIO
+{
+    public class IOTexture
+    {
+        public string Name { get; private set; }
+        public ImageC Image { get; private set; }
+
+        public IOTexture(string name, ImageC image)
+        {
+            this.Name = name;
+            this.Image = image;
+        }
+
+        public int Width { get { return Image.Width; } }
+        public int Height { get { return Image.Height; } }
+    }
+}
