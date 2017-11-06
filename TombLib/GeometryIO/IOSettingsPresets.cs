0da6440cb3db3087e6f54dec40549ba9c46b5fe0
MontyTRC89 <montyhammet@hotmail.it>
2017-11-06 21:22:59 +0100

    Added missing file

---
+++ TombLib/GeometryIO/IOSettingsPresets.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TombLib.GeometryIO
+{
+    public static class IOSettingsPresets
+    {
+        public static List<IOGeometrySettingsPreset> SettingsPresets { get; private set; }
+
+        static IOSettingsPresets()
+        {
+            SettingsPresets = new List<IOGeometrySettingsPreset>();
+
+            // Blender
+            SettingsPresets.Add(new IOGeometrySettingsPreset
+            {
+                Name = "Blender OBJ",
+                Settings = new IOGeometrySettings
+                {
+                    Scale = 1024.0f,
+                    FlipZ = true,
+                    FlipUV_V = true,
+                    PremultiplyUV = true,
+                    WrapUV = true
+                }
+            });
+
+            SettingsPresets.Add(new IOGeometrySettingsPreset
+            {
+                Name = "Blender PLY",
+                Settings = new IOGeometrySettings
+                {
+                    Scale = 1024.0f,
+                    SwapYZ = true,
+                    FlipZ = false,
+                    FlipUV_V = true,
+                    PremultiplyUV = true,
+                    WrapUV = true
+                }
+            });
+
+            // Metasequoia
+            SettingsPresets.Add(new IOGeometrySettingsPreset
+            {
+                Name = "Metasequoia OBJ",
+                Settings = new IOGeometrySettings
+                {
+                    Scale = 1024.0f,
+                    FlipZ = true,
+                    FlipUV_V = true,
+                    PremultiplyUV = true,
+                    WrapUV = true
+                }
+            });
+
+            SettingsPresets.Add(new IOGeometrySettingsPreset
+            {
+                Name = "Metasequoia PLY",
+                Settings = new IOGeometrySettings
+                {
+                    Scale = 1024.0f,
+                    SwapYZ = true,
+                    FlipZ = false,
+                    FlipUV_V = true,
+                    PremultiplyUV = true,
+                    WrapUV = true
+                }
+            });
+
+            SettingsPresets.Add(new IOGeometrySettingsPreset
+            {
+                Name = "Metasequoia MQO",
+                Settings = new IOGeometrySettings
+                {
+                    Scale = 1.0f,
+                    FlipZ = true,
+                    FlipUV_V = false,
+                    PremultiplyUV = true,
+                    WrapUV = true
+                }
+            });
+        }
+    }
+}
