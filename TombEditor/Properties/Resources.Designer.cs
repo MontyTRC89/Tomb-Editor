17e7acf537645ad54e2e7be0b44e5ec244f02dab
Lwmte <initiator@nm.ru>
2017-11-09 05:09:13 +0300

    Added dummy disable custom geometry picking button

--- TombEditor/Properties/Resources.Designer.cs
+++ TombEditor/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace TombEditor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap actions_DisableGeometryPicking_16 {
+            get {
+                object obj = ResourceManager.GetObject("actions_DisableGeometryPicking_16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap actions_door_opened_16 {
             get {
                 object obj = ResourceManager.GetObject("actions_door_opened_16", resourceCulture);
