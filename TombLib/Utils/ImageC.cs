21b0e654e51ff8f21b05c26321f58a61b41a24d3
ActuallyaDeviloper <ActuallyaDeviloper@users.noreply.github.com>
2017-12-27 13:09:48 +0100

    Remove code that turns out to be unimportant and apparently causes a bug where the area of a TextureView becomes invalid.

--- TombLib/Utils/ImageC.cs
+++ TombLib/Utils/ImageC.cs
@@ -380,8 +380,7 @@ namespace TombLib.Utils
             if ((toX < 0) || (toY < 0) || (fromX < 0) || (fromY < 0) || (width < 0) || (height < 0) ||
                 (toX + width > Width) || (toY + height > Height) ||
                 (fromX + width > fromImage.Width) || (fromY + height > fromImage.Height))
-                return;
-             //   throw new ArgumentOutOfRangeException();
+                throw new ArgumentOutOfRangeException();
 
             // Copy data quickly
             fixed (void* toPtr = _data)
