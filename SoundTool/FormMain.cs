3cb209bb056588856e5476215e31b11ef2cecc5c
MontyTRC89 <montyhammet@hotmail.it>
2017-12-29 17:07:48 +0100

    Started working on UI of Sound Tool

--- SoundTool/FormMain.cs
+++ SoundTool/FormMain.cs
@@ -16,5 +16,10 @@ namespace SoundTool
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
