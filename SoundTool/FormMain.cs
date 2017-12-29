08ec841fc1178429de78d3c9dc96df04947784cd
MontyTRC89 <montyhammet@hotmail.it>
2017-12-29 17:04:48 +0100

    Added Sound Tool

---
+++ SoundTool/FormMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundTool
+{
+    public partial class FormMain : DarkUI.Forms.DarkForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
