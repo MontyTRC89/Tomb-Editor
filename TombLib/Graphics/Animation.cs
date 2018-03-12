174a1e8bd1c92a963e8e3203429b9afbb9aa7849
MontyTRC89 <montyhammet@hotmail.it>
2018-03-13 00:10:39 +0100

    Added code for playing animations; Some fixes to main form of WadTool; Some code cleanup;

--- TombLib/Graphics/Animation.cs
+++ TombLib/Graphics/Animation.cs
@@ -11,11 +11,6 @@ namespace TombLib.Graphics
         public short Framerate { get; set; }
         public float Speed { get; set; }
         public float Acceleration { get; set; }
-        public short NextAnimation { get; set; }
-        public short NextFrame { get; set; }
-        public short StateId { get; set; }
         public List<KeyFrame> KeyFrames { get; set; } = new List<KeyFrame>();
-        public List<StateChange> StateChanges { get; set; } = new List<StateChange>();
-        public List<ushort> AnimCommands { get; set; } = new List<ushort>();
     }
 }
--- WadTool/FormMain.cs
+++ WadTool/FormMain.cs
@@ -14,6 +14,7 @@ using TombLib.Wad.Catalog;
 using System.Diagnostics;
 using NLog;
 using TombLib.Graphics;
+using DarkUI.Controls;
 
 namespace WadTool
 {
@@ -21,6 +22,7 @@ namespace WadTool
     {
         private WadToolClass _tool;
         private DeviceManager _deviceManager;
+        private bool _playAnimation;
 
         public FormMain(WadToolClass tool)
         {
@@ -98,6 +100,23 @@ namespace WadTool
                     panel3D.CurrentObjectId = mainSelection.Value.Id;
                     panel3D.AnimationIndex = 0;
                     panel3D.KeyFrameIndex = 0;
+
+                    // Update animations list
+                    if (mainSelection.Value.Id is WadMoveableId)
+                    {
+                        var moveableId = (WadMoveableId)mainSelection.Value.Id;
+                        var moveable = panel3D.CurrentWad.Moveables[moveableId];
+                        var animationsNodes = new List<DarkTreeNode>();
+                        treeAnimations.Nodes.Clear();
+                        for (int i = 0; i < moveable.Animations.Count; i++)
+                        {
+                            var nodeAnimation = new DarkTreeNode(moveable.Animations[i].Name);
+                            nodeAnimation.Tag = i;
+                            animationsNodes.Add(nodeAnimation);
+                        }
+                        treeAnimations.Nodes.AddRange(animationsNodes);
+                    }
+
                     panel3D.Invalidate();
                 }
 
@@ -113,6 +132,7 @@ namespace WadTool
 
         private void treeSourceWad_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             IWadObjectId currentSelection = treeSourceWad.SelectedWadObjectIds.FirstOrDefault();
             if (currentSelection != null)
                 _tool.MainSelection = new MainSelection { WadArea = WadArea.Source, Id = currentSelection };
@@ -120,6 +140,7 @@ namespace WadTool
 
         private void treeDestWad_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             IWadObjectId currentSelection = treeDestWad.SelectedWadObjectIds.FirstOrDefault();
             if (currentSelection != null)
                 _tool.MainSelection = new MainSelection { WadArea = WadArea.Destination, Id = currentSelection };
@@ -127,26 +148,31 @@ namespace WadTool
 
         private void openDestinationWad2ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.LoadWad(_tool, this, true);
         }
 
         private void butOpenDestWad_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.LoadWad(_tool, this, true);
         }
 
         private void butOpenSourceWad_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.LoadWad(_tool, this, false);
         }
 
         private void butAddObject_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CopyObject(_tool, this, treeSourceWad.SelectedWadObjectIds.ToList(), false);
         }
 
         private void butDeleteObject_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             foreach (IWadObjectId id in treeDestWad.SelectedWadObjectIds)
                 _tool.DestinationWad.Remove(id);
             _tool.DestinationWadChanged();
@@ -154,21 +180,25 @@ namespace WadTool
 
         private void butSave_Click(object sender, EventArgs e)
         {
-           WadActions.SaveWad(_tool, this, _tool.DestinationWad, false);
+            StopAnimation();
+            WadActions.SaveWad(_tool, this, _tool.DestinationWad, false);
         }
 
         private void butSaveAs_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.SaveWad(_tool, this, _tool.DestinationWad, true);
         }
 
         private void saveWad2AsToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             butSaveAs_Click(null, null);
         }
 
         private void butAddObjectToDifferentSlot_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CopyObject(_tool, this, treeSourceWad.SelectedWadObjectIds.ToList(), true);
         }
 
@@ -192,23 +222,31 @@ namespace WadTool
             currentSound.Play();
         }
 
+        private void StopAnimation()
+        {
+            timerPlayAnimation.Stop();
+            _playAnimation = false;
+            butPlayAnimation.Enabled = true;
+            butStop.Enabled = false;
+        }
+
         private void butNewWad_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CreateNewWad(_tool, this);
         }
 
         private void butChangeSlot_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.ChangeSlot(_tool, this);
         }
 
         private void lstAnimations_Click(object sender, EventArgs e)
         {
-            // Get selected object
-            if (_tool.MainSelection != null)
-                return;
             Wad2 wad = _tool.GetWad(_tool.MainSelection.Value.WadArea);
-            WadMoveable moveable = _tool.MainSelection.Value.Id as WadMoveable;
+            var moveableId = (WadMoveableId)_tool.MainSelection.Value.Id;
+            var moveable = wad.Moveables[moveableId];
             if (moveable == null)
                 return;
 
@@ -217,11 +255,25 @@ namespace WadTool
                 return;
             var node = treeAnimations.SelectedNodes[0];
             int animationIndex = (int)node.Tag;
+            var animation = moveable.Animations[animationIndex];
+
+            // Reset scrollbar
+            scrollbarAnimations.Value = 0;
+            scrollbarAnimations.Maximum = animation.RealNumberOfFrames - 1;
 
             // Reset panel 3D
             panel3D.AnimationIndex = animationIndex;
             panel3D.KeyFrameIndex = 0;
-            panel3D.Invalidate();
+
+            if (_playAnimation)
+            {
+                int frameRate = (int)Math.Max(animation.FrameDuration, (short)1);
+                timerPlayAnimation.Stop();
+                timerPlayAnimation.Interval = 1000 / 30 / frameRate;
+                timerPlayAnimation.Start();
+            }
+
+            panel3D.Draw();
         }
 
         private void importModelAsStaticMeshToolStripMenuItem_Click(object sender, EventArgs e)
@@ -237,21 +289,25 @@ namespace WadTool
 
         private void newWad2ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CreateNewWad(_tool, this);
         }
 
         private void treeDestWad_DoubleClick(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.EditObject(_tool, this, _deviceManager);
         }
 
         private void treeSourceWad_DoubleClick(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.EditObject(_tool, this, _deviceManager);
         }
 
         private void treeDestWad_KeyDown(object sender, KeyEventArgs e)
         {
+            StopAnimation();
             switch (e.KeyCode)
             {
                 case Keys.Delete:
@@ -262,31 +318,30 @@ namespace WadTool
 
         private void treeSourceWad_KeyDown(object sender, KeyEventArgs e)
         {
-            switch (e.KeyCode)
-            {
-                case Keys.Delete:
-                    WadActions.DeleteObjects(_tool, this, WadArea.Source, treeSourceWad.SelectedWadObjectIds.ToList());
-                    break;
-            }
+            StopAnimation();            
         }
 
         private void newMoveableToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CreateObject(_tool, this, new WadMoveable(new WadMoveableId()));
         }
 
         private void newStaticToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CreateObject(_tool, this, new WadStatic(new WadStaticId()));
         }
 
         private void newSpriteSequenceToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CreateObject(_tool, this, new WadSpriteSequence(new WadSpriteSequenceId()));
         }
 
         private void newFixedSoundInfoToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            StopAnimation();
             WadActions.CreateObject(_tool, this, new WadFixedSoundInfo(new WadFixedSoundInfoId()));
         }
 
@@ -329,6 +384,105 @@ namespace WadTool
 
         private void debugAction9ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+        }
+
+        private void timerPlayAnimation_Tick(object sender, EventArgs e)
+        {
+            if (_playAnimation)
+            {
+                // Get selected moveable
+                var wad = _tool.GetWad(_tool.MainSelection.Value.WadArea);
+                var moveableId = (WadMoveableId)_tool.MainSelection.Value.Id;
+                var moveable = wad.Moveables[moveableId];
+                var model = wad.DirectXMoveables[(WadMoveableId)_tool.MainSelection.Value.Id];
+                if (moveable == null || model == null || moveable.Animations.Count == 0)
+                {
+                    _playAnimation = false;
+                    return;
+                }
+
+                // Get selected animation
+                if (treeAnimations.SelectedNodes.Count == 0)
+                    return;
+                var node = treeAnimations.SelectedNodes[0];
+                var animationIndex = (int)node.Tag;
+                if (animationIndex >= moveable.Animations.Count)
+                {
+                    _playAnimation = false;
+                    return;
+                }
+                var animation = moveable.Animations[animationIndex];
+
+                // Update animation
+                if (panel3D.KeyFrameIndex >= animation.RealNumberOfFrames)
+                    panel3D.KeyFrameIndex = 0;
+                else
+                    panel3D.KeyFrameIndex++;
+                model.UpdateAnimation(animationIndex, panel3D.KeyFrameIndex);
+                panel3D.Draw();
+            }
+        }
+
+        private void butPlayAnimation_Click(object sender, EventArgs e)
+        {
+            // Get selected moveable
+            var wad = _tool.GetWad(_tool.MainSelection.Value.WadArea);
+            var moveableId = (WadMoveableId)_tool.MainSelection.Value.Id;
+            var moveable = wad.Moveables[moveableId];
+            var model = wad.DirectXMoveables[(WadMoveableId)_tool.MainSelection.Value.Id];
+            if (moveable == null || model == null || moveable.Animations.Count == 0)
+            {
+                _playAnimation = false;
+                return;
+            }
+
+            // Get selected animation
+            if (treeAnimations.SelectedNodes.Count == 0)
+                return;
+            var node = treeAnimations.SelectedNodes[0];
+            var animationIndex = (int)node.Tag;
+            if (animationIndex >= moveable.Animations.Count)
+            {
+                _playAnimation = false;
+                return;
+            }
+            var animation = moveable.Animations[animationIndex];
+
+            panel3D.AnimationIndex = animationIndex;
+            panel3D.KeyFrameIndex = 0;
+
+            // Start animation
+            _playAnimation = true;
+            int frameRate = (int)Math.Max(animation.FrameDuration, (short)1);
+            timerPlayAnimation.Stop();
+            timerPlayAnimation.Interval = 1000 / 30 / frameRate;
+            timerPlayAnimation.Start();
+
+            butPlayAnimation.Enabled = false;
+            butStop.Enabled = true;
+        }
+
+        private void butStop_Click(object sender, EventArgs e)
+        {
+            _playAnimation = false;
+            timerPlayAnimation.Stop();
+            butStop.Enabled = false;
+            butPlayAnimation.Enabled = true;
+        }
+
+        private void butEditItem_Click(object sender, EventArgs e)
+        {
+            StopAnimation();
+        }
+
+        private void butRenameAnimation_Click(object sender, EventArgs e)
+        {
+            StopAnimation();
+        }
+
+        private void butRenameSound_Click(object sender, EventArgs e)
+        {
+            StopAnimation();
         }
     }
 }
