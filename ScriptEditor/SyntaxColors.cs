9ed1f93bb853388530bf88279cf967f997b250e8
Unknown <nickelkwn@gmail.com>
2018-07-10 14:55:17 +0200

    Major syntax highlight / tooltips update.

--- ScriptEditor/SyntaxColors.cs
+++ ScriptEditor/SyntaxColors.cs
@@ -5,12 +5,13 @@ namespace ScriptEditor
 {
 	public class SyntaxColors
 	{
-		public static TextStyle Comments = new TextStyle(new SolidBrush(Properties.Settings.Default.CommentsColor), null, FontStyle.Regular);
-		public static TextStyle Keys = new TextStyle(new SolidBrush(Properties.Settings.Default.KeyValueColor), null, FontStyle.Regular);
+		public static TextStyle Comments = new TextStyle(new SolidBrush(Properties.Settings.Default.CommentColor), null, FontStyle.Regular);
+		public static TextStyle NewCommands = new TextStyle(new SolidBrush(Properties.Settings.Default.NewCommandColor), null, FontStyle.Regular);
+		public static TextStyle OldCommands = new TextStyle(new SolidBrush(Properties.Settings.Default.OldCommandColor), null, FontStyle.Regular);
 		public static TextStyle Headers = new TextStyle(new SolidBrush(Properties.Settings.Default.HeaderColor), null, FontStyle.Bold);
-		public static TextStyle References = new TextStyle(new SolidBrush(Properties.Settings.Default.ReferencesColor), null, FontStyle.Regular);
+		public static TextStyle References = new TextStyle(new SolidBrush(Properties.Settings.Default.ReferenceColor), null, FontStyle.Regular);
 		public static TextStyle Regular = new TextStyle(null, null, FontStyle.Regular);
 		public static TextStyle Unknown = new TextStyle(new SolidBrush(Properties.Settings.Default.UnknownColor), null, FontStyle.Bold);
-		public static TextStyle Values = new TextStyle(new SolidBrush(Properties.Settings.Default.ValuesColor), null, FontStyle.Regular);
+		public static TextStyle Values = new TextStyle(new SolidBrush(Properties.Settings.Default.ValueColor), null, FontStyle.Regular);
 	}
 }
