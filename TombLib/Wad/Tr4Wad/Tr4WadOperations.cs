8d84cbccfddbe76fd9aa5be3640358608ae1ca67
Lwmte <initiator@nm.ru>
2018-08-25 13:26:14 +0300

    More detailed log message for wrong NextFrame values in anim dispatches

--- TombLib/Wad/Tr4Wad/Tr4WadOperations.cs
+++ TombLib/Wad/Tr4Wad/Tr4WadOperations.cs
@@ -666,7 +666,7 @@ namespace TombLib.Wad.Tr4Wad
                             // From tests it seems that's ok to delete the dispatch or put the NextFrame equal to max frame number.
                             if (newFrame > newMoveable.Animations[animDispatch.NextAnimation].RealNumberOfFrames)
                             {
-                                logger.Warn("Animation " + i + " has wrong anim dispatch " + j + " refering to nonexistent frame " + newFrame + ", corrected to max. frame");
+                                logger.Warn("Animation " + i + " has wrong anim dispatch " + j + " refering to nonexistent frame " + newFrame + ", corrected to " + newMoveable.Animations[animDispatch.NextAnimation].RealNumberOfFrames);
                                 newFrame = (ushort)(newMoveable.Animations[animDispatch.NextAnimation].RealNumberOfFrames);
                             }
 
