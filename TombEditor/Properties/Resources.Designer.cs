8c2daf3d8ed994c7d8fb5a4439aaacc26ef92917
Lwmte <initiator@nm.ru>
2018-08-31 20:45:24 +0300

    Add "copy with comments" to trigger script

--- TombEditor/Properties/Resources.Designer.cs
+++ TombEditor/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace TombEditor.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -326,6 +326,16 @@ namespace TombEditor.Properties {
         internal static System.Drawing.Bitmap general_copy_16 {
             get {
                 object obj = ResourceManager.GetObject("general_copy_16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap general_copy_comments_16 {
+            get {
+                object obj = ResourceManager.GetObject("general_copy_comments_16", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
