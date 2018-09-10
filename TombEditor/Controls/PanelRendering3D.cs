f3e47899b49304348fde6ff1b321771f66a34afb
Lwmte <initiator@nm.ru>
2018-09-11 02:50:45 +0300

    Limit inwards dragging for portal digger

--- TombEditor/Controls/PanelRendering3D.cs
+++ TombEditor/Controls/PanelRendering3D.cs
@@ -684,6 +684,8 @@ namespace TombEditor.Controls
                                     case PortalDirection.Floor:
                                     case PortalDirection.Ceiling:
                                         var newHeight = (-dragValue.Value.Y * verticalPrecision);
+                                        if (resizeHeight[1] - resizeHeight[0] + (portalDirection == PortalDirection.Floor ? newHeight : -newHeight) <= 0)
+                                            return;  // Limit inwards dragging
                                         resizeHeight[0] = (short)(portalDirection == PortalDirection.Floor ? 0 : newHeight);
                                         resizeHeight[1] = (short)(portalDirection == PortalDirection.Floor ? newHeight : 0);
                                         break;
