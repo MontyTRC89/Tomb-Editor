5292dbb8a679e57636e00eb93a88347b91286513
Lwmte <initiator@nm.ru>
2018-11-02 00:50:43 +0300

    Bring back SlicePitch patch which fixes #235

--- TombLib.Rendering/Rendering/DirectX11/Dx11RenderingTextureAllocator.cs
+++ TombLib.Rendering/Rendering/DirectX11/Dx11RenderingTextureAllocator.cs
@@ -60,7 +60,7 @@ namespace TombLib.Rendering.DirectX11
                 DataBox box;
                 box.DataPointer = ptr + (texture.From.X + texture.Image.Width * texture.From.Y) * ImageC.PixelSize;
                 box.RowPitch = texture.Image.Width * ImageC.PixelSize;
-                box.SlicePitch = box.RowPitch * texture.Image.Height;
+                box.SlicePitch = 0;
                 Context.UpdateSubresource(box, Texture, subresourceIndex, region);
             });
         }
