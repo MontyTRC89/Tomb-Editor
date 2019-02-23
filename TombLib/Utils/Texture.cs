7f8d804c4409330228a2e263f2a8916264c04d35
MontyTRC89 <montyhammet@hotmail.it>
2019-02-23 18:01:20 +0100

    Fixed serious bug with oversized textures

--- TombLib/Utils/Texture.cs
+++ TombLib/Utils/Texture.cs
@@ -217,7 +217,9 @@ namespace TombLib.Utils
 
                 Vector2 max = Vector2.Max(Vector2.Max(TexCoord0, TexCoord1), TexCoord2);
                 Vector2 min = Vector2.Min(Vector2.Min(TexCoord0, TexCoord1), TexCoord2);
-                return min.X < 0.0f || min.Y < 0.0f || max.X > Texture.Image.Width || max.Y > Texture.Image.Height;
+                return min.X < 0.0f || min.Y < 0.0f || max.X > Texture.Image.Width || max.Y > Texture.Image.Height ||
+                       max.X - min.X >= 256.0f || max.Y - min.Y >= 256.0f;
+                //return min.X < 0.0f || min.Y < 0.0f || max.X > Texture.Image.Width || max.Y > Texture.Image.Height;
             }
         }
 
@@ -229,7 +231,9 @@ namespace TombLib.Utils
                     return false;
                 Vector2 max = Vector2.Max(Vector2.Max(TexCoord0, TexCoord1), Vector2.Max(TexCoord2, TexCoord3));
                 Vector2 min = Vector2.Min(Vector2.Min(TexCoord0, TexCoord1), Vector2.Min(TexCoord2, TexCoord3));
-                return min.X < 0.0f || min.Y < 0.0f || max.X > Texture.Image.Width || max.Y > Texture.Image.Height;
+                return min.X < 0.0f || min.Y < 0.0f || max.X > Texture.Image.Width || max.Y > Texture.Image.Height ||
+                       max.X - min.X >= 256.0f || max.Y - min.Y >= 256.0f;
+                //return min.X < 0.0f || min.Y < 0.0f || max.X > Texture.Image.Width || max.Y > Texture.Image.Height;
             }
         }
 
