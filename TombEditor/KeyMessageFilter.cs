7210d918e3275d8d32916d6ca4d6b8fa239da26a
Nickelony <nickelkwn@gmail.com>
2019-03-30 09:42:25 +0100

    Fixed UI scaling in Windows 10 when toggling FlyMode.

---
+++ TombEditor/KeyMessageFilter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TombEditor
+{
+	public class KeyMessageFilter : IMessageFilter
+	{
+		private const int WM_KEYDOWN = 0x0100;
+		private const int WM_KEYUP = 0x0101;
+		private bool _keyPressed = false;
+
+		public Dictionary<Keys, bool> KeyTable { get; private set; } = new Dictionary<Keys, bool>();
+
+		public bool IsKeyPressed()
+		{
+			return _keyPressed;
+		}
+
+		public bool IsKeyPressed(Keys k)
+		{
+			if (KeyTable.TryGetValue(k, out bool pressed))
+				return pressed;
+
+			return false;
+		}
+
+		public bool PreFilterMessage(ref Message m)
+		{
+			if (m.Msg == WM_KEYDOWN)
+			{
+				KeyTable[(Keys)m.WParam] = true;
+
+				_keyPressed = true;
+			}
+
+			if (m.Msg == WM_KEYUP)
+			{
+				KeyTable[(Keys)m.WParam] = false;
+
+				_keyPressed = false;
+			}
+
+			return false;
+		}
+	}
+}
