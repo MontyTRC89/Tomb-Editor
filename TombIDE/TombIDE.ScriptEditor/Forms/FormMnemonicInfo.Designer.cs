813a7e04c5b3405a9c1664d49fa4869e2ae024af
Nickelony <nickelkwn@gmail.com>
2019-08-18 22:41:02 +0200

    Minor adjustments.

--- TombIDE/TombIDE.ScriptEditor/Forms/FormMnemonicInfo.Designer.cs
+++ TombIDE/TombIDE.ScriptEditor/Forms/FormMnemonicInfo.Designer.cs
@@ -18,7 +18,6 @@
 		private void InitializeComponent()
 		{
 			this.label_FlagName = new DarkUI.Controls.DarkLabel();
-			this.label_Paolone = new DarkUI.Controls.DarkLabel();
 			this.panel_01 = new System.Windows.Forms.Panel();
 			this.panel_02 = new System.Windows.Forms.Panel();
 			this.richTextBox_Description = new System.Windows.Forms.RichTextBox();
@@ -28,46 +27,34 @@
 			// 
 			// label_FlagName
 			// 
-			this.label_FlagName.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label_FlagName.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.label_FlagName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label_FlagName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
 			this.label_FlagName.Location = new System.Drawing.Point(0, 0);
 			this.label_FlagName.Name = "label_FlagName";
-			this.label_FlagName.Size = new System.Drawing.Size(622, 32);
+			this.label_FlagName.Size = new System.Drawing.Size(622, 46);
 			this.label_FlagName.TabIndex = 0;
 			this.label_FlagName.Text = "FLAG_NAME_GOES_HERE";
-			this.label_FlagName.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-			// 
-			// label_Paolone
-			// 
-			this.label_Paolone.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.label_Paolone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-			this.label_Paolone.ForeColor = System.Drawing.Color.Gray;
-			this.label_Paolone.Location = new System.Drawing.Point(0, 34);
-			this.label_Paolone.Name = "label_Paolone";
-			this.label_Paolone.Size = new System.Drawing.Size(622, 20);
-			this.label_Paolone.TabIndex = 1;
-			this.label_Paolone.Text = "(According to Paolone)";
-			this.label_Paolone.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label_FlagName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// panel_01
 			// 
 			this.panel_01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.panel_01.Controls.Add(this.label_Paolone);
 			this.panel_01.Controls.Add(this.label_FlagName);
 			this.panel_01.Dock = System.Windows.Forms.DockStyle.Top;
 			this.panel_01.Location = new System.Drawing.Point(0, 0);
 			this.panel_01.Name = "panel_01";
-			this.panel_01.Size = new System.Drawing.Size(624, 56);
+			this.panel_01.Size = new System.Drawing.Size(624, 48);
 			this.panel_01.TabIndex = 0;
 			// 
 			// panel_02
 			// 
+			this.panel_02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.panel_02.Controls.Add(this.richTextBox_Description);
 			this.panel_02.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel_02.Location = new System.Drawing.Point(0, 56);
+			this.panel_02.Location = new System.Drawing.Point(0, 48);
 			this.panel_02.Name = "panel_02";
-			this.panel_02.Size = new System.Drawing.Size(624, 385);
+			this.panel_02.Size = new System.Drawing.Size(624, 393);
 			this.panel_02.TabIndex = 1;
 			// 
 			// richTextBox_Description
@@ -77,10 +64,10 @@
 			this.richTextBox_Description.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.richTextBox_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.richTextBox_Description.ForeColor = System.Drawing.Color.Gainsboro;
-			this.richTextBox_Description.Location = new System.Drawing.Point(0, 0);
+			this.richTextBox_Description.Location = new System.Drawing.Point(1, 1);
 			this.richTextBox_Description.Name = "richTextBox_Description";
 			this.richTextBox_Description.ReadOnly = true;
-			this.richTextBox_Description.Size = new System.Drawing.Size(624, 385);
+			this.richTextBox_Description.Size = new System.Drawing.Size(620, 389);
 			this.richTextBox_Description.TabIndex = 1;
 			this.richTextBox_Description.Text = "";
 			// 
@@ -104,7 +91,6 @@
 		#endregion
 
 		private DarkUI.Controls.DarkLabel label_FlagName;
-		private DarkUI.Controls.DarkLabel label_Paolone;
 		private System.Windows.Forms.Panel panel_01;
 		private System.Windows.Forms.Panel panel_02;
 		private System.Windows.Forms.RichTextBox richTextBox_Description;
