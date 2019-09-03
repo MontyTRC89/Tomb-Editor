8d10abc35872bd703cb6275aa6c7400d0ac5d004
MontyTRC89 <montyhammet@hotmail.it>
2019-09-03 22:10:46 +0200

    Fixed sinks

--- TombLib/LevelData/Compilers/LevelCompilerClassicTR.cs
+++ TombLib/LevelData/Compilers/LevelCompilerClassicTR.cs
@@ -216,6 +216,9 @@ namespace TombLib.LevelData.Compilers
                         _cameraTable.Add(obj, cameraSinkID++);
                     foreach (var obj in room.Objects.OfType<FlybyCameraInstance>())
                         _flybyTable.Add(obj, flybyID++);
+                }
+                foreach (var room in _level.Rooms.Where(room => room != null))
+                {
                     foreach (var obj in room.Objects.OfType<SinkInstance>())
                         _sinkTable.Add(obj, cameraSinkID++);
                 }
