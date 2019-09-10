06145ae96cc72e88048016f2f89bf73784238d58
Lwmte <initiator@nm.ru>
2019-09-11 02:32:08 +0300

    Fix textbox size in room options

--- TombEditor/ToolWindows/RoomOptions.cs
+++ TombEditor/ToolWindows/RoomOptions.cs
@@ -23,6 +23,10 @@ namespace TombEditor.ToolWindows
             _editor = Editor.Instance;
             _editor.EditorEventRaised += EditorEventRaised;
             EditorEventRaised(new Editor.InitEvent());
+
+            // A hack to edit textbox size
+            tbRoomTags.AutoSize = false;
+            tbRoomTags.Height = 23;
         }
 
         protected override void Dispose(bool disposing)
