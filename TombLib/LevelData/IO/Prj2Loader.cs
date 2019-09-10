9139cb39ffc9d643682b5d7e8d08ed8492c9cbcc
Lwmte <initiator@nm.ru>
2019-09-11 01:53:10 +0300

    Don't make dummy empty tags on prj2 opening

--- TombLib/LevelData/IO/Prj2Loader.cs
+++ TombLib/LevelData/IO/Prj2Loader.cs
@@ -546,7 +546,13 @@ namespace TombLib.LevelData.IO
                     else if (id2 == Prj2Chunks.RoomPosition)
                         room.Position = VectorInt3.FromRounded(chunkIO.ReadChunkVector3(chunkSize2));
                     else if (id2 == Prj2Chunks.RoomTags)
-                        room.Tags = System.Text.Encoding.UTF8.GetString(chunkIO.ReadChunkArrayOfBytes(chunkSize2)).Split(' ').ToList();
+                    {
+                        var tags = System.Text.Encoding.UTF8.GetString(chunkIO.ReadChunkArrayOfBytes(chunkSize2)).Split(' ');
+                        if (tags.Count() == 1 && string.IsNullOrEmpty(tags[0]))
+                            room.Tags = new List<string>();
+                        else
+                            room.Tags = tags.ToList();
+                    }
 
 
                     // Read sectors
