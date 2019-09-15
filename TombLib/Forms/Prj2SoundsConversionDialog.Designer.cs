cb99293a8b685b833fdc29ba04691762385f6b44
MontyTRC89 <montyhammet@hotmail.it>
2019-09-15 17:21:36 +0200

    Fixed MatchDoorShades

--- TombLib/Forms/Prj2SoundsConversionDialog.Designer.cs
+++ TombLib/Forms/Prj2SoundsConversionDialog.Designer.cs
@@ -35,21 +35,26 @@ namespace TombLib.Forms
             this.butOK = new DarkUI.Controls.DarkButton();
             this.darkLabel1 = new DarkUI.Controls.DarkLabel();
             this.dgvSoundInfos = new DarkUI.Controls.DarkDataGridView();
-            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
-            this.darkStatusStrip1 = new DarkUI.Controls.DarkStatusStrip();
-            this.statusSamples = new System.Windows.Forms.ToolStripStatusLabel();
             this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.newIdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.newNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.darkStatusStrip1 = new DarkUI.Controls.DarkStatusStrip();
+            this.statusSamples = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.butSearchSampelsPaths = new DarkUI.Controls.DarkButton();
+            this.darkLabel7 = new DarkUI.Controls.DarkLabel();
+            this.tbSamplesPath = new DarkUI.Controls.DarkTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSoundInfos)).BeginInit();
             this.darkStatusStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // butCancel
             // 
             this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.butCancel.Location = new System.Drawing.Point(562, 587);
+            this.butCancel.Location = new System.Drawing.Point(562, 589);
             this.butCancel.Name = "butCancel";
             this.butCancel.Size = new System.Drawing.Size(80, 23);
             this.butCancel.TabIndex = 3;
@@ -59,7 +64,7 @@ namespace TombLib.Forms
             // butOK
             // 
             this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.butOK.Location = new System.Drawing.Point(476, 587);
+            this.butOK.Location = new System.Drawing.Point(476, 589);
             this.butOK.Name = "butOK";
             this.butOK.Size = new System.Drawing.Size(80, 23);
             this.butOK.TabIndex = 2;
@@ -86,8 +91,7 @@ namespace TombLib.Forms
             this.dgvSoundInfos.AllowUserToAddRows = false;
             this.dgvSoundInfos.AllowUserToDeleteRows = false;
             this.dgvSoundInfos.AllowUserToDragDropRows = false;
-            this.dgvSoundInfos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.dgvSoundInfos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvSoundInfos.ColumnHeadersHeight = 17;
             this.dgvSoundInfos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -97,29 +101,11 @@ namespace TombLib.Forms
             this.dgvSoundInfos.Location = new System.Drawing.Point(12, 70);
             this.dgvSoundInfos.Name = "dgvSoundInfos";
             this.dgvSoundInfos.RowHeadersWidth = 41;
-            this.dgvSoundInfos.Size = new System.Drawing.Size(630, 511);
+            this.dgvSoundInfos.Size = new System.Drawing.Size(630, 448);
             this.dgvSoundInfos.TabIndex = 5;
             this.dgvSoundInfos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSamples_CellContentClick);
             this.dgvSoundInfos.CellValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvSoundInfos_CellValidated);
             this.dgvSoundInfos.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.DgvSoundInfos_CellValidating);
-            // 
-            // darkStatusStrip1
-            // 
-            this.darkStatusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-            this.darkStatusStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-            this.darkStatusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.statusSamples});
-            this.darkStatusStrip1.Location = new System.Drawing.Point(0, 617);
-            this.darkStatusStrip1.Name = "darkStatusStrip1";
-            this.darkStatusStrip1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 3);
-            this.darkStatusStrip1.Size = new System.Drawing.Size(654, 28);
-            this.darkStatusStrip1.TabIndex = 12;
-            this.darkStatusStrip1.Text = "darkStatusStrip1";
-            // 
-            // statusSamples
-            // 
-            this.statusSamples.Name = "statusSamples";
-            this.statusSamples.Size = new System.Drawing.Size(0, 0);
             // 
             // nameColumn
             // 
@@ -139,13 +125,71 @@ namespace TombLib.Forms
             this.newNameColumn.Name = "newNameColumn";
             this.newNameColumn.Width = 150;
             // 
+            // darkStatusStrip1
+            // 
+            this.darkStatusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkStatusStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkStatusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusSamples});
+            this.darkStatusStrip1.Location = new System.Drawing.Point(0, 619);
+            this.darkStatusStrip1.Name = "darkStatusStrip1";
+            this.darkStatusStrip1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 3);
+            this.darkStatusStrip1.Size = new System.Drawing.Size(654, 28);
+            this.darkStatusStrip1.TabIndex = 12;
+            this.darkStatusStrip1.Text = "darkStatusStrip1";
+            // 
+            // statusSamples
+            // 
+            this.statusSamples.Name = "statusSamples";
+            this.statusSamples.Size = new System.Drawing.Size(0, 0);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.butSearchSampelsPaths);
+            this.panel2.Controls.Add(this.darkLabel7);
+            this.panel2.Controls.Add(this.tbSamplesPath);
+            this.panel2.Location = new System.Drawing.Point(12, 524);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(630, 50);
+            this.panel2.TabIndex = 13;
+            // 
+            // butSearchSampelsPaths
+            // 
+            this.butSearchSampelsPaths.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butSearchSampelsPaths.Location = new System.Drawing.Point(535, 23);
+            this.butSearchSampelsPaths.Name = "butSearchSampelsPaths";
+            this.butSearchSampelsPaths.Size = new System.Drawing.Size(92, 22);
+            this.butSearchSampelsPaths.TabIndex = 3;
+            this.butSearchSampelsPaths.Text = "Search";
+            // 
+            // darkLabel7
+            // 
+            this.darkLabel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel7.Location = new System.Drawing.Point(0, 3);
+            this.darkLabel7.Name = "darkLabel7";
+            this.darkLabel7.Size = new System.Drawing.Size(439, 17);
+            this.darkLabel7.TabIndex = 1;
+            this.darkLabel7.Text = "Folder in which all runtime game components reside:";
+            // 
+            // tbSamplesPath
+            // 
+            this.tbSamplesPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSamplesPath.Location = new System.Drawing.Point(0, 23);
+            this.tbSamplesPath.Name = "tbSamplesPath";
+            this.tbSamplesPath.Size = new System.Drawing.Size(529, 22);
+            this.tbSamplesPath.TabIndex = 2;
+            // 
             // Prj2SoundsConversionDialog
             // 
             this.AcceptButton = this.butOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.butCancel;
-            this.ClientSize = new System.Drawing.Size(654, 645);
+            this.ClientSize = new System.Drawing.Size(654, 647);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.darkStatusStrip1);
             this.Controls.Add(this.dgvSoundInfos);
             this.Controls.Add(this.darkLabel1);
@@ -163,6 +207,8 @@ namespace TombLib.Forms
             ((System.ComponentModel.ISupportInitialize)(this.dgvSoundInfos)).EndInit();
             this.darkStatusStrip1.ResumeLayout(false);
             this.darkStatusStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -179,5 +225,9 @@ namespace TombLib.Forms
         private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn newIdColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn newNameColumn;
+        private System.Windows.Forms.Panel panel2;
+        private DarkUI.Controls.DarkButton butSearchSampelsPaths;
+        private DarkUI.Controls.DarkLabel darkLabel7;
+        private DarkUI.Controls.DarkTextBox tbSamplesPath;
     }
 }
