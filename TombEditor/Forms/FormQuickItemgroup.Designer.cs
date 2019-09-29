bc081ca8f55d9e5e15d6342d075ae60d9925a828
Nils Gaitzsch <nils.gaitzsch@googlemail.com>
2019-09-29 14:20:48 +0200

    TE-0016: Tracking Files

---
+++ TombEditor/Forms/FormQuickItemgroup.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TombEditor.Forms
+{
+    partial class FormQuickItemgroup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.cbSlots = new DarkUI.Controls.DarkComboBox();
+            this.butCancel = new DarkUI.Controls.DarkButton();
+            this.butOk = new DarkUI.Controls.DarkButton();
+            this.SuspendLayout();
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(12, 9);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(268, 23);
+            this.darkLabel1.TabIndex = 0;
+            this.darkLabel1.Text = "Enter the slot you want to create an itemgroup of:";
+            // 
+            // cbSlots
+            // 
+            this.cbSlots.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbSlots.FormattingEnabled = true;
+            this.cbSlots.Location = new System.Drawing.Point(12, 35);
+            this.cbSlots.Name = "cbSlots";
+            this.cbSlots.Size = new System.Drawing.Size(268, 21);
+            this.cbSlots.TabIndex = 1;
+            // 
+            // butCancel
+            // 
+            this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.butCancel.Location = new System.Drawing.Point(200, 68);
+            this.butCancel.Name = "butCancel";
+            this.butCancel.Size = new System.Drawing.Size(80, 23);
+            this.butCancel.TabIndex = 4;
+            this.butCancel.Text = "Cancel";
+            this.butCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.butCancel.Click += new System.EventHandler(this.ButCancel_Click);
+            // 
+            // butOk
+            // 
+            this.butOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butOk.Location = new System.Drawing.Point(114, 68);
+            this.butOk.Name = "butOk";
+            this.butOk.Size = new System.Drawing.Size(80, 23);
+            this.butOk.TabIndex = 3;
+            this.butOk.Text = "OK";
+            this.butOk.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.butOk.Click += new System.EventHandler(this.ButOk_Click);
+            // 
+            // FormQuickItemgroup
+            // 
+            this.ClientSize = new System.Drawing.Size(292, 103);
+            this.Controls.Add(this.butCancel);
+            this.Controls.Add(this.butOk);
+            this.Controls.Add(this.cbSlots);
+            this.Controls.Add(this.darkLabel1);
+            this.Name = "FormQuickItemgroup";
+            this.Text = "Make quick itemgroup";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private DarkUI.Controls.DarkComboBox cbSlots;
+        private DarkUI.Controls.DarkButton butCancel;
+        private DarkUI.Controls.DarkButton butOk;
+    }
+}
