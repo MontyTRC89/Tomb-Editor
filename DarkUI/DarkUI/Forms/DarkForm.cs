20fa0ed3bd9b918fbde48c09c575cab4a9362ef5
Lwmte <initiator@nm.ru>
2019-10-08 07:26:17 +0300

    Show non-modal centered forms properly

--- DarkUI/DarkUI/Forms/DarkForm.cs
+++ DarkUI/DarkUI/Forms/DarkForm.cs
@@ -1,4 +1,5 @@
 ﻿using DarkUI.Config;
+using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
@@ -41,6 +42,25 @@ namespace DarkUI.Forms
         {
             get { return base.BackColor; }
             set { base.BackColor = value; }
+        }
+
+        #endregion
+
+        #region Methods Region
+
+        public new void Show(IWin32Window parent)
+        {
+            // A hack to well-known problem of non-modal forms being non-centered
+            // https://stackoverflow.com/questions/8566582/how-to-centerparent-a-non-modal-form
+
+            if (StartPosition == FormStartPosition.CenterParent && !Modal && parent != null && parent is DarkForm)
+            {
+                var form = (DarkForm)parent;
+                StartPosition = FormStartPosition.Manual;
+                Location = new Point(form.Location.X + (form.Width - Width) / 2, form.Location.Y + (form.Height - Height) / 2);
+            }
+
+            base.Show(parent);
         }
 
         #endregion
