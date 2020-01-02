891c69784598ce6f1af088a46be90da19aaca1bb
Lwmte <initiator@nm.ru>
2020-01-02 22:44:22 +0300

    TE: Disable minimize for find/replace form

--- TombEditor/Forms/FormReplaceObject.Designer.cs
+++ TombEditor/Forms/FormReplaceObject.Designer.cs
@@ -275,6 +275,7 @@
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(10000, 264);
+            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(600, 264);
             this.Name = "FormReplaceObject";
             this.ShowIcon = false;
