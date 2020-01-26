aa4ddb1ec306abec5049417255d4af80e2af9986
Lwmte <initiator@nm.ru>
2020-01-26 15:25:37 +0300

    Update version to 1.2.11

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.10")]
-[assembly: AssemblyFileVersion("1.2.10")]
+[assembly: AssemblyVersion("1.2.11")]
+[assembly: AssemblyFileVersion("1.2.11")]
