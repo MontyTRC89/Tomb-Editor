5612bb7bcc3231c41608393d91848846cd7bcae9
Lwmte <initiator@nm.ru>
2020-05-03 02:02:28 +0300

    One more UI change

--- TombEditor/ToolWindows/TriggerList.Designer.cs
+++ TombEditor/ToolWindows/TriggerList.Designer.cs
@@ -108,7 +108,7 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Controls.Add(this.panelTriggerList);
             this.Controls.Add(this.panelTriggerTools);
-            this.DockText = "Triggers";
+            this.DockText = "Legacy Triggers";
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MinimumSize = new System.Drawing.Size(100, 100);
             this.Name = "TriggerList";
