7d3284d3d7b1e08c26097023cdf72d31ea527ba6
Nickelony <nickelkwn@gmail.com>
2020-05-05 20:11:40 +0200

    Fixed DarkTabbedContainer having a margin border.

--- TombLib.Forms/Controls/DarkTabbedContainer.cs
+++ TombLib.Forms/Controls/DarkTabbedContainer.cs
@@ -129,5 +129,14 @@ namespace TombLib.Controls
 
             base.OnParentBackColorChanged(e);
         }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Hide tabs by trapping the TCM_ADJUSTRECT message
+            if (m.Msg == 0x1328 && !DesignMode)
+                m.Result = (IntPtr)1;
+            else
+                base.WndProc(ref m);
+        }
     }
 }
