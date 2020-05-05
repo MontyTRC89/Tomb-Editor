2f2a391406e74ad4df85c311169b78fdba906288
Nickelony <nickelkwn@gmail.com>
2020-05-05 21:45:03 +0200

    Fixed broken Lua syntax highlighting.

--- TombLib.Scripting/Resources/LuaKeyWordsAndPatterns.cs
+++ TombLib.Scripting/Resources/LuaKeyWordsAndPatterns.cs
@@ -7,77 +7,86 @@ namespace TombLib.Scripting.Resources
 		public static string[] Keywords
 		{
 			get
-            {
+			{
 				return new string[]
 				{
-                    "if",
-                    "then",
-                    "else",
-                    "elseif",
-                    "function",
-                    "end",
-                    "for",
-                    "while",
-                    "in",
-                    "repeat",
-                    "until",
-                    "break",
-                    "return",
-                    "local",
-                    "print"
-                };
+					"if",
+					"then",
+					"else",
+					"elseif",
+					"function",
+					"end",
+					"for",
+					"while",
+					"in",
+					"repeat",
+					"until",
+					"break",
+					"return",
+					"local",
+					"print"
+				};
 			}
 		}
 
-        public static string[] Values
-        {
-            get
-            {
-                return new string[]
-                {
-                    "true",
-                    "false",
-                    "nil"
-                };
-            }
-        }
-
-        public static string[] Operators
-        {
+		public static string[] Values
+		{
 			get
 			{
 				return new string[]
-                {
-                    "%",
-                    @"\+",
-                    "-",
-                    @"\*",
-                    "/",
-                    "^",
-                    "==",
-                    "~=",
-                    ">",
-                    "<",
-                    "<=",
-                    ">=",
-                    "and",
-                    "or",
-                    "not",
-                    ":",
-                    @"\."
-                };
+				{
+					"true",
+					"false",
+					"nil"
+				};
+			}
+		}
+
+		public static string[] Operators
+		{
+			get
+			{
+				return new string[]
+				{
+					"%",
+					@"\+",
+					@"\-",
+					@"\*",
+					"/",
+					@"\^",
+					@"\=",
+					@"~\=",
+					@"\>",
+					@"\<",
+					@"\:",
+					@"\."
+				};
+			}
+		}
+
+		public static string[] SpecialOperators
+		{
+			get
+			{
+				return new string[]
+				{
+					"and",
+					"or",
+					"not",
+				};
 			}
 		}
 
 		#endregion Public variables
 	}
 
-    public struct LuaPatterns
-    {
-        public static string Comments { get { return @"--.*$"; } }
-        public static string Tables { get { return @"[\[\],]"; } }
-        public static string Operators { get { return @"\b(" + string.Join("|", LuaKeyWords.Operators) + @")\b\s*?="; } }
-        public static string Keywords { get { return @"\b(" + string.Join("|", LuaKeyWords.Keywords) + @")\b\s*?="; } }
-        public static string Values { get { return @"\b(" + string.Join("|", LuaKeyWords.Values) + @")\b\s*?="; } }
-    }
+	public struct LuaPatterns
+	{
+		public static string Comments { get { return @"--.*$"; } }
+		public static string Tables { get { return @"\[\]"; } }
+		public static string Operators { get { return @"(" + string.Join("|", LuaKeyWords.Operators) + @")"; } }
+		public static string SpecialOperators { get { return @"\b(" + string.Join("|", LuaKeyWords.SpecialOperators) + @")\b"; } }
+		public static string Keywords { get { return @"\b(" + string.Join("|", LuaKeyWords.Keywords) + @")\b"; } }
+		public static string Values { get { return @"\b(" + string.Join("|", LuaKeyWords.Values) + @")\b"; } }
+	}
 }
