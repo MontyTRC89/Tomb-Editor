c7ec12d5ac1fcc80b79e566578d653e7f2a47a00
Lwmte <initiator@nm.ru>
2020-05-06 12:25:22 +0300

    Update PanelRendering3D.cs

--- TombEditor/Controls/PanelRendering3D.cs
+++ TombEditor/Controls/PanelRendering3D.cs
@@ -10,7 +10,6 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TombEditor.Controls.ContextMenus;
 using TombLib;
