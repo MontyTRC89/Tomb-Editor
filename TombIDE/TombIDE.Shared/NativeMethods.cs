ed1a33c24024973483bf86c0ee815ffbb0462b10
Nickelony <nickelkwn@gmail.com>
2020-05-23 15:27:05 +0200

    Some final adjustments.

--- TombIDE/TombIDE.Shared/NativeMethods.cs
+++ TombIDE/TombIDE.Shared/NativeMethods.cs
@@ -7,6 +7,6 @@ namespace TombIDE.Shared
 	internal static class NativeMethods
 	{
 		[DllImport("shell32.dll")]
-		public static extern bool SHGetSpecialFolderPath(IntPtr hwndOwner, [Out]StringBuilder lpszPath, int nFolder, bool fCreate);
+		public static extern bool SHGetSpecialFolderPath(IntPtr hwndOwner, [Out] StringBuilder lpszPath, int nFolder, bool fCreate);
 	}
 }
