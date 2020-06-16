eddf22ddd677e6a4d88320fcf1dc10398ab9da64
Lwmte <initiator@nm.ru>
2020-06-17 00:43:35 +0300

    Attempt to fix some DarkUI controls

--- DarkUI/DarkUI/Controls/DarkTextBox.cs
+++ DarkUI/DarkUI/Controls/DarkTextBox.cs
@@ -1,28 +1,13 @@
 ﻿using DarkUI.Config;
-using System;
+using DarkUI.Win32;
 using System.ComponentModel;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace DarkUI.Controls
 {
     public class DarkTextBox : TextBox
     {
-        #region Win32 Region
-
-        const uint RDW_INVALIDATE = 0x1;
-        const uint RDW_IUPDATENOW = 0x100;
-        const uint RDW_FRAME = 0x400;
-        [DllImport("user32.dll")]
-        static extern IntPtr GetWindowDC(IntPtr hWnd);
-        [DllImport("user32.dll")]
-        static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
-        [DllImport("user32.dll")]
-        static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprc, IntPtr hrgn, uint flags);
-
-        #endregion
-
         #region Constructor Region
 
         public DarkTextBox()
@@ -79,11 +64,13 @@ namespace DarkUI.Controls
 
             if (m.Msg == 0x000F && BorderStyle == BorderStyle.FixedSingle)
             {
-                var hdc = GetWindowDC(Handle);
+                var hdc = Native.GetWindowDC(Handle);
                 using (var g = Graphics.FromHdcInternal(hdc))
                 {
                     var rect = new Rectangle(0, 0, ClientSize.Width - 1, ClientSize.Height - 1);
-                    using (var p = new Pen(Colors.GreySelection))
+                    var borderColor = Focused && TabStop ? Colors.BlueHighlight : Colors.GreySelection;
+
+                    using (var p = new Pen(borderColor))
                         g.DrawRectangle(p, rect);
                 }
             }
