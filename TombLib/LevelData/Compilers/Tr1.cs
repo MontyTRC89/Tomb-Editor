1f2852fb797910e2a885b571f92d9da673e1fb27
Lwmte <initiator@nm.ru>
2020-08-20 07:32:51 +0300

    Simplify angle packing

--- TombLib/LevelData/Compilers/Tr1.cs
+++ TombLib/LevelData/Compilers/Tr1.cs
@@ -79,7 +79,7 @@ namespace TombLib.LevelData.Compilers
 
                 // Create palette and 8-bit indexed textures
                 tr_color[] palette;
-                var textureData = PackTextureMap32To8Bit(_texture32Data, 256, predefinedPaletteColors, out palette);
+                var textureData = PackTextureMap32To8Bit(_texture32Data, predefinedPaletteColors, out palette);
 
                 // Write 8-bit textures
                 writer.Write(textureData);
