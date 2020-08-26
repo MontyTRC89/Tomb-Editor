882d1f1a8fd6709e4f7588073ef252bfbfc77e64
Lwmte <initiator@nm.ru>
2020-08-26 20:23:46 +0300

    Clear trigger list selection if selected trigger is out of scope

--- TombEditor/ToolWindows/TriggerList.cs
+++ TombEditor/ToolWindows/TriggerList.cs
@@ -108,7 +108,10 @@ namespace TombEditor.ToolWindows
                 {
                     var trigger = _editor.SelectedObject as TriggerInstance;
                     var entry = lstTriggers.Items.FirstOrDefault(t => t.Tag == trigger);
-                    if (entry != null) lstTriggers.SelectItem(lstTriggers.Items.IndexOf(entry));
+                    if (entry != null)
+                        lstTriggers.SelectItem(lstTriggers.Items.IndexOf(entry));
+                    else
+                        lstTriggers.ClearSelection();
                 }
             }
 
