588599dacbfc2e3bfd9eed73b2f1ae99273143f6
Lwmte <initiator@nm.ru>
2020-08-29 03:17:05 +0300

    Allow undoing room property changes

--- TombEditor/Forms/FormRoomProperties.cs
+++ TombEditor/Forms/FormRoomProperties.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using DarkUI.Forms;
-using TombLib.LevelData;
-using System.ComponentModel;
-using System.Linq;
 using System.Collections.Generic;
 using TombLib.Utils;
 
@@ -21,51 +18,40 @@ namespace TombEditor.Forms
 
         private void butOk_Click(object sender, EventArgs e)
         {
-            if (DarkMessageBox.Show(this, "Are you sure you want to apply selected properties to selected rooms rooms?\n" +
-                                          "This action can't be undone.",
-                                    "Apply room properties", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            var undoList = new List<UndoRedoInstance>();
+
+            var curr = _editor.SelectedRoom;
+            foreach (var r in _editor.SelectedRooms)
             {
-                var undoList = new List<UndoRedoInstance>();
+                undoList.Add(new RoomPropertyUndoInstance(_editor.UndoManager, r));
 
-                var curr = _editor.SelectedRoom;
-                foreach (var r in _editor.SelectedRooms)
-                {
-                    undoList.Add(new RoomPropertyUndoInstance(_editor.UndoManager, r));
+                // Apply selected attribs
 
-                    // Apply selected attribs
+                if (cbAmbient.Checked)       r.Properties.AmbientLight = curr.Properties.AmbientLight;
+                if (cbCold.Checked)          r.Properties.FlagCold = curr.Properties.FlagCold;
+                if (cbDamage.Checked)        r.Properties.FlagDamage = curr.Properties.FlagDamage;
+                if (cbLensflare.Checked)     r.Properties.FlagNoLensflare = curr.Properties.FlagCold;
+                if (cbLightEffect.Checked)   r.Properties.LightEffect = curr.Properties.LightEffect;
+                if (cbLightStrength.Checked) r.Properties.LightEffectStrength = curr.Properties.LightEffectStrength;
+                if (cbPathfinding.Checked)   r.Properties.FlagExcludeFromPathFinding = curr.Properties.FlagExcludeFromPathFinding;
+                if (cbPortalShade.Checked)   r.Properties.LightInterpolationMode = curr.Properties.LightInterpolationMode;
+                if (cbReverb.Checked)        r.Properties.Reverberation = curr.Properties.Reverberation;
+                if (cbRoomType.Checked)      r.Properties.Type = curr.Properties.Type;
+                if (cbRoomType.Checked)      r.Properties.TypeStrength = curr.Properties.TypeStrength;
+                if (cbSky.Checked)           r.Properties.FlagHorizon = curr.Properties.FlagHorizon;
+                if (cbTags.Checked)          r.Properties.Tags = curr.Properties.Tags;
+                if (cbVisible.Checked)       r.Properties.Hidden = curr.Properties.Hidden;
+                if (cbWind.Checked)          r.Properties.FlagOutside = curr.Properties.FlagOutside;
 
-                    if (cbAmbient.Checked)       r.Properties.AmbientLight = curr.Properties.AmbientLight;
-                    if (cbCold.Checked)          r.Properties.FlagCold = curr.Properties.FlagCold;
-                    if (cbDamage.Checked)        r.Properties.FlagDamage = curr.Properties.FlagDamage;
-                    if (cbLensflare.Checked)     r.Properties.FlagNoLensflare = curr.Properties.FlagCold;
-                    if (cbLightEffect.Checked)   r.Properties.LightEffect = curr.Properties.LightEffect;
-                    if (cbLightStrength.Checked) r.Properties.LightEffectStrength = curr.Properties.LightEffectStrength;
-                    if (cbPathfinding.Checked)   r.Properties.FlagExcludeFromPathFinding = curr.Properties.FlagExcludeFromPathFinding;
-                    if (cbPortalShade.Checked)   r.Properties.LightInterpolationMode = curr.Properties.LightInterpolationMode;
-                    if (cbReverb.Checked)        r.Properties.Reverberation = curr.Properties.Reverberation;
-                    if (cbRoomType.Checked)      r.Properties.Type = curr.Properties.Type;
-                    if (cbRoomType.Checked)      r.Properties.TypeStrength = curr.Properties.TypeStrength;
-                    if (cbSky.Checked)           r.Properties.FlagHorizon = curr.Properties.FlagHorizon;
-                    if (cbTags.Checked)          r.Properties.Tags = curr.Properties.Tags;
-                    if (cbVisible.Checked)       r.Properties.Hidden = curr.Properties.Hidden;
-                    if (cbWind.Checked)          r.Properties.FlagOutside = curr.Properties.FlagOutside;
+                // Updating operations
 
-                    // Updating operations
-
-                    if (cbAmbient.Checked)
-                    {
-                        r.BuildGeometry();
-                        r.RebuildLighting(_editor.Configuration.Rendering3D_HighQualityLightPreview);
-                    }
-
-                    _editor.RoomPropertiesChange(r);
-                }
-
-                _editor.UndoManager.Push(undoList);
-                DialogResult = DialogResult.OK;
+                if (cbAmbient.Checked)
+                    r.RebuildLighting(_editor.Configuration.Rendering3D_HighQualityLightPreview);
+                _editor.RoomPropertiesChange(r);
             }
 
-            Close();
+            _editor.UndoManager.Push(undoList);
+            DialogResult = DialogResult.OK;
         }
 
         private void butCancel_Click(object sender, EventArgs e)
