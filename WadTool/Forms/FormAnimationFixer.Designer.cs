b15213658fddf06e1036a32bf8d182f6b3af131d
Lwmte <initiator@nm.ru>
2020-09-04 14:11:12 +0300

    Update FormAnimationFixer.Designer.cs

--- WadTool/Forms/FormAnimationFixer.Designer.cs
+++ WadTool/Forms/FormAnimationFixer.Designer.cs
@@ -148,6 +148,8 @@
             this.Controls.Add(this.btCancel);
             this.Controls.Add(this.btOk);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormAnimationFixer";
             this.ShowIcon = false;
