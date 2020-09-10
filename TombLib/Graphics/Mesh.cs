462b3fa049c7150519181568fa81b8b26305d72a
Lwmte <initiator@nm.ru>
2020-09-10 18:58:36 +0300

    Properly save input layouts in mesh class

--- TombLib/Graphics/Mesh.cs
+++ TombLib/Graphics/Mesh.cs
@@ -7,6 +7,7 @@ namespace TombLib.Graphics
     public abstract class Mesh<T> : GraphicsResource, IRenderableObject where T : struct, IVertex
     {
         public Buffer<T> VertexBuffer { get; set; }
+        public VertexInputLayout VertexLayout { get; set; }
         public Buffer IndexBuffer { get; set; }
         public List<Material> Materials { get; set; }
         public List<T> Vertices { get; set; } = new List<T>();
