e26cfbaeb606af8afa99092a3ccfd1af0ae68591
Lwmte <initiator@nm.ru>
2020-09-14 18:39:04 +0300

    Release 1.3.7

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.6")]
-[assembly: AssemblyFileVersion("1.3.6")]
+[assembly: AssemblyVersion("1.3.7")]
+[assembly: AssemblyFileVersion("1.3.7")]
