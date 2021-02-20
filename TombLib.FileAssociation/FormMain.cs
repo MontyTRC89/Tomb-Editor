7b49895e5c1f9a01aedd0c9881dcd4816c9417fd
Nickelony <nickelkwn@gmail.com>
2021-02-20 11:56:28 +0100

    Added FileAssociation project.

---
+++ TombLib.FileAssociation/FormMain.cs
@@ -0,0 +1,69 @@
+﻿using DarkUI.Forms;
+using System;
+
+namespace TombLib.FileAssociation
+{
+	public partial class FormMain : DarkForm
+	{
+		private bool _wasPRJ2Checked = false;
+		private bool _wasWAD2Checked = false;
+		private bool _wasTRPROJChecked = false;
+
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			if (Association.IsPRJ2Associated())
+				checkBox_prj2.Checked = true;
+
+			if (Association.IsWAD2Associated())
+				checkBox_wad2.Checked = true;
+
+			if (Association.IsTRPROJAssociated())
+				checkBox_trproj.Checked = true;
+
+			_wasPRJ2Checked = checkBox_prj2.Checked;
+			_wasWAD2Checked = checkBox_wad2.Checked;
+			_wasTRPROJChecked = checkBox_trproj.Checked;
+
+			button_Apply.Enabled = false;
+
+			base.OnLoad(e);
+		}
+
+		private void button_Apply_Click(object sender, EventArgs e)
+		{
+			if (checkBox_prj2.Checked)
+				Association.AssociatePRJ2();
+			else
+				Association.RemoveAssociation(".prj2");
+
+			if (checkBox_wad2.Checked)
+				Association.AssociateWAD2();
+			else
+				Association.RemoveAssociation(".wad2");
+
+			if (checkBox_trproj.Checked)
+				Association.AssociateTRPROJ();
+			else
+				Association.RemoveAssociation(".trproj");
+
+			_wasPRJ2Checked = checkBox_prj2.Checked;
+			_wasWAD2Checked = checkBox_wad2.Checked;
+			_wasTRPROJChecked = checkBox_trproj.Checked;
+
+			button_Apply.Enabled = false;
+		}
+
+		private void button_Close_Click(object sender, EventArgs e)
+			=> Close();
+
+		private void checkBox_CheckedChanged(object sender, EventArgs e)
+			=> button_Apply.Enabled = checkBox_prj2.Checked != _wasPRJ2Checked
+				|| checkBox_wad2.Checked != _wasWAD2Checked
+				|| checkBox_trproj.Checked != _wasTRPROJChecked;
+	}
+}
---
+++ TombLib.FileAssociation/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TombLib File Association")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tomb Editor Team")]
+[assembly: AssemblyProduct("TombLib File Association")]
+[assembly: AssemblyCopyright("Copyright © Nickelony 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0b145643-7b29-40ed-8b52-4245136d3200")]
