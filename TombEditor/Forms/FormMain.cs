f5d8b679b083aa797745195df612123b75543f3d
Lwmte <initiator@nm.ru>
2021-03-20 05:47:11 +0300

    Display room size in status bar

--- TombEditor/Forms/FormMain.cs
+++ TombEditor/Forms/FormMain.cs
@@ -201,9 +201,10 @@ namespace TombEditor.Forms
                 else
                     statusStripSelectedRoom.Text = "Selected room: " +
                         "Name = " + room + " | " +
+                        "Size = " + room.NumXSectors + " x " + room.NumZSectors + " | " +
                         "Pos = (" + room.Position.X + ", " + room.Position.Y + ", " + room.Position.Z + ") | " +
                         "Floor = " + (room.Position.Y + room.GetLowestCorner()) + " | " +
-                         "Ceiling = " + (room.Position.Y + room.GetHighestCorner());
+                        "Ceiling = " + (room.Position.Y + room.GetHighestCorner());
             }
 
             // Update selection information of the status strip
