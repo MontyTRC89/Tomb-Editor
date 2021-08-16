9b5f161acb35198247df67151c1586e30db96293
Lwmte <initiator@nm.ru>
2021-08-16 03:42:49 +0300

    Update Undo.cs

--- TombEditor/Undo.cs
+++ TombEditor/Undo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
@@ -147,7 +146,6 @@ namespace TombEditor
         private float? RotationY = null;
         private float? RotationX = null;
         private float? Roll = null;
-        private Dictionary<int, Vector3> GroupedObjectPositions;
 
         public TransformObjectUndoInstance(EditorUndoManager parent, PositionBasedObjectInstance obj) : base(parent, obj.Room)
         {
