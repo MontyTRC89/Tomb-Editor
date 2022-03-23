bd60d4de40dd928d399dd9eea18cee36f2d0b478
MontyTRC89 <montyhammet@hotmail.it>
2022-03-23 05:45:35 +0100

    Added remapping of sound slots

--- TombLib/TombLib/LevelData/IO/Prj2TombEngineConverter.cs
+++ TombLib/TombLib/LevelData/IO/Prj2TombEngineConverter.cs
@@ -11,7 +11,7 @@ namespace TombLib.LevelData.IO
 {
     public static class Prj2TombEngineConverter
     {
-        private static string _tenReferenceWad = Path.Combine(DefaultPaths.ProgramDirectory, "Assets", "Wads", "TombEngine.wad2");
+        private static readonly string _tenReferenceWad = Path.Combine(DefaultPaths.ProgramDirectory, "Assets", "Wads", "TombEngine.wad2");
 
         public static string Start(string fileName, IWin32Window owner, IProgressReporter progressReporter)
         {
@@ -24,7 +24,7 @@ namespace TombLib.LevelData.IO
             progressReporter.ReportInfo("TombEngine Project Converter");
             progressReporter.ReportInfo(" ");
 
-            var newProject = ConvertProject(fileName, progressReporter);
+            string newProject = ConvertProject(fileName, progressReporter);
 
             if (string.IsNullOrEmpty(newProject))
             {
@@ -51,10 +51,10 @@ namespace TombLib.LevelData.IO
             try
             {
                 // Load new TombEngine reference Wad2
-                var referenceWad = Wad2Loader.LoadFromFile(_tenReferenceWad, true);
+                Wad2 referenceWad = Wad2Loader.LoadFromFile(_tenReferenceWad, true);
 
                 // Load level and all related resources
-                var level = Path.GetExtension(source).ToLower() == ".prj" ? 
+                Level level = Path.GetExtension(source).ToLower() == ".prj" ?
                     PrjLoader.LoadFromPrj(source, string.Empty, true, false, null) : Prj2Loader.LoadFromPrj2(source, null);
 
                 if (level == null)
@@ -63,35 +63,36 @@ namespace TombLib.LevelData.IO
                     return string.Empty;
                 }
 
-                if (level.Settings.GameVersion.Native() != TRVersion.Game.TR4)
+                if (level.Settings.GameVersion.Native() != TRVersion.Game.TR4 &&
+                    level.Settings.GameVersion.Native() != TRVersion.Game.TRNG)
                 {
                     progressReporter.ReportWarn("Only TR4 and TRNG projects can be converted to TEN at this time.");
                     return string.Empty;
                 }
 
                 // Now convert resources to new format
-                var newWads = new List<ReferencedWad>();
+                List<ReferencedWad> newWads = new List<ReferencedWad>();
                 Dictionary<uint, uint> remappedSlots = new Dictionary<uint, uint>();
 
                 string newFileName;
                 string newPath;
                 bool addedTimex = false;
 
-                foreach (var wadRef in level.Settings.Wads)
+                foreach (ReferencedWad wadRef in level.Settings.Wads)
                 {
-                    var wad = wadRef.Wad;
-                    var newWad = new Wad2 { GameVersion = TRVersion.Game.TombEngine };
+                    Wad2 wad = wadRef.Wad;
+                    Wad2 newWad = new Wad2 { GameVersion = TRVersion.Game.TombEngine };
 
                     // Get base remap object which is ANIMATING33 (first animating which doesn't exist in any legacy engines)
                     bool isMoveable;
-                    var remappedObjectIndex = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, "ANIMATING33", out isMoveable).Value;
+                    uint remappedObjectIndex = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, "ANIMATING33", out isMoveable).Value;
 
                     // Copy all objects to new wad
-                    foreach (var moveable in wad.Moveables)
+                    foreach (KeyValuePair<WadMoveableId, WadMoveable> moveable in wad.Moveables)
                     {
                         uint newSlot;
-                        var oldId = TrCatalog.GetMoveableName(TRVersion.Game.TR4, moveable.Key.TypeId);
-                        var newId = TrCatalog.GetMoveableTombEngineSlot(TRVersion.Game.TR4, moveable.Key.TypeId);
+                        string oldId = TrCatalog.GetMoveableName(TRVersion.Game.TR4, moveable.Key.TypeId);
+                        string newId = TrCatalog.GetMoveableTombEngineSlot(TRVersion.Game.TR4, moveable.Key.TypeId);
 
                         if (string.IsNullOrEmpty(newId))
                         {
@@ -102,13 +103,15 @@ namespace TombLib.LevelData.IO
                         }
                         else
                         {
-                            var found = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, newId, out isMoveable);
+                            uint? found = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, newId, out isMoveable);
                             if (!found.HasValue)
                             {
                                 continue;
                             }
                             else
+                            {
                                 newSlot = found.Value;
+                            }
                         }
 
                         string newSlotName = TrCatalog.GetMoveableName(TRVersion.Game.TombEngine, newSlot);
@@ -127,10 +130,10 @@ namespace TombLib.LevelData.IO
                         {
                             progressReporter.ReportInfo("    Copying mesh #14 to mesh #7 for " + newSlotName);
 
-                            var mesh = moveable.Value.Bones[14].Mesh.Clone();
+                            WadMesh mesh = moveable.Value.Bones[14].Mesh.Clone();
                             for (int i = 0; i < mesh.VertexPositions.Count; i++)
                             {
-                                var pos = mesh.VertexPositions[i];
+                                Vector3 pos = mesh.VertexPositions[i];
                                 pos.Y += 256;
                                 mesh.VertexPositions[i] = pos;
                             }
@@ -144,15 +147,19 @@ namespace TombLib.LevelData.IO
                         {
                             progressReporter.ReportInfo("    Copying holsters meshes for " + newSlotName);
 
-                            var laraMoveable = referenceWad.Moveables[new WadMoveableId(0)];
-                            var newBones = new List<WadBone>();
-                            var newMeshes = new List<WadMesh>();
+                            WadMoveable laraMoveable = referenceWad.Moveables[new WadMoveableId(0)];
+                            List<WadBone> newBones = new List<WadBone>();
+                            List<WadMesh> newMeshes = new List<WadMesh>();
 
-                            foreach (var oldBone in laraMoveable.Bones)
+                            foreach (WadBone oldBone in laraMoveable.Bones)
+                            {
                                 newBones.Add(oldBone.Clone());
+                            }
 
-                            foreach (var oldMesh in laraMoveable.Meshes)
+                            foreach (WadMesh oldMesh in laraMoveable.Meshes)
+                            {
                                 newMeshes.Add(oldMesh.Clone());
+                            }
 
                             newBones[1].Mesh = moveable.Value.Bones[4].Mesh.Clone();
                             newMeshes[1] = newBones[1].Mesh;
@@ -166,35 +173,35 @@ namespace TombLib.LevelData.IO
                             moveable.Value.Meshes.AddRange(newMeshes);
                         }
 
-                        if (newSlotName == "TWOBLOCK_PLATFORM" || 
-                            newSlotName.ToLower().Contains("falling_block") || 
+                        if (newSlotName == "TWOBLOCK_PLATFORM" ||
+                            newSlotName.ToLower().Contains("falling_block") ||
                             newSlotName.ToLower().Contains("trapdoor"))
                         {
                             progressReporter.ReportInfo("    Adjusting bridge object collision box " + newSlotName);
 
                             if (moveable.Value.Animations.Count > 0)
                             {
-                                var anim = moveable.Value.Animations[0];
+                                WadAnimation anim = moveable.Value.Animations[0];
                                 for (int f = 0; f < anim.KeyFrames.Count; f++)
                                 {
-                                    var oldBB = anim.KeyFrames[f].BoundingBox;
+                                    BoundingBox oldBB = anim.KeyFrames[f].BoundingBox;
                                     oldBB.Maximum = new Vector3(oldBB.Maximum.X, oldBB.Maximum.Y * 0.8f, oldBB.Maximum.Z);
                                     anim.KeyFrames[f].BoundingBox = oldBB;
                                 }
                             }
                         }
-                        
+
                         if (newSlotName == "EXPANDING_PLATFORM")
                         {
                             progressReporter.ReportInfo("    Adjusting mesh and pivot point for " + newSlotName);
 
                             for (int m = 0; m < moveable.Value.Bones.Count; m++)
                             {
-                                var mesh = moveable.Value.Bones[m].Mesh.Clone();
+                                WadMesh mesh = moveable.Value.Bones[m].Mesh.Clone();
 
                                 for (int i = 0; i < mesh.VertexPositions.Count; i++)
                                 {
-                                    var pos = mesh.VertexPositions[i];
+                                    Vector3 pos = mesh.VertexPositions[i];
                                     pos.Z += 512;
                                     mesh.VertexPositions[i] = pos;
                                 }
@@ -202,23 +209,29 @@ namespace TombLib.LevelData.IO
                                 moveable.Value.Meshes[m] = mesh;
                             }
 
-                            foreach (var anim in moveable.Value.Animations)
-                                foreach (var frame in anim.KeyFrames)
+                            foreach (WadAnimation anim in moveable.Value.Animations)
+                            {
+                                foreach (WadKeyFrame frame in anim.KeyFrames)
                                 {
-                                    var bb = frame.BoundingBox;
+                                    BoundingBox bb = frame.BoundingBox;
                                     bb.Maximum.Z += 512;
                                     bb.Minimum.Z += 512;
                                     frame.BoundingBox = bb;
                                 }
+                            }
                         }
 
                         if (newSlotName == "ANIMATING16")
                         {
                             progressReporter.ReportInfo("    Setting hardcoded collision for " + newSlotName);
 
-                            foreach (var anim in moveable.Value.Animations)
-                                foreach (var frame in anim.KeyFrames)
+                            foreach (WadAnimation anim in moveable.Value.Animations)
+                            {
+                                foreach (WadKeyFrame frame in anim.KeyFrames)
+                                {
                                     frame.BoundingBox = new BoundingBox();
+                                }
+                            }
                         }
 
                         if (!addedTimex &&
@@ -227,7 +240,7 @@ namespace TombLib.LevelData.IO
                         {
                             progressReporter.ReportInfo("    Adding TIMEX from reference Wad2");
 
-                            var timexIndex = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, "TIMEX_ITEM", out isMoveable).Value;
+                            uint timexIndex = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, "TIMEX_ITEM", out isMoveable).Value;
                             newWad.Add(new WadMoveableId(timexIndex), referenceWad.Moveables[new WadMoveableId(timexIndex)]);
                             addedTimex = true;
                         }
@@ -240,23 +253,29 @@ namespace TombLib.LevelData.IO
                         {
                             newWad.Add(new WadMoveableId(newSlot), moveable.Value);
                             if (!addedTimex && newSlotName == "TIMEX_ITEM")
+                            {
                                 addedTimex = true;
+                            }
                         }
 
                         if (!remappedSlots.ContainsKey(moveable.Key.TypeId))
+                        {
                             remappedSlots.Add(moveable.Key.TypeId, newSlot);
+                        }
                     }
 
                     // Copy all statics
-                    foreach (var staticModel in wad.Statics)
+                    foreach (KeyValuePair<WadStaticId, WadStatic> staticModel in wad.Statics)
+                    {
                         newWad.Statics.Add(staticModel.Key, staticModel.Value);
+                    }
 
                     // Copy all sprite sequences
-                    foreach (var sequence in wad.SpriteSequences)
+                    foreach (KeyValuePair<WadSpriteSequenceId, WadSpriteSequence> sequence in wad.SpriteSequences)
                     {
                         uint newSlot;
-                        var oldId = TrCatalog.GetMoveableName(TRVersion.Game.TR4, sequence.Key.TypeId);
-                        var newId = TrCatalog.GetMoveableTombEngineSlot(TRVersion.Game.TR4, sequence.Key.TypeId);
+                        string oldId = TrCatalog.GetMoveableName(TRVersion.Game.TR4, sequence.Key.TypeId);
+                        string newId = TrCatalog.GetMoveableTombEngineSlot(TRVersion.Game.TR4, sequence.Key.TypeId);
 
                         if (string.IsNullOrEmpty(newId))
                         {
@@ -265,13 +284,15 @@ namespace TombLib.LevelData.IO
                         }
                         else
                         {
-                            var found = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, newId, out isMoveable);
+                            uint? found = TrCatalog.GetItemIndex(TRVersion.Game.TombEngine, newId, out isMoveable);
                             if (!found.HasValue)
                             {
                                 continue;
                             }
                             else
+                            {
                                 newSlot = found.Value;
+                            }
                         }
 
                         string newSlotName = TrCatalog.GetSpriteSequenceName(TRVersion.Game.TombEngine, newSlot);
@@ -283,7 +304,7 @@ namespace TombLib.LevelData.IO
                             progressReporter.ReportInfo("    DEFAULT_SPRITES found, adding new sprites slots from a reference Wad2");
 
                             // Open TombEngine sprites Wad2
-                            foreach (var spr in referenceWad.SpriteSequences)
+                            foreach (KeyValuePair<WadSpriteSequenceId, WadSpriteSequence> spr in referenceWad.SpriteSequences)
                             {
                                 if (!newWad.Contains(spr.Key))
                                 {
@@ -320,16 +341,26 @@ namespace TombLib.LevelData.IO
                 level.Settings.Wads.Clear();
                 level.Settings.Wads.AddRange(newWads);
 
-                foreach (var room in level.Rooms)
+                foreach (Room room in level.Rooms)
+                {
                     if (room != null)
-                        foreach (var instance in room.Objects)
+                    {
+                        foreach (PositionBasedObjectInstance instance in room.Objects)
                         {
                             if (instance is MoveableInstance)
+                            {
                                 if (!remappedSlots.ContainsKey(((MoveableInstance)instance).WadObjectId.TypeId))
+                                {
                                     progressReporter.ReportWarn("Slot not found! " + ((MoveableInstance)instance).WadObjectId.TypeId);
+                                }
                                 else
+                                {
                                     ((MoveableInstance)instance).WadObjectId = new WadMoveableId(remappedSlots[((MoveableInstance)instance).WadObjectId.TypeId]);
+                                }
+                            }
                         }
+                    }
+                }
 
                 level.Settings.GameVersion = TRVersion.Game.TombEngine;
 
