80436e7b6309a59968880e07741a7150b91e8e85
Nickelony <k.kupilas@outlook.com>
2022-05-07 19:05:12 +0200

    (TIDE) Minor adjustment.

--- TombIDE/TombIDE.ProjectMaster/Forms/FormGameArchive.Designer.cs
+++ TombIDE/TombIDE.ProjectMaster/Forms/FormGameArchive.Designer.cs
@@ -42,7 +42,7 @@
 			this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel.Name = "tableLayoutPanel";
 			this.tableLayoutPanel.RowCount = 4;
-			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
 			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
 			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
@@ -69,7 +69,7 @@
 			this.label_Info.Location = new System.Drawing.Point(20, 20);
 			this.label_Info.Margin = new System.Windows.Forms.Padding(20, 20, 20, 0);
 			this.label_Info.Name = "label_Info";
-			this.label_Info.Size = new System.Drawing.Size(744, 80);
+			this.label_Info.Size = new System.Drawing.Size(744, 100);
 			this.label_Info.TabIndex = 3;
 			this.label_Info.Text = resources.GetString("label_Info.Text");
 			// 
@@ -79,11 +79,11 @@
 			this.panel_TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.panel_TextBox.Controls.Add(this.richTextBox);
 			this.panel_TextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel_TextBox.Location = new System.Drawing.Point(20, 130);
+			this.panel_TextBox.Location = new System.Drawing.Point(20, 150);
 			this.panel_TextBox.Margin = new System.Windows.Forms.Padding(20, 0, 20, 0);
 			this.panel_TextBox.Name = "panel_TextBox";
 			this.panel_TextBox.Padding = new System.Windows.Forms.Padding(3);
-			this.panel_TextBox.Size = new System.Drawing.Size(744, 361);
+			this.panel_TextBox.Size = new System.Drawing.Size(744, 341);
 			this.panel_TextBox.TabIndex = 1;
 			// 
 			// richTextBox
@@ -96,7 +96,7 @@
 			this.richTextBox.Location = new System.Drawing.Point(3, 3);
 			this.richTextBox.Margin = new System.Windows.Forms.Padding(20, 0, 20, 0);
 			this.richTextBox.Name = "richTextBox";
-			this.richTextBox.Size = new System.Drawing.Size(736, 353);
+			this.richTextBox.Size = new System.Drawing.Size(736, 333);
 			this.richTextBox.TabIndex = 0;
 			this.richTextBox.Text = "";
 			// 
@@ -105,7 +105,7 @@
 			this.label_Enter.AutoSize = true;
 			this.label_Enter.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.label_Enter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-			this.label_Enter.Location = new System.Drawing.Point(20, 100);
+			this.label_Enter.Location = new System.Drawing.Point(20, 120);
 			this.label_Enter.Margin = new System.Windows.Forms.Padding(20, 0, 20, 0);
 			this.label_Enter.Name = "label_Enter";
 			this.label_Enter.Size = new System.Drawing.Size(744, 30);
@@ -124,7 +124,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(784, 561);
 			this.Controls.Add(this.tableLayoutPanel);
-			this.MinimumSize = new System.Drawing.Size(640, 480);
+			this.MinimumSize = new System.Drawing.Size(740, 480);
 			this.Name = "FormGameArchive";
 			this.ShowIcon = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
