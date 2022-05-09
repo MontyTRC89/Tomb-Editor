7fde62b4c6577fb6bd591f53d21ed4cb7e07f1ac
Nickelony <k.kupilas@outlook.com>
2022-05-09 21:41:57 +0200

    (TIDE) Fixed directory missing exception.

--- TombIDE/TombIDE.ProjectMaster/Sections/SectionPluginList.cs
+++ TombIDE/TombIDE.ProjectMaster/Sections/SectionPluginList.cs
@@ -37,7 +37,8 @@ namespace TombIDE.ProjectMaster
 			string pluginsPath = Path.Combine(_ide.Project.ProjectPath, "Plugins");
 			_pluginsDirectory = new DirectoryInfo(pluginsPath);
 
-			if (!_pluginsDirectory.Exists || !_pluginsDirectory.EnumerateFileSystemInfos().Any())
+			if (Directory.Exists(DefaultPaths.TRNGPluginsDirectory) &&
+				(!_pluginsDirectory.Exists || !_pluginsDirectory.EnumerateFileSystemInfos().Any()))
 				CopyPluginsFromTIDEToProject();
 
 			UpdatePlugins();
