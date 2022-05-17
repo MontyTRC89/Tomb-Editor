2eda7cf85fd4609afc1f4821a1a88be02ea003df
Nickelony <k.kupilas@outlook.com>
2022-05-17 11:52:54 +0200

    (TIDE) #FIRST_ID now does something.

--- TombLib/TombLib.Scripting.ClassicScript/Parsers/GlobalParser.cs
+++ TombLib/TombLib.Scripting.ClassicScript/Parsers/GlobalParser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using TombLib.Scripting.Helpers;
 
 namespace TombLib.Scripting.ClassicScript.Parsers
@@ -23,7 +24,7 @@ namespace TombLib.Scripting.ClassicScript.Parsers
 				"AddEffect", "ColorRGB", "GlobalTrigger", "Image", "ItemGroup", "MultiEnvCondition",
 				"Organizer", "Parameters", "TestPosition", "TriggerGroup", "Plugin"))
 			{
-				IEnumerable<int> takenIndicesList;
+				int loopStartLine = 1;
 
 				if (DocumentParser.DocumentContainsSections(document))
 				{
@@ -32,13 +33,13 @@ namespace TombLib.Scripting.ClassicScript.Parsers
 					if (sectionStartLine == null)
 						return -1;
 
-					int sectionStartLineNumber = sectionStartLine.LineNumber;
-					takenIndicesList = GetTakenIndicesList(document, commandKey, sectionStartLineNumber + 1);
+					loopStartLine = sectionStartLine.LineNumber + 1;
 				}
-				else
-					takenIndicesList = GetTakenIndicesList(document, commandKey, 1);
 
-				int nextFreeIndex = 1;
+				int firstId = GetFirstId(document, commandKey, loopStartLine);
+				IEnumerable<int> takenIndicesList = GetTakenIndicesList(document, commandKey, loopStartLine);
+
+				int nextFreeIndex = firstId;
 
 				while (takenIndicesList.Contains(nextFreeIndex))
 					nextFreeIndex++;
@@ -47,6 +48,83 @@ namespace TombLib.Scripting.ClassicScript.Parsers
 			}
 
 			return -1;
+		}
+
+		private static int GetFirstId(TextDocument document, string commandKey, int loopStartLine) // REFACTOR !!!
+		{
+			try // Not sure if this will work correctly, so putting this into a try / catch for safety
+			{
+				var firstIdRegex = new Regex($@"^\s*#FIRST_ID\s+{commandKey}\s*=\s*(.*)\s*(;.*)?$", RegexOptions.IgnoreCase);
+
+				for (int i = loopStartLine; i <= document.LineCount; i++)
+				{
+					string processedLineText = CommandParser.GetWholeCommandLineText(document, document.GetLineByNumber(i).Offset);
+
+					if (processedLineText == null)
+						continue;
+
+					processedLineText = LineParser.EscapeComments(processedLineText);
+
+					Match match = firstIdRegex.Match(processedLineText);
+
+					if (match.Success)
+					{
+						string[] numbers = match.Groups[1].Value.Split(new[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+						int finalResult = 0;
+
+						char[] mathSymbols = match.Groups[1].Value.Where(c => c == '+' || c == '-').ToArray();
+
+						for (int j = 0; j < numbers.Length; j++)
+						{
+							char equationType = '+';
+
+							if (j > 0)
+								equationType = mathSymbols[j - 1];
+
+							string number = numbers[j];
+
+							if (int.TryParse(number.Trim(), out int value))
+							{
+								switch (equationType)
+								{
+									case '+': finalResult += value; break;
+									case '-': finalResult -= value; break;
+								}
+							}
+							else
+							{
+								var defineRegex = new Regex($@"^\s*#DEFINE\s+(.*)\s+(\d+)\s*(;.*)?$", RegexOptions.IgnoreCase);
+
+								foreach (DocumentLine line in document.Lines)
+								{
+									string lineText = CommandParser.GetWholeCommandLineText(document, line.Offset);
+
+									if (lineText == null)
+										continue;
+
+									lineText = LineParser.EscapeComments(lineText);
+
+									Match defineMatch = defineRegex.Match(lineText);
+
+									if (defineMatch.Success && int.TryParse(defineMatch.Groups[2].Value.Trim(), out int defineValue))
+									{
+										switch (equationType)
+										{
+											case '+': finalResult += defineValue; break;
+											case '-': finalResult -= defineValue; break;
+										}
+									}
+								}
+							}
+						}
+
+						return finalResult == 0 ? 1 : finalResult;
+					}
+				}
+			}
+			catch { }
+
+			return 1;
 		}
 
 		private static IEnumerable<int> GetTakenIndicesList(TextDocument document, string commandKey, int loopStartLine)
