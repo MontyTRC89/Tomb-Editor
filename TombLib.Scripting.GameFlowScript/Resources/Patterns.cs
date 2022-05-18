f6bfdeb312ea2c04e7eb96608605356008bccad1
Nickelony <k.kupilas@outlook.com>
2022-05-18 11:58:11 +0200

    Restore RegEx consistency.

--- TombLib.Scripting.GameFlowScript/Resources/Patterns.cs
+++ TombLib.Scripting.GameFlowScript/Resources/Patterns.cs
@@ -7,7 +7,7 @@
 		public static string SpecialProperties => @"^\b(" + string.Join("|", Keywords.SpecialProperties) + @")\b:";
 		public static string Properties => @"^\s*\b(" + string.Join("|", Keywords.Properties) + @")\b:";
 		public static string Constants => @"\b(" + string.Join("|", Keywords.Constants) + @")\b";
-		public static string Values => "\\d|\\w|\"|'|\\.|\\\\|/";
+		public static string Values => "\\d|\\w|\"|'|\\.|\\\\";
 
 		public static string LevelProperty => @"^\bLEVEL:\s*";
 	}
