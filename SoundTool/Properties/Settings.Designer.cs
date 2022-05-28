e0437e159934445734a2dad2145f709dbe460cc4
Nickelony <k.kupilas@outlook.com>
2022-05-28 19:39:37 +0200

    Removed unused solution files.

--- SoundTool/Properties/Settings.Designer.cs
+++ SoundTool/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SoundTool.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -22,17 +22,14 @@ namespace SoundTool.Properties {
                 return defaultInstance;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        public global::System.Collections.Generic.List<System.String> RecentProjects
-        {
-            get
-            {
+        public global::System.Collections.Generic.List<System.String> RecentProjects {
+            get {
                 return ((global::System.Collections.Generic.List<System.String>)(this["RecentProjects"]));
             }
-            set
-            {
+            set {
                 this["RecentProjects"] = value;
             }
         }
