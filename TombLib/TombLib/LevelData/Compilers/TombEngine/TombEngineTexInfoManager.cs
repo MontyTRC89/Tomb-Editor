4df0ef0087794102d5f9da209365ffc7fbc0d5fd
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-07-23 23:28:54 +0300

    Fix compile error

--- TombLib/TombLib/LevelData/Compilers/TombEngine/TombEngineTexInfoManager.cs
+++ TombLib/TombLib/LevelData/Compilers/TombEngine/TombEngineTexInfoManager.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
