2f3ab2fc0f3adf85684117ada4419863ea11ba11
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-08-04 19:49:41 +0300

    Remove some code which was causing warnings

--- TombEditor/Forms/FormReplaceObject.Designer.cs
+++ TombEditor/Forms/FormReplaceObject.Designer.cs
@@ -2,11 +2,6 @@
 {
     partial class FormReplaceObject
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
         #region Windows Form Designer generated code
 
         /// <summary>
