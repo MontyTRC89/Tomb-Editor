fdf3c9e8c8f379cac7a1c3fd2f95df32f18f6bb2
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-08-07 03:03:54 +0300

    Display framework version in about screen

--- TombLib/TombLib.Forms/Forms/FormAbout.cs
+++ TombLib/TombLib.Forms/Forms/FormAbout.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
 using System.Windows.Forms;
 using DarkUI.Config;
 using DarkUI.Forms;
@@ -20,9 +23,15 @@ namespace TombLib.Forms
 
         private void PictureBox_Paint(object sender, PaintEventArgs e)
         {
+            var assembly = Assembly.GetExecutingAssembly()
+                           .GetCustomAttributes(false).OfType<TargetFrameworkAttribute>()
+                           .First().FrameworkDisplayName;
+
             var font = new Font(Font.FontFamily, 10.0f);
             using (var b = new SolidBrush(Colors.LightText))
-                e.Graphics.DrawString("Version " + Application.ProductVersion, font, b, pictureBox.Width, pictureBox.Height, new StringFormat { Alignment = StringAlignment.Far, LineAlignment = StringAlignment.Far });
+                e.Graphics.DrawString("Version " + Application.ProductVersion + " (" + assembly + ")", 
+                font, b, pictureBox.Width, pictureBox.Height,
+                new StringFormat { Alignment = StringAlignment.Far, LineAlignment = StringAlignment.Far });
         }
 
         private void btnLink_Click(object sender, EventArgs e)
