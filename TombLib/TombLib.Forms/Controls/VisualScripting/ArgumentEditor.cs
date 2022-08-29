74313bba4a9642f2c6757e7bcd69db951ca742ed
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-08-29 20:33:38 +0300

    Always include activator for moveable enums

--- TombLib/TombLib.Forms/Controls/VisualScripting/ArgumentEditor.cs
+++ TombLib/TombLib.Forms/Controls/VisualScripting/ArgumentEditor.cs
@@ -34,6 +34,7 @@ namespace TombLib.Controls.VisualScripting
         private const string _separatorChar = ",";
         private const string _quoteChar = "\"";
         private const string _objectIDLuaPrefix = "Objects.ObjID.";
+        private const string _activatorPrefix = "activator:GetName()";
 
         private ArgumentType _argumentType = ArgumentType.Numerical;
 
@@ -107,6 +108,7 @@ namespace TombLib.Controls.VisualScripting
                         cbList.Items.Add(new ComboBoxItem(item));
                     break;
                 case ArgumentType.Moveables:
+                    cbList.Items.Add(new ComboBoxItem("Volume activator", _activatorPrefix));
                     foreach (var item in editor.CachedMoveables)
                         cbList.Items.Add(new ComboBoxItem(item));
                     break;
