62aafb3bac64b36aca64cb8eef32c8d2354c3c6f
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-09-06 01:22:45 +0300

    Update Default.lua

--- TombLib/TombLib/Catalogs/TEN Node Scripting/Default.lua
+++ TombLib/TombLib/Catalogs/TEN Node Scripting/Default.lua
@@ -70,7 +70,7 @@ end
 -- !Name "Flash screen"
 -- !Section "Effects"
 -- !Description "Flashes screen with specified color and for specified duration.\nDuration value of 1 takes 1 second to flash."
--- !Arguments "Color, 10, Flash colour" "Numerical, 20, [ 0.1 | 10 | 2 ], Flash speed" 
+-- !Arguments "Color, 10, Flash colour" "Numerical, 20, [ 0.1 | 10 | 2 | 0.1 | 0.5 ], Flash speed" 
 
 LevelFuncs.FlashScreen = function(color, duration)
     Effects.FlashScreen(color, duration)
