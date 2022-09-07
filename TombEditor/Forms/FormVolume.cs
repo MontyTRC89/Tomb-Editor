f52cef3ecfe2f292cd3385ff64b15fc14b9e6bb9
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-09-07 12:05:38 +0300

    Fix strange TryGetValue bug by doing a check in advance

--- TombEditor/Forms/FormVolume.cs
+++ TombEditor/Forms/FormVolume.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using DarkUI.Forms;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using DarkUI.Forms;
 using TombLib.Forms;
 using TombLib.LevelData;
 using TombLib.Utils;
@@ -135,6 +135,9 @@ namespace TombEditor.Forms
 
             foreach (var vol in volumes)
             {
+                if (!_backupVolumes.ContainsKey(vol))
+                    continue;
+
                 int index = -1;
                 var entry = _backupVolumes.TryGetValue(vol, out index);
                 if (index >= 0)
@@ -223,12 +226,12 @@ namespace TombEditor.Forms
 
         private void UpdateUI()
         {
-            tbName.Enabled = 
-            grpActivators.Enabled = 
-            tcEvents.Enabled = 
+            tbName.Enabled =
+            grpActivators.Enabled =
+            tcEvents.Enabled =
             butUnassignEventSet.Enabled = _instance.EventSet != null;
 
-            butCloneEventSet.Enabled = 
+            butCloneEventSet.Enabled =
             butDeleteEventSet.Enabled = lstEvents.SelectedItem != null;
 
             butSearch.Enabled = lstEvents.Items.Count > 0;
@@ -263,8 +266,8 @@ namespace TombEditor.Forms
 
         private void butNewEventSet_Click(object sender, EventArgs e)
         {
-            var newSet = new VolumeEventSet() 
-            { 
+            var newSet = new VolumeEventSet()
+            {
                 Name = "New event set " + lstEvents.Items.Count,
                 LastUsedEventIndex = _editor.Configuration.NodeEditor_DefaultEventToEdit
             };
