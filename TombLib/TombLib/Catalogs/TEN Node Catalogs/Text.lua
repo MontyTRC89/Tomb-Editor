3488e8c7c688483908ee768ea9644f272b7de9c5
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-09-08 17:52:59 +0300

    Add DrawTextForTimespan

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Text.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Text.lua
@@ -1,6 +1,6 @@
 ﻿-- !Name "Draw text"
 -- !Section "Text"
--- !Description "Draws simple label on a screen."
+-- !Description "Draws simple label on a screen for one frame."
 -- !Arguments "NewLine, String, Text"
 -- !Arguments "NewLine, Numerical, 20, X position, [ 0 | 100 | 0 ]" "Numerical, 20, Y position, [ 0 | 100 | 0 ]" 
 -- !Arguments "Boolean, 20, Centered" "Boolean, 20, Shadow" "Color, 20, Text color"
@@ -9,4 +9,18 @@ LevelFuncs.DrawText = function(text, x, y, center, shadow, color)
 
 	local string = LevelFuncs.GenerateString(text, x, y, center, shadow, color)
 	TEN.Strings.ShowString(string, 1 / 30)
+end
+
+-- !Name "Draw text for a time span"
+-- !Section "Text"
+-- !Description "Draws simple label on a screen for a specified time span."
+-- !Arguments "Numerical, 20, [ 0 | 180 | 2 | 0.25 | 1 ], Time span in seconds"
+-- !Arguments "NewLine, String, Text"
+-- !Arguments "NewLine, Numerical, 20, X position, [ 0 | 100 | 0 ]" "Numerical, 20, Y position, [ 0 | 100 | 0 ]" 
+-- !Arguments "Boolean, 20, Centered" "Boolean, 20, Shadow" "Color, 20, Text color"
+
+LevelFuncs.DrawTextForTimespan = function(time, text, x, y, center, shadow, color)
+
+	local string = LevelFuncs.GenerateString(text, x, y, center, shadow, color)
+	TEN.Strings.ShowString(string, time)
 end
