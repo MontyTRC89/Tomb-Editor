4ad84eb8f4b1825eb86d5c23c1e373ef487cfc60
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-09-12 19:00:01 +0300

    Move hit status check to separate file

---
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Creatures.lua
@@ -0,0 +1,9 @@
+﻿-- !Name "If creature was hit with weapon..."
+-- !Section "Creature state"
+-- !Description "Checks if creature was shot with any weapon."
+-- !Conditional "True"
+-- !Arguments "NewLine, Moveables"
+
+LevelFuncs.Engine.Node.TestCreatureHitStatus = function(creatureName)
+	return TEN.Objects.GetMoveableByName(creatureName):GetHitStatus()
+end
