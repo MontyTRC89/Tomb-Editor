43e52e0cc66c9cf18e3a7f41bdfd06bdbf3dd69e
Lwmte <3331699+Lwmte@users.noreply.github.com>
2022-09-13 18:13:16 +0300

    Add game variable node functions

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Variables.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Variables.lua
@@ -14,13 +14,13 @@ end
 
 -- !Name "Modify or create level variable"
 -- !Section "Variables"
--- !Description "Modify variable, according to specified operator and operand."
--- !Description "If variable with specified name does not exist,\nit is initialized as 0 before performing modify operation."
+-- !Description "Modify level variable, according to specified operator and operand."
+-- !Description "If level variable with specified name does not exist,\nit is initialized as 0 before performing modify operation."
 -- !Arguments "NewLine, String, 50, [ NoMultiline ]" "Enumeration, 25, [ + | - | * | / ]" "Numerical, 25, [ -65536 | 65535 | 2 ]"
 
 LevelFuncs.Engine.Node.ModifyLevelVariable = function(varName, operator, operand)
     if (LevelVars[varName] == nil) then
-        print("Variable " .. varName .. " did not exist and was initialized as 0.")
+        print("Level variable " .. varName .. " did not exist and was initialized as 0.")
         LevelVars[varName] = 0
     end
 
@@ -36,6 +36,48 @@ LevelFuncs.Engine.Node.DeleteLevelVariable = function(varName, operator, operand
     if (LevelVars[varName] ~= nil) then
         LevelVars[varName] = nil
     else
-        print("Variable " .. varName .. " did not exist and was not deleted.")
+        print("Level variable " .. varName .. " did not exist and was not deleted.")
+    end
+end
+
+-- !Name "If game variable is..."
+-- !Section "Variables"
+-- !Description "Checks if specified game variable complies to specified compare function."
+-- !Conditional "True"
+-- !Arguments "NewLine, String, 50, [ NoMultiline ]" "CompareOperator, 25" "Numerical, 25"
+
+LevelFuncs.Engine.Node.TestGameVariable = function(varName, operator, value)
+    if (GameVars[varName] == nil) then
+        return false
+    else
+        return LevelFuncs.Engine.Node.CompareValue(GameVars[varName], value, operator)
+    end
+end
+
+-- !Name "Modify or create game variable"
+-- !Section "Variables"
+-- !Description "Modify game variable, according to specified operator and operand."
+-- !Description "If game variable with specified name does not exist,\nit is initialized as 0 before performing modify operation."
+-- !Arguments "NewLine, String, 50, [ NoMultiline ]" "Enumeration, 25, [ + | - | * | / ]" "Numerical, 25, [ -65536 | 65535 | 2 ]"
+
+LevelFuncs.Engine.Node.ModifyGameVariable = function(varName, operator, operand)
+    if (GameVars[varName] == nil) then
+        print("Game variable " .. varName .. " did not exist and was initialized as 0.")
+        GameVars[varName] = 0
+    end
+
+    GameVars[varName] = LevelFuncs.Engine.Node.ModifyValue(operand, GameVars[varName], operator)
+end
+
+-- !Name "Delete game variable"
+-- !Section "Variables"
+-- !Description "Delete game variable, if it exists."
+-- !Arguments "NewLine, String, 100, [ NoMultiline ]"
+
+LevelFuncs.Engine.Node.DeleteGameVariable = function(varName, operator, operand)
+    if (GameVars[varName] ~= nil) then
+        GameVars[varName] = nil
+    else
+        print("Game variable " .. varName .. " did not exist and was not deleted.")
     end
 end
