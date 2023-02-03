7e7b3813dd652a490840d41324c3cc159fdc91f9
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2023-02-03 14:20:05 +0000

    Split nodes

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Particles.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Particles.lua
@@ -29,24 +29,24 @@ LevelFuncs.Engine.Node.MoveableParticleEmitter = function(
     poison
 )
 
-    local pos = GetMoveableByName(moveable):GetPosition()
-    local blendMode = LevelFuncs.Engine.Node.GetBlendMode(blendID)
+		local pos = GetMoveableByName(moveable):GetPosition()
+		local blendMode = LevelFuncs.Engine.Node.GetBlendMode(blendID)
 
-    TEN.Effects.EmitParticle(
-        pos, 
-        velocity, 
-        spriteID, 
-        gravity, 
-        rotation, 
-        startColor, 
-        endColor, 
-        blendMode, 
-        startSize, 
-        endSize, 
-        life, 
-        damage, 
-        poison
-    )
+		TEN.Effects.EmitParticle(
+			pos, 
+			velocity, 
+			spriteID, 
+			gravity, 
+			rotation, 
+			startColor, 
+			endColor, 
+			blendMode, 
+			startSize, 
+			endSize, 
+			life, 
+			damage, 
+			poison
+		)
 
 end
 
@@ -54,8 +54,7 @@ end
 --!Section "Particles"
 --!Conditional "False"
 --!Description "Emit particles from a specified mesh.\nThis effect is drawn every frame."
---!Arguments "NewLine, Moveables, 70, Choose the moveable where the particles will spawn from." 
---!Arguments "Numerical, 30, [0 | 100], Mesh Number.\nThis can be found in Wadtool in the Animation Editor"
+--!Arguments "NewLine, Moveables, 70, Choose the moveable where the particles will spawn from." "Numerical, 30, [0 | 100], Mesh Number.\nThis can be found in Wadtool in the Animation Editor"
 --!Arguments "NewLine, Vector3, 100, [-32000 | 32000], Velocity X Y Z\n Please note that the Y value is inversed. Positive values are down and negative is up"
 --!Arguments "NewLine, Numerical, 100, [0 | 31], Choose sprite for emitter.\nSprites are based on the DEFAULT_SPRITES slot in Wadtool\n0 = Flame Emitter\n1 = Underwater blood\n2 = Waterfall\n3 = Mist\n4 = Splash Ring 1\n5 = Splash Ring 2\n6 = Splash Ring 3\n7 = Splash Ring 4\n8 = Water Splash\n9 = Water Ring\n11 = Specular\n13 = Underwater Bubble\n14 = Underwater Dust\n15 = Blood\n28 = Lightning\n29 = Lensflare Ring\n30 = Lensflare Ring 2 \n31 = Lensflare Sundisc\n32 = Lensflare Bright Spark"
 --!Arguments "NewLine, 100, [-32768 |  32767], Gravity X Y\n Please note that the Y value is inversed. Positive values are down and negative is up"
@@ -67,44 +66,44 @@ end
 --!Arguments "NewLine, 100, [-32000 | 32000], Lifetime (in seconds)"
 --!Arguments "NewLine, Boolean, 50, Add damage?" , "Boolean, 50, Add poison?"
 
-LevelFuncs.Engine.Node.MeshParticleEmitter=function(
-	activator, 
-	meshnum, 
-	velocity, 
-	spriteID, 
-	gravity, 
-	rotation, 
-	startColor, 
-	endColor, 
-	blendID, 
-	startSize, 
-	endSize, 
-	life, 
-	damage, 
-	poison
-	)
-
-    local pos = GetMoveableByName(activator):GetJointPosition(meshnum)
-	local blendmode = LevelFuncs.Engine.Node.GetBlendMode(blendID)
-
-
-    TEN.Effects.EmitParticle(
-		pos, 
+	LevelFuncs.Engine.Node.MeshParticleEmitter=function(
+		activator, 
+		meshnum, 
 		velocity, 
 		spriteID, 
 		gravity, 
 		rotation, 
 		startColor, 
 		endColor, 
-		blendmode, 
+		blendID, 
 		startSize, 
 		endSize, 
 		life, 
 		damage, 
 		poison
-    )
-	
-end
+		)
+
+		local pos = GetMoveableByName(activator):GetJointPosition(meshnum)
+		local blendmode = LevelFuncs.Engine.Node.GetBlendMode(blendID)
+
+
+		TEN.Effects.EmitParticle(
+			pos, 
+			velocity, 
+			spriteID, 
+			gravity, 
+			rotation, 
+			startColor, 
+			endColor, 
+			blendmode, 
+			startSize, 
+			endSize, 
+			life, 
+			damage, 
+			poison
+		)
+		
+	end
 
 --!Name "Emit lightning arc"
 --!Section "Particles"
@@ -112,7 +111,7 @@ end
 --!Description "Emit a lightning arc between two points in 3D space"
 --!Arguments "Newline, Moveables, 50, Source Position", "Moveables, 50, Destination Position"
 --!Arguments "NewLine, Color, 100, Color of Lightning Effect"
---!Arguments "Newline, Number, 25, [0 | 4.2 | 1], Lifetime in seconds." , "Number, 25, [1 | 255 | 0], Amplitude (strength) of effect", "Number, 25, [1 | 127 | 0], Beam Width", "Number, 25, [1 | 127 | 0], Detail of effect\n 1 is 1 segment between the two points for example."
+--!Arguments "Newline, Number, 25, [ 0 | 4.2 | 1 ], Lifetime in seconds." , "Number, 25, [ 1 | 255 | 0 ], Amplitude (strength) of effect", "Number, 25, [ 1 | 127 | 0 ], Beam Width", "Number, 25, [ 1 | 127 | 0 ], Detail of effect\n 1 is 1 segment between the two points for example."
 --!Arguments "Newline, Boolean, 50, Toggle smooth effect.", "Boolean, 50, Toggle end drift."
 --!Arguments "NewLine, Boolean, 30, Add source light" , "Boolean, 40, Add destination light" , "Boolean, 30, Toggle SFX"
 
@@ -170,118 +169,52 @@ end
 --!Section "Particles"
 --!Description "Emit a shockwave effect from a moveable's origin point."
 --!Arguments "NewLine, Moveables, Choose origin point of shockwave"
---!Arguments "NewLine, Number, 50, [0|10400|0], Inner Radius" "Number, 50, [0|10400|0], Outer Radius."
+--!Arguments "NewLine, Number, 50, [ 0 | 10400 | 0 ], Inner Radius" "Number, 50, [0|10400|0], Outer Radius."
 --!Arguments "NewLine, Color, 100, Color of shockwave effect"
---!Arguments "NewLine, Numerical, 33, [0|8.5|1], Lifetime of effect (in seconds)" , "Number, 33, [0|500|0], Speed" , "Number, 33, [-360|360|0], Angle around the X axis"
+--!Arguments "NewLine, Numerical, 33, [ 0 | 8.5 | 1 ], Lifetime of effect (in seconds)" , "Number, 33, [ 0 | 500 | 0 ], Speed" , "Number, 33, [ -360 | 360 |0 ], Angle around the X axis"
 --!Arguments "NewLine, Boolean, 30, Toogle Lara Damage" , "Boolean, 33, Toggle Rumble", "Boolean, 33, Toggle SFX"
 
-LevelFuncs.Engine.Node.Shockwave = function(
-	pos, 
-	innerRadius, 
-	outerRadius, 
-	color, 
-	lifetime, 
-	speed, 
-	angle, 
-	hurtPlayer, 
-	rumble, 
-	sound
-	)
-
-		local origin = GetMoveableByName(pos):GetPosition()
-		
-		local function earthquake()
-			TEN.Effects.MakeEarthquake(20)
-			TEN.Misc.Vibrate(20, 0.3)
-		end
-
-		TEN.Effects.EmitShockwave(
-		origin, 
+	LevelFuncs.Engine.Node.Shockwave = function(
+		pos, 
 		innerRadius, 
 		outerRadius, 
 		color, 
 		lifetime, 
 		speed, 
 		angle, 
-		hurtPlayer
+		hurtPlayer, 
+		rumble, 
+		sound
 		)
 
-		local Rumblecheck
-		if rumble == true
-		then 
-			earthquake()
-		end
+			local origin = GetMoveableByName(pos):GetPosition()
+			
+			local function earthquake()
+				TEN.Effects.MakeEarthquake(20)
+				TEN.Misc.Vibrate(20, 0.3)
+			end
 
-		local sfxcheck
-		if sound == true
-		then
-			TEN.Misc.PlaySound(197, startingPoint)
-		end
+			TEN.Effects.EmitShockwave(
+			origin, 
+			innerRadius, 
+			outerRadius, 
+			color, 
+			lifetime, 
+			speed, 
+			angle, 
+			hurtPlayer
+			)
 
-end
+			local Rumblecheck
+			if rumble == true
+			then 
+				earthquake()
+			end
 
--- !Name "Add dynamic light to moveable's mesh."
--- !Section "Dynamic Light"
--- !Conditional "False"
--- !Description "Add A Dynamic Light To A Moveable's mesh. (Version 1.0)"
--- !Arguments "NewLine, Moveables, 70, Select Moveable To Attach Light To" , "Numerical, [0|100|0] , 30, Select Mesh Number of Moveable \n This can be found in the Animation Editor within Wadtool."
--- !Arguments "NewLine, Color, 70", "Numerical, [0|100|0] , 30, Select Range of Light \n(Range is in blocks from origin"
--- !Arguments "NewLine, Boolean, Add a random effect to the light? "
+			local sfxcheck
+			if sound == true
+			then
+				TEN.Misc.PlaySound(197, startingPoint)
+			end
 
-LevelFuncs.Engine.Node.DynamicLightMesh = function(
-	moveable,
-	meshnumber,
-	lightcolor,
-	range,
-	randomcheck
-	
-	)
-
-		local moveable = GetMoveableByName(moveable)
-		local moveablemeshpos = moveable:GetJointPosition(meshnumber)
-		
-		if randomcheck == false
-		then 
-			TEN.Effects.EmitLight(moveablemeshpos, lightcolor, Range)
-		else 
-			TEN.Effects.EmitLight(moveablemeshpos, lightcolor, math.random((Range)/2, Range))
-		end
-
-		
-	
-end
-
--- !Name "Add Dynamic Light to Static"
--- !Section "Dynamic Light"
--- !Conditional "False"
--- !Description "Add A Dynamic Light To A Static Object. (Version 1.0)"
--- !Arguments "NewLine, Statics, Select Static To Attach Light To" , 
--- !Arguments "NewLine, Color, 70", "Numerical, 30, [0], Select Range of Light \n(Range is in blocks from origin"
--- !Arguments "NewLine, Boolean, Add Sound Effect, 30" , "SoundEffects, 70, Choose sound to play"
--- !Arguments "NewLine, Boolean, Add a Random Effect to the light? "
-
-LevelFuncs.Engine.Node.DynamicLightStaticMesh = function(
-	static,
-	lightcolor,
-	range,
-	randomcheck,
-	soundeffectcheck,
-	soundeffect
-	)
-
-		local static = GetStaticByName(static)
-		local staticpos = static:GetPosition()
-	
-		if randomcheck == false
-		then
-			TEN.Effects.EmitLight(staticpos, lightcolor,range)
-		else
-			TEN.Effects.EmitLight(staticpos, LightColor, math.random(range))
-		end
-		
-		if soundeffectcheck == true
-		then
-			TEN.Misc.PlaySound(soundeffect, staticpos)
-		end
-end
-
+	end
