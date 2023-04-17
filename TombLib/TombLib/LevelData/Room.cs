a47446a9b593bbe2c4b87be7e5a3da6e289a6e1a
Lwmte <3331699+Lwmte@users.noreply.github.com>
2023-04-17 10:56:22 +0300

    Fix script ID assignment when duplicating rooms from the context menu or via hotkey command

--- TombLib/TombLib/LevelData/Room.cs
+++ TombLib/TombLib/LevelData/Room.cs
@@ -301,7 +301,12 @@ namespace TombLib.LevelData
                 result._objects = new List<PositionBasedObjectInstance>();
                 foreach (var instance in AnyObjects)
                     if (decideToCopy(instance))
-                        result.AddObjectAndSingularPortal(level, instance.Clone());
+                    {
+                        var copy = instance.Clone();
+                        result.AddObjectAndSingularPortal(level, copy);
+                        if (copy is IHasScriptID)
+                            (copy as IHasScriptID).AllocateNewScriptId();
+                    }
             }
 
             return result;
