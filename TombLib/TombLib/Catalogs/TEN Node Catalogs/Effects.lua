2ee12be1ab186b14eab35c1f12d40dd6d569d5db
Lwmte <3331699+Lwmte@users.noreply.github.com>
2023-06-17 19:28:02 +0200

    Squashed commit of the following:
    commit b1a7b53a5e8b8d4c7f580cee9b64d815f4ee9553
    Merge: c18db985 9a962082
    Author: Lwmte <3331699+Lwmte@users.noreply.github.com>
    Date:   Fri Jun 16 23:21:56 2023 +0200
    
        Merge branch 'master' into sound_enhancements
    
    commit c18db9858f455f2ec7495e4714726d275dff10cb
    Author: Lwmte <3331699+Lwmte@users.noreply.github.com>
    Date:   Fri Jun 16 00:02:03 2023 +0200
    
        Update Text.lua
    
    commit 2ce2d1fbb1858eb24cef0c546383163cd5d7ece6
    Author: Lwmte <3331699+Lwmte@users.noreply.github.com>
    Date:   Thu Jun 15 23:07:16 2023 +0200
    
        Add draw subtitle node function
    
    commit 29ec12360dfc1dd9cb4f66a7fc11893b8b599453
    Merge: 666253db 500dc389
    Author: Lwmte <3331699+Lwmte@users.noreply.github.com>
    Date:   Thu Jun 15 19:40:17 2023 +0200
    
        Merge branch 'master' into sound_enhancements
    
    commit 666253db21294465a802602d53498201f2ca86a8
    Author: Lwmte <3331699+Lwmte@users.noreply.github.com>
    Date:   Thu Jun 15 19:39:56 2023 +0200
    
        Add new sound node functions

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Effects.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Effects.lua
@@ -1,4 +1,4 @@
--- !Name "Check if fade out is complete"
+-- !Name "If fade out is complete..."
 -- !Section "Effects"
 -- !Conditional "True"
 -- !Description "Check if fade out was finished and screen is totally black."
@@ -81,10 +81,32 @@ LevelFuncs.Engine.Node.Vibrate = function(strength, time)
     TEN.Misc.Vibrate(strength, time)
 end
 
+-- !Name "If audio track is playing..."
+-- !Section "Effects"
+-- !Conditional "True"
+-- !Description "Checks if specified audio track is playing."
+-- !Arguments "NewLine, SoundTracks, Name of the audiotrack to check"
+
+LevelFuncs.Engine.Node.AudioTrackIsPlaying = function(name)
+	return TEN.Misc.IsAudioTrackPlaying(name)
+end
+
+-- !Name "If audio track loudness is..."
+-- !Section "Effects"
+-- !Conditional "True"
+-- !Description "Checks specified audio track loudness."
+-- !Arguments "NewLine, CompareOperator, 33, Compare operation" "Numerical, 33, [ 0 | 1 | 2 | 0.05 | 0.1 ], Loudness"
+-- !Arguments "Enumeration, 34, [ One shot | Looped | Voice ]"
+
+LevelFuncs.Engine.Node.CheckAudioTrackLoudness = function(operator, value, mode)
+	return LevelFuncs.Engine.Node.CompareValue(TEN.Misc.GetAudioTrackLoudness(LevelFuncs.Engine.Node.GetSoundTrackType(mode)), value, operator)
+end
+
 -- !Name "Play audio track"
 -- !Section "Effects"
 -- !Description "Plays specified audio track."
--- !Arguments "NewLine, 82, SoundTracks, Name of the audiotrack to play" "Boolean, 18, Looped"
+-- !Arguments "NewLine, 75, SoundTracks, Name of the audiotrack to play"
+-- !Arguments "Enumeration, 25, [ One shot | Looped | Voice ]"
 
 LevelFuncs.Engine.Node.PlayAudioTrack = function(name, looped)
 	TEN.Misc.PlayAudioTrack(name, looped)
@@ -93,15 +115,15 @@ end
 -- !Name "Stop audio track"
 -- !Section "Effects"
 -- !Description "Stops audio track of the specified mode."
--- !Arguments "Boolean, 18, Looped"
+-- !Arguments "Enumeration, 25, [ One shot | Looped | Voice ]"
 
 LevelFuncs.Engine.Node.StopAudioTrack = function(looped)
 	TEN.Misc.StopAudioTrack(looped)
 end
 
--- !Name "Stop both audio tracks"
+-- !Name "Stop all audio tracks"
 -- !Section "Effects"
--- !Description "Stops audio tracks of both looped and one-shot types."
+-- !Description "Stops audio tracks of all types."
 
 LevelFuncs.Engine.Node.StopAudioTracks = function()
 	TEN.Misc.StopAudioTracks()
