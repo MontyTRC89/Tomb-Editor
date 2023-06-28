6e95099d85bc198c779465ec61cb91df20f672df
Kewin Kupilas <k.kupilas@outlook.com>
2023-06-28 16:43:30 +0100

    Demo progress report

---
+++ DarkUI.WPF.Demo/OriginalComparision.cs
@@ -0,0 +1,63 @@
+﻿using DarkUI.Controls;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DarkUI.WPF.Demo
+{
+	public partial class OriginalComparison : UserControl
+	{
+		public OriginalComparison()
+		{
+			InitializeComponent();
+
+			darkButton12.NotifyDefault(true);
+			darkTextBox3.BackColor = Color.FromArgb(60, 63, 65);
+
+			darkComboBox1.SelectedIndex = 0;
+			darkComboBox2.SelectedIndex = 0;
+
+			// darkSearchableComboBox1.SelectedIndex = 0;
+			// darkSearchableComboBox2.SelectedIndex = 0;
+
+			var root1 = new DarkTreeNode("Root 1");
+			var root2 = new DarkTreeNode("Root 2");
+			var longRoot = new DarkTreeNode("Very long item name which exceeds the width of the TreeView");
+
+			root1.Nodes.Add(new DarkTreeNode("Child 1"));
+			root1.Nodes.Add(new DarkTreeNode("Child 2"));
+			root1.Nodes.Add(new DarkTreeNode("Child 3"));
+
+			root2.Nodes.Add(new DarkTreeNode("Child 1"));
+			root2.Nodes.Add(new DarkTreeNode("Child 2"));
+			root2.Nodes.Add(new DarkTreeNode("Child 3"));
+
+			darkTreeView1.Nodes.Add(root1);
+			darkTreeView1.Nodes.Add(root2);
+			darkTreeView1.Nodes.Add(longRoot);
+
+			darkDataGridView1.Font = new Font("Segoe UI", 8F);
+		}
+
+		private void darkCheckBox8_CheckedChanged(object sender, System.EventArgs e)
+		{
+
+		}
+
+		private void darkCheckBox9_CheckedChanged(object sender, System.EventArgs e)
+		{
+		}
+
+		private void darkCheckBox11_CheckedChanged(object sender, System.EventArgs e)
+		{
+		}
+
+		private void darkCheckBox12_CheckedChanged(object sender, System.EventArgs e)
+		{
+		}
+
+		private void darkLabel5_Click(object sender, System.EventArgs e)
+		{
+
+		}
+	}
+}
