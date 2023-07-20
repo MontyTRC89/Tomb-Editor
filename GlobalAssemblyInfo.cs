f5aae652117ec884392010e778798b39c1b23904
Kubsy <kubadd475@gmail.com>
2023-07-20 11:45:08 +0100

    Update GlobalAssemblyInfo.cs

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.6.7")]
-[assembly: AssemblyFileVersion("1.6.7")]
+[assembly: AssemblyVersion("1.6.8")]
+[assembly: AssemblyFileVersion("1.6.8")]
