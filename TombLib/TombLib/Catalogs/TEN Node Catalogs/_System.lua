c0569ef8675112a8217a46ae4780f3964c3d5d4d
davidmarr <116632612+davidmarr@users.noreply.github.com>
2023-10-20 15:13:04 +0200

    correct namespace for nodes
    correct namespace for nodes

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/_System.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/_System.lua
@@ -2,30 +2,26 @@ LevelFuncs.Engine.Node = {}
 
 -- Helper function for value comparisons. Any function which uses
 -- CompareOperator arguments should use this helper function for comparison.
-
 LevelFuncs.Engine.Node.CompareValue = function(operand, reference, operator)
-
 	local result = false
-	
+
 	-- Fix Lua-specific treatment of bools as non-numerical values
 	if (operand == false) then operand = 0 end;
-	if (operand == true)  then operand = 1 end;
+	if (operand == true) then operand = 1 end;
 	if (reference == false) then reference = 0 end;
-	if (reference == true)  then reference = 1 end;
+	if (reference == true) then reference = 1 end;
 
 	if (operator == 0 and operand == reference) then result = true end
 	if (operator == 1 and operand ~= reference) then result = true end
-	if (operator == 2 and operand <  reference) then result = true end
+	if (operator == 2 and operand < reference) then result = true end
 	if (operator == 3 and operand <= reference) then result = true end
-	if (operator == 4 and operand >  reference) then result = true end
-	if (operator == 5 and operand >= reference) then result = true end	
+	if (operator == 4 and operand > reference) then result = true end
+	if (operator == 5 and operand >= reference) then result = true end
 	return result
 end
 
 -- Helper function for value modification.
-
 LevelFuncs.Engine.Node.ModifyValue = function(operand, reference, operator)
-
 	local result = reference
 	if (operator == 0) then result = reference + operand end
 	if (operator == 1) then result = reference - operand end
@@ -36,10 +32,8 @@ LevelFuncs.Engine.Node.ModifyValue = function(operand, reference, operator)
 end
 
 -- Helper function for easy generation of a display string with all parameters set.
-
 LevelFuncs.Engine.Node.GenerateString = function(text, x, y, center, shadow, color)
-
-	local options = { }
+	local options = {}
 	if (shadow == true) then table.insert(options, TEN.Strings.DisplayStringOption.SHADOW) end
 	if (center == true) then table.insert(options, TEN.Strings.DisplayStringOption.CENTER) end
 	local rX, rY = TEN.Misc.PercentToScreen(x, y)
@@ -47,25 +41,21 @@ LevelFuncs.Engine.Node.GenerateString = function(text, x, y, center, shadow, col
 end
 
 -- Helper function to split string using specified delimiter.
-
 LevelFuncs.Engine.Node.SplitString = function(inputStr, delimiter)
+	if inputStr == nil then
+		inputStr = "%s"
+	end
 
-   if inputStr == nil then
-      inputStr = "%s"
-   end
-   
-   local t = {}
-   for str in string.gmatch(inputStr, "([^"..delimiter.."]+)") do
-      table.insert(t, str)
-   end
-   
-   return t
+	local t = {}
+	for str in string.gmatch(inputStr, "([^" .. delimiter .. "]+)") do
+		table.insert(t, str)
+	end
+
+	return t
 end
 
 -- Wrap angle value around 360
-
 LevelFuncs.Engine.Node.WrapRotation = function(source, value)
-
 	if (value == 0) then
 		return source
 	end
@@ -80,51 +70,83 @@ LevelFuncs.Engine.Node.WrapRotation = function(source, value)
 end
 
 -- Convert UI enum to room flag ID enum
-
 LevelFuncs.Engine.Node.GetRoomFlag = function(value)
-
-	if (value == 0) then return Objects.RoomFlagID.WATER end
-	if (value == 1) then return Objects.RoomFlagID.QUICKSAND end
-	if (value == 2) then return Objects.RoomFlagID.SKYBOX end
-	if (value == 3) then return Objects.RoomFlagID.WIND end
-	if (value == 4) then return Objects.RoomFlagID.COLD end
-	if (value == 5) then return Objects.RoomFlagID.DAMAGE end
-	if (value == 6) then return Objects.RoomFlagID.NOLENSFLARE end
-
-	return Objects.RoomFlagID.WATER
+	local RoomFlagID =
+	{
+		[0] = Objects.RoomFlagID.WATER,
+		[1] = Objects.RoomFlagID.QUICKSAND,
+		[2] = Objects.RoomFlagID.SKYBOX,
+		[3] = Objects.RoomFlagID.WIND,
+		[4] = Objects.RoomFlagID.COLD,
+		[5] = Objects.RoomFlagID.DAMAGE,
+		[6] = Objects.RoomFlagID.NOLENSFLARE,
+	}
+	return RoomFlagID[value]
 end
 
 LevelFuncs.Engine.Node.GetSoundTrackType = function(value)
-
-	if (value == 0) then return Misc.SoundTrackType.ONESHOT end
-	if (value == 1) then return Misc.SoundTrackType.LOOPED end
-	if (value == 2) then return Misc.SoundTrackType.VOICE end
-
-	return Misc.SoundTrackType.ONESHOT
+	local SoundTrackType =
+	{
+		[0] = Misc.SoundTrackType.ONESHOT,
+		[1] = Misc.SoundTrackType.LOOPED,
+		[2] = Misc.SoundTrackType.VOICE,
+	}
+	return SoundTrackType[value]
 end
 
 LevelFuncs.Engine.Node.GetBlendMode = function(index)
+	local blendID =
+	{
+		[0] = TEN.Effects.BlendID.OPAQUE,
+		[1] = TEN.Effects.BlendID.ALPHATEST,
+		[2] = TEN.Effects.BlendID.ADDITIVE,
+		[3] = TEN.Effects.BlendID.SUBTRACTIVE,
+		[4] = TEN.Effects.BlendID.EXCLUDE,
+		[5] = TEN.Effects.BlendID.SCREEN,
+		[6] = TEN.Effects.BlendID.LIGHTEN,
+		[7] = TEN.Effects.BlendID.ALPHABLEND
+	}
+	return blendID[index]
+end
 
-	local blendID = TEN.Effects.BlendID.OPAQUE
+LevelFuncs.Engine.Node.GetDisplaySpriteAlignMode = function(index)
+	local displaySpriteAlignMode =
+	{
+		[0] = TEN.DisplaySpriteEnum.AlignMode.CENTER,
+		[1] = TEN.DisplaySpriteEnum.AlignMode.CENTER_TOP,
+		[2] = TEN.DisplaySpriteEnum.AlignMode.CENTER_BOTTOM,
+		[3] = TEN.DisplaySpriteEnum.AlignMode.CENTER_LEFT,
+		[4] = TEN.DisplaySpriteEnum.AlignMode.CENTER_RIGHT,
+		[5] = TEN.DisplaySpriteEnum.AlignMode.TOP_LEFT,
+		[6] = TEN.DisplaySpriteEnum.AlignMode.TOP_RIGHT,
+		[7] = TEN.DisplaySpriteEnum.AlignMode.BOTTOM_LEFT,
+		[8] = TEN.DisplaySpriteEnum.AlignMode.BOTTOM_RIGHT
+	}
+	return displaySpriteAlignMode
+end
 
-	if (index == 0) then
-		blendID = TEN.Effects.BlendID.OPAQUE
-	elseif (index == 1) then
-		blendID = TEN.Effects.BlendID.ALPHATEST
-	elseif (index == 2) then
-		blendID = TEN.Effects.BlendID.ADDITIVE
-	elseif (index == 3) then
-		blendID = TEN.Effects.BlendID.SUBTRACTIVE
-	elseif (index == 4) then
-		blendID = TEN.Effects.BlendID.EXCLUDE
-	elseif (index == 5) then
-		blendID = TEN.Effects.BlendID.SCREEN
-	elseif (index == 6) then
-		blendID = TEN.Effects.BlendID.LIGHTEN
-	elseif (index == 7) then
-		blendID = TEN.Effects.BlendID.ALPHABLEND 
-	end
+LevelFuncs.Engine.Node.GetDisplaySpriteScaleMode = function(index)
+	local displaySpriteScaleMode =
+	{
+		[0] = TEN.DisplaySpriteEnum.ScaleMode.FIT,
+		[1] = TEN.DisplaySpriteEnum.ScaleMode.FILL,
+		[2] = TEN.DisplaySpriteEnum.ScaleMode.STRETCH
+	}
+	return displaySpriteScaleMode
+end
 
-	return blendID
-	
+-- Helper function for choosing sprite slots
+LevelFuncs.Engine.Node.GetSpriteSlot = function(index)
+	local spriteSlot =
+	{
+		[0] = TEN.Objects.ObjID.DEFAULT_SPRITES,
+		[1] = TEN.Objects.ObjID.MISC_SPRITES,
+		[2] = TEN.Objects.ObjID.CUSTOM_SPRITES,
+		[3] = TEN.Objects.ObjID.FIRE_SPRITES,
+		[4] = TEN.Objects.ObjID.SMOKE_SPRITES,
+		[5] = TEN.Objects.ObjID.SPARK_SPRITE,
+		[6] = TEN.Objects.ObjID.DRIP_SPRITE,
+		[7] = TEN.Objects.ObjID.EXPLOSION_SPRITES
+	}
+	return spriteSlot[index]
 end
