82e04b84a8a917c267cdf7d60403f09c6f3fee04
davidmarr <116632612+davidmarr@users.noreply.github.com>
2023-11-22 23:02:12 +0100

    Update FormVolume.cs

--- TombEditor/Forms/FormVolume.cs
+++ TombEditor/Forms/FormVolume.cs
@@ -243,8 +243,8 @@ namespace TombEditor.Forms
                     toolTip.SetToolTip(cbEvents, "Occurs when exiting to title. \nThis event performs once.");
                     break;
 
-                case VolumeEventType.OnLaraDeath:
-                    toolTip.SetToolTip(cbEvents, "Occurs when the level finishes upon Lara's death. \nThis event performs once.");
+                case VolumeEventType.OnPlayerDeath:
+                    toolTip.SetToolTip(cbEvents, "Occurs when the level finishes upon Player's death. \nThis event performs once.");
                     break;
             }
         }
@@ -290,6 +290,9 @@ namespace TombEditor.Forms
 
             foreach (VolumeEventType eventType in Enum.GetValues(typeof(VolumeEventType)))
             {
+                if ((_genericMode && !_genericVolume) && eventType < VolumeEventType.OnLoop)
+                    continue;
+
                 if ((!_genericMode || _genericVolume) && eventType > VolumeEventType.OnVolumeLeave)
                     break;
 
@@ -386,7 +389,12 @@ namespace TombEditor.Forms
             int selectedEvent = Math.Min((int)_instance.EventSet.LastUsedEvent, (int)cbEvents.Items.Count - 1);
 
             cbEvents.SelectedIndex = selectedEvent;
-            triggerManager.Event = _instance.EventSet.Events[(VolumeEventType)selectedEvent];
+
+            if (_genericMode && !_genericVolume)
+                triggerManager.Event = _instance.EventSet.Events[(VolumeEventType)selectedEvent + 3];
+
+            if (!_genericMode || _genericVolume)
+                triggerManager.Event = _instance.EventSet.Events[(VolumeEventType)selectedEvent];
 
             tbName.Text = _instance.EventSet.Name;
 
@@ -573,9 +581,9 @@ namespace TombEditor.Forms
                 _instance.EventSet.LastUsedEvent = (VolumeEventType)cbEvents.SelectedIndex;
                 triggerManager.Event = _instance.EventSet.Events[_instance.EventSet.LastUsedEvent];
             }
-            if (_genericMode && !_genericVolume)
-                if (cbEvents.SelectedIndex < 3)
-                    cbEvents.SelectedIndex = 3;
+            //if (_genericMode && !_genericVolume)
+            //    if (cbEvents.SelectedIndex < 3)
+            //        cbEvents.SelectedIndex = 3;
         }
 
         private void tbName_Validated(object sender, EventArgs e)
