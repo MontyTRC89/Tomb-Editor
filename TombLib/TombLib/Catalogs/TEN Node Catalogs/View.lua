ad917ab576b5d363f1f83119df05ac31c03b36e3
Lwmte <3331699+Lwmte@users.noreply.github.com>
2023-12-25 15:18:02 +0100

    Update View.lua

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/View.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/View.lua
@@ -1,6 +1,6 @@
--- !Name "Set display color mode"
+-- !Name "Set display postprocessing mode"
 -- !Section "View"
--- !Description "Set the color mode for all graphics"
+-- !Description "Set the postprocessing mode for all graphics, excluding GUI"
 -- !Arguments "NewLine, Enumeration, 50, [ None | Monochrome | Negative | Exclusion ], Sets the color mode"
 -- !Arguments "Color, 25, Set the tint color that overlays over the chosen color mode"
 -- !Arguments "Numerical, 25, [ 0 | 1 | 1 | 0.1 | 0.5 ], Color overlay strength"
