876fabb8d17f2f6ca588bcb92a94c7300810219e
Kewin Kupilas <k.kupilas@outlook.com>
2024-01-28 13:33:39 +0000

    Fix slope detection

--- TombLib/TombLib/LevelData/Block.cs
+++ TombLib/TombLib/LevelData/Block.cs
@@ -1185,10 +1185,10 @@ namespace TombLib.LevelData
         {
             Plane[] tri = new Plane[2];
 
-            var p0 = new Vector3(0, Floor.XnZp, 0);
-            var p1 = new Vector3(4, Floor.XpZp, 0);
-            var p2 = new Vector3(4, Floor.XpZn, -4);
-            var p3 = new Vector3(0, Floor.XnZn, -4);
+            var p0 = new Vector3(0, Floor.XnZp / Level.FullClickHeight, 0);
+            var p1 = new Vector3(4, Floor.XpZp / Level.FullClickHeight, 0);
+            var p2 = new Vector3(4, Floor.XpZn / Level.FullClickHeight, -4);
+            var p3 = new Vector3(0, Floor.XnZn / Level.FullClickHeight, -4);
 
             // Create planes based on floor split direction
 
