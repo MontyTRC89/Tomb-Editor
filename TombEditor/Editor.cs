63847f3734a2b9100011232eda24ae6149b2ff6a
Lwmte <3331699+Lwmte@users.noreply.github.com>
2024-02-04 11:48:29 +0100

    Update Editor.cs

--- TombEditor/Editor.cs
+++ TombEditor/Editor.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using TombLib;
 using TombLib.Forms;
 using TombLib.LevelData;
