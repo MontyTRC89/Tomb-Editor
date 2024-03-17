29b3d223b60339eaf3a1457c6adf393eea7e8025
Kewin Kupilas <k.kupilas@outlook.com>
2024-03-17 23:29:16 +0000

    (TIDE) Add splash screen customization dialog

---
+++ TombIDE/TombIDE.ProjectMaster/Forms/FormAdjustFrame.cs
@@ -0,0 +1,279 @@
+﻿using DarkUI.Forms;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+using TombIDE.ProjectMaster.Splash;
+using TombIDE.Shared;
+
+namespace TombIDE.ProjectMaster.Forms;
+
+public partial class FormAdjustFrame : DarkForm
+{
+	private readonly IDE _ide;
+	private readonly string _configPath;
+	private readonly SplashConfiguration _config;
+
+	private readonly ColorDialog _colorDialog = new() { FullOpen = true };
+
+	public FormAdjustFrame(IDE ide)
+	{
+		_ide = ide;
+		_configPath = Path.Combine(_ide.Project.GetEngineRootDirectoryPath(), "splash.xml");
+		_config = new SplashConfiguration().Load(_configPath);
+
+		InitializeComponent();
+
+		LoadConfiguration();
+	}
+
+	protected override void OnClosed(EventArgs e)
+	{
+		_config.Save(_configPath);
+		base.OnClosed(e);
+	}
+
+	private void LoadConfiguration()
+	{
+		panel_Top_StartColor.BackColor = ColorTranslator.FromHtml(_config.TopBar_GradientStartColor);
+		panel_Top_EndColor.BackColor = ColorTranslator.FromHtml(_config.TopBar_GradientEndColor);
+		panel_Bottom_StartColor.BackColor = ColorTranslator.FromHtml(_config.BottomBar_GradientStartColor);
+		panel_Bottom_EndColor.BackColor = ColorTranslator.FromHtml(_config.BottomBar_GradientEndColor);
+		panel_FontColor.BackColor = ColorTranslator.FromHtml(_config.FontColor);
+
+		comboBox_Top_GradientFlow.SelectedIndex = (int)_config.TopBar_GradientFlow;
+		comboBox_Bottom_GradientFlow.SelectedIndex = (int)_config.BottomBar_GradientFlow;
+
+		numUpDown_Top_StartAlpha.Value = _config.TopBar_GradientStartAlpha;
+		numUpDown_Top_EndAlpha.Value = _config.TopBar_GradientEndAlpha;
+		numUpDown_Bottom_StartAlpha.Value = _config.BottomBar_GradientStartAlpha;
+		numUpDown_Bottom_EndAlpha.Value = _config.BottomBar_GradientEndAlpha;
+
+		comboBox_WindowAccent.SelectedIndex = _config.WindowAccent switch
+		{
+			ACCENT.ENABLE_ACRYLICBLURBEHIND => 1,
+			_ => 0
+		};
+
+		numUpDown_DisplayTime.Value = _config.DisplayTimeMilliseconds;
+
+		label_Message.ForeColor = ColorTranslator.FromHtml(_config.FontColor);
+	}
+
+	private void panel_Top_Paint(object sender, PaintEventArgs e)
+	{
+		Point start = GetGradientStartPoint(_config.TopBar_GradientFlow, panel_Top.ClientRectangle.Size);
+		Point end = GetGradientEndPoint(_config.TopBar_GradientFlow, panel_Top.ClientRectangle.Size);
+
+		Color startColor = ColorTranslator.FromHtml(_config.TopBar_GradientStartColor);
+		Color endColor = ColorTranslator.FromHtml(_config.TopBar_GradientEndColor);
+		startColor = Color.FromArgb(_config.TopBar_GradientStartAlpha, startColor.R, startColor.G, startColor.B);
+		endColor = Color.FromArgb(_config.TopBar_GradientEndAlpha, endColor.R, endColor.G, endColor.B);
+
+		var brush = new LinearGradientBrush(start, end, startColor, endColor);
+		e.Graphics.FillRectangle(brush, 0, 0, panel_Top.Width, panel_Top.Height);
+	}
+
+	private void panel_Bottom_Paint(object sender, PaintEventArgs e)
+	{
+		Point start = GetGradientStartPoint(_config.BottomBar_GradientFlow, panel_Bottom.ClientRectangle.Size);
+		Point end = GetGradientEndPoint(_config.BottomBar_GradientFlow, panel_Bottom.ClientRectangle.Size);
+
+		Color startColor = ColorTranslator.FromHtml(_config.BottomBar_GradientStartColor);
+		Color endColor = ColorTranslator.FromHtml(_config.BottomBar_GradientEndColor);
+		startColor = Color.FromArgb(_config.BottomBar_GradientStartAlpha, startColor.R, startColor.G, startColor.B);
+		endColor = Color.FromArgb(_config.BottomBar_GradientEndAlpha, endColor.R, endColor.G, endColor.B);
+
+		var brush = new LinearGradientBrush(start, end, startColor, endColor);
+		e.Graphics.FillRectangle(brush, 0, 0, panel_Bottom.Width, panel_Bottom.Height);
+	}
+
+	private Point GetGradientStartPoint(GradientFlow flow, Size rectSize) => flow switch
+	{
+		GradientFlow.RightToLeft => new Point(rectSize.Width, 0),
+		GradientFlow.BottomToTop => new Point(0, rectSize.Height),
+		_ => Point.Empty,
+	};
+
+	private Point GetGradientEndPoint(GradientFlow flow, Size rectSize) => flow switch
+	{
+		GradientFlow.LeftToRight => new Point(rectSize.Width, 0),
+		GradientFlow.TopToBottom => new Point(0, rectSize.Height),
+		_ => Point.Empty,
+	};
+
+	private void panel_Top_StartColor_Click(object sender, EventArgs e)
+	{
+		DialogResult result = _colorDialog.ShowDialog(this);
+
+		if (result is DialogResult.OK)
+		{
+			panel_Top_StartColor.BackColor = _colorDialog.Color;
+			_config.TopBar_GradientStartColor = ColorTranslator.ToHtml(_colorDialog.Color);
+		}
+
+		panel_Top.Invalidate();
+	}
+
+	private void panel_Top_EndColor_Click(object sender, EventArgs e)
+	{
+		DialogResult result = _colorDialog.ShowDialog(this);
+
+		if (result is DialogResult.OK)
+		{
+			panel_Top_EndColor.BackColor = _colorDialog.Color;
+			_config.TopBar_GradientEndColor = ColorTranslator.ToHtml(_colorDialog.Color);
+		}
+
+		panel_Top.Invalidate();
+	}
+
+	private void panel_Bottom_StartColor_Click(object sender, EventArgs e)
+	{
+		DialogResult result = _colorDialog.ShowDialog(this);
+
+		if (result is DialogResult.OK)
+		{
+			panel_Bottom_StartColor.BackColor = _colorDialog.Color;
+			_config.BottomBar_GradientStartColor = ColorTranslator.ToHtml(_colorDialog.Color);
+		}
+
+		panel_Bottom.Invalidate();
+	}
+
+	private void panel_Bottom_EndColor_Click(object sender, EventArgs e)
+	{
+		DialogResult result = _colorDialog.ShowDialog(this);
+
+		if (result is DialogResult.OK)
+		{
+			panel_Bottom_EndColor.BackColor = _colorDialog.Color;
+			_config.BottomBar_GradientEndColor = ColorTranslator.ToHtml(_colorDialog.Color);
+		}
+
+		panel_Bottom.Invalidate();
+	}
+
+	private void comboBox_Top_GradientFlow_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		_config.TopBar_GradientFlow = (GradientFlow)comboBox_Top_GradientFlow.SelectedIndex;
+		panel_Top.Invalidate();
+	}
+
+	private void comboBox_Bottom_GradientFlow_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		_config.BottomBar_GradientFlow = (GradientFlow)comboBox_Bottom_GradientFlow.SelectedIndex;
+		panel_Bottom.Invalidate();
+	}
+
+	private void numUpDown_Top_StartAlpha_ValueChanged(object sender, EventArgs e)
+	{
+		_config.TopBar_GradientStartAlpha = (int)numUpDown_Top_StartAlpha.Value;
+		panel_Top.Invalidate();
+	}
+
+	private void numUpDown_Top_EndAlpha_ValueChanged(object sender, EventArgs e)
+	{
+		_config.TopBar_GradientEndAlpha = (int)numUpDown_Top_EndAlpha.Value;
+		panel_Top.Invalidate();
+	}
+
+	private void numUpDown_Bottom_StartAlpha_ValueChanged(object sender, EventArgs e)
+	{
+		_config.BottomBar_GradientStartAlpha = (int)numUpDown_Bottom_StartAlpha.Value;
+		panel_Bottom.Invalidate();
+	}
+
+	private void numUpDown_Bottom_EndAlpha_ValueChanged(object sender, EventArgs e)
+	{
+		_config.BottomBar_GradientEndAlpha = (int)numUpDown_Bottom_EndAlpha.Value;
+		panel_Bottom.Invalidate();
+	}
+
+	private void panel_FontColor_Click(object sender, EventArgs e)
+	{
+		DialogResult result = _colorDialog.ShowDialog(this);
+
+		if (result is DialogResult.OK)
+		{
+			panel_FontColor.BackColor = _colorDialog.Color;
+			label_Message.ForeColor = _colorDialog.Color;
+
+			_config.FontColor = ColorTranslator.ToHtml(_colorDialog.Color);
+		}
+	}
+
+	private void comboBox_WindowAccent_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		_config.WindowAccent = comboBox_WindowAccent.SelectedIndex switch
+		{
+			1 => ACCENT.ENABLE_ACRYLICBLURBEHIND,
+			_ => ACCENT.DISABLED
+		};
+	}
+
+	private void numUpDown_DisplayTime_ValueChanged(object sender, EventArgs e)
+	{
+		_config.DisplayTimeMilliseconds = (int)numUpDown_DisplayTime.Value;
+	}
+
+	private void button_RestoreDefaults_Click(object sender, EventArgs e)
+	{
+		DialogResult result = DarkMessageBox.Show(this,
+			"Are you sure you want to restore default frames?", "Are you sure?",
+			MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+		if (result != DialogResult.Yes)
+			return;
+
+		var defaultConfig = new SplashConfiguration();
+
+		_config.TopBar_GradientFlow = defaultConfig.TopBar_GradientFlow;
+		_config.TopBar_GradientStartColor = defaultConfig.TopBar_GradientStartColor;
+		_config.TopBar_GradientStartAlpha = defaultConfig.TopBar_GradientStartAlpha;
+		_config.TopBar_GradientEndColor = defaultConfig.TopBar_GradientEndColor;
+		_config.TopBar_GradientEndAlpha = defaultConfig.TopBar_GradientEndAlpha;
+
+		_config.BottomBar_GradientFlow = defaultConfig.BottomBar_GradientFlow;
+		_config.BottomBar_GradientStartColor = defaultConfig.BottomBar_GradientStartColor;
+		_config.BottomBar_GradientStartAlpha = defaultConfig.BottomBar_GradientStartAlpha;
+		_config.BottomBar_GradientEndColor = defaultConfig.BottomBar_GradientEndColor;
+		_config.BottomBar_GradientEndAlpha = defaultConfig.BottomBar_GradientEndAlpha;
+
+		_config.WindowAccent = defaultConfig.WindowAccent;
+		_config.FontColor = defaultConfig.FontColor;
+		_config.DisplayTimeMilliseconds = defaultConfig.DisplayTimeMilliseconds;
+
+		LoadConfiguration();
+
+		panel_Top.Invalidate();
+		panel_Bottom.Invalidate();
+		label_Message.ForeColor = ColorTranslator.FromHtml(_config.FontColor);
+	}
+
+	private void label_LivePreview_Click(object sender, EventArgs e)
+	{
+		_config.Save(_configPath);
+
+		string launcherExecutable = _ide.Project.GetLauncherFilePath();
+		string originalFileName = FileVersionInfo.GetVersionInfo(launcherExecutable).OriginalFilename;
+
+		if (!originalFileName.Equals("launch.exe", StringComparison.OrdinalIgnoreCase))
+		{
+			DarkMessageBox.Show(this, "Project not supported.", string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			return;
+		}
+
+		var startInfo = new ProcessStartInfo
+		{
+			FileName = launcherExecutable,
+			Arguments = "-p",
+			WorkingDirectory = Path.GetDirectoryName(launcherExecutable),
+			UseShellExecute = true
+		};
+
+		Process.Start(startInfo);
+	}
+}
--- TombIDE/TombIDE.ProjectMaster/Sections/Settings Sections/SettingsSplashScreen.Designer.cs
+++ TombIDE/TombIDE.ProjectMaster/Sections/Settings Sections/SettingsSplashScreen.Designer.cs
@@ -17,132 +17,152 @@
 
 		private void InitializeComponent()
 		{
-			this.button_Change = new DarkUI.Controls.DarkButton();
-			this.button_Preview = new DarkUI.Controls.DarkButton();
-			this.button_Remove = new DarkUI.Controls.DarkButton();
-			this.label_01 = new DarkUI.Controls.DarkLabel();
-			this.label_Blank = new DarkUI.Controls.DarkLabel();
-			this.label_NotSupported = new DarkUI.Controls.DarkLabel();
-			this.panel_Preview = new System.Windows.Forms.Panel();
-			this.sectionPanel = new DarkUI.Controls.DarkSectionPanel();
-			this.panel_Preview.SuspendLayout();
-			this.sectionPanel.SuspendLayout();
-			this.SuspendLayout();
+			button_Change = new DarkUI.Controls.DarkButton();
+			button_Preview = new DarkUI.Controls.DarkButton();
+			button_Remove = new DarkUI.Controls.DarkButton();
+			label_01 = new DarkUI.Controls.DarkLabel();
+			label_Blank = new DarkUI.Controls.DarkLabel();
+			label_NotSupported = new DarkUI.Controls.DarkLabel();
+			panel_Preview = new System.Windows.Forms.Panel();
+			sectionPanel = new DarkUI.Controls.DarkSectionPanel();
+			panel1 = new System.Windows.Forms.Panel();
+			button_AdjustFrame = new DarkUI.Controls.DarkButton();
+			panel_Preview.SuspendLayout();
+			sectionPanel.SuspendLayout();
+			SuspendLayout();
 			// 
 			// button_Change
 			// 
-			this.button_Change.Checked = false;
-			this.button_Change.Location = new System.Drawing.Point(476, 200);
-			this.button_Change.Margin = new System.Windows.Forms.Padding(3, 3, 6, 6);
-			this.button_Change.Name = "button_Change";
-			this.button_Change.Size = new System.Drawing.Size(155, 26);
-			this.button_Change.TabIndex = 2;
-			this.button_Change.Text = "Change...";
-			this.button_Change.Click += new System.EventHandler(this.button_Change_Click);
+			button_Change.Checked = false;
+			button_Change.Location = new System.Drawing.Point(476, 200);
+			button_Change.Margin = new System.Windows.Forms.Padding(3, 3, 6, 6);
+			button_Change.Name = "button_Change";
+			button_Change.Size = new System.Drawing.Size(155, 26);
+			button_Change.TabIndex = 2;
+			button_Change.Text = "Change image...";
+			button_Change.Click += button_Change_Click;
 			// 
 			// button_Preview
 			// 
-			this.button_Preview.Checked = false;
-			this.button_Preview.Location = new System.Drawing.Point(476, 37);
-			this.button_Preview.Margin = new System.Windows.Forms.Padding(3, 12, 6, 6);
-			this.button_Preview.Name = "button_Preview";
-			this.button_Preview.Size = new System.Drawing.Size(155, 26);
-			this.button_Preview.TabIndex = 0;
-			this.button_Preview.Text = "Live Preview";
-			this.button_Preview.Click += new System.EventHandler(this.button_Preview_Click);
+			button_Preview.Checked = false;
+			button_Preview.Location = new System.Drawing.Point(476, 37);
+			button_Preview.Margin = new System.Windows.Forms.Padding(3, 12, 6, 6);
+			button_Preview.Name = "button_Preview";
+			button_Preview.Size = new System.Drawing.Size(155, 26);
+			button_Preview.TabIndex = 0;
+			button_Preview.Text = "Live Preview";
+			button_Preview.Click += button_Preview_Click;
 			// 
 			// button_Remove
 			// 
-			this.button_Remove.Checked = false;
-			this.button_Remove.Location = new System.Drawing.Point(476, 235);
-			this.button_Remove.Margin = new System.Windows.Forms.Padding(3, 3, 6, 6);
-			this.button_Remove.Name = "button_Remove";
-			this.button_Remove.Size = new System.Drawing.Size(155, 26);
-			this.button_Remove.TabIndex = 3;
-			this.button_Remove.Text = "Remove (use blank)";
-			this.button_Remove.Click += new System.EventHandler(this.button_Remove_Click);
+			button_Remove.Checked = false;
+			button_Remove.Location = new System.Drawing.Point(476, 235);
+			button_Remove.Margin = new System.Windows.Forms.Padding(3, 3, 6, 6);
+			button_Remove.Name = "button_Remove";
+			button_Remove.Size = new System.Drawing.Size(155, 26);
+			button_Remove.TabIndex = 3;
+			button_Remove.Text = "Remove (use blank)";
+			button_Remove.Click += button_Remove_Click;
 			// 
 			// label_01
 			// 
-			this.label_01.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-			this.label_01.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-			this.label_01.Location = new System.Drawing.Point(476, 72);
-			this.label_01.Margin = new System.Windows.Forms.Padding(3, 3, 6, 6);
-			this.label_01.Name = "label_01";
-			this.label_01.Size = new System.Drawing.Size(155, 119);
-			this.label_01.TabIndex = 1;
-			this.label_01.Text = "Supported image sizes:\r\n\r\n- 512x256 px (Small)\r\n- 768x384 px (Medium)\r\n- 1024x512" +
-    " px (Large)";
-			this.label_01.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			label_01.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			label_01.ForeColor = System.Drawing.Color.FromArgb(220, 220, 220);
+			label_01.Location = new System.Drawing.Point(476, 69);
+			label_01.Margin = new System.Windows.Forms.Padding(3, 0, 6, 3);
+			label_01.Name = "label_01";
+			label_01.Size = new System.Drawing.Size(155, 79);
+			label_01.TabIndex = 1;
+			label_01.Text = "Supported image sizes:\r\n\r\n- 512x256 px (Small)\r\n- 768x384 px (Medium)\r\n- 1024x512 px (Large)";
 			// 
 			// label_Blank
 			// 
-			this.label_Blank.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.label_Blank.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-			this.label_Blank.ForeColor = System.Drawing.Color.Gray;
-			this.label_Blank.Location = new System.Drawing.Point(0, 0);
-			this.label_Blank.Name = "label_Blank";
-			this.label_Blank.Size = new System.Drawing.Size(458, 228);
-			this.label_Blank.TabIndex = 0;
-			this.label_Blank.Text = "BLANK";
-			this.label_Blank.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			this.label_Blank.Visible = false;
+			label_Blank.Dock = System.Windows.Forms.DockStyle.Fill;
+			label_Blank.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			label_Blank.ForeColor = System.Drawing.Color.Gray;
+			label_Blank.Location = new System.Drawing.Point(0, 0);
+			label_Blank.Name = "label_Blank";
+			label_Blank.Size = new System.Drawing.Size(458, 228);
+			label_Blank.TabIndex = 0;
+			label_Blank.Text = "BLANK";
+			label_Blank.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			label_Blank.Visible = false;
 			// 
 			// label_NotSupported
 			// 
-			this.label_NotSupported.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.label_NotSupported.ForeColor = System.Drawing.Color.Gray;
-			this.label_NotSupported.Location = new System.Drawing.Point(0, 0);
-			this.label_NotSupported.Name = "label_NotSupported";
-			this.label_NotSupported.Size = new System.Drawing.Size(458, 228);
-			this.label_NotSupported.TabIndex = 1;
-			this.label_NotSupported.Text = "Legacy projects don\'t support splash screens.";
-			this.label_NotSupported.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			this.label_NotSupported.Visible = false;
+			label_NotSupported.Dock = System.Windows.Forms.DockStyle.Fill;
+			label_NotSupported.ForeColor = System.Drawing.Color.Gray;
+			label_NotSupported.Location = new System.Drawing.Point(0, 0);
+			label_NotSupported.Name = "label_NotSupported";
+			label_NotSupported.Size = new System.Drawing.Size(458, 228);
+			label_NotSupported.TabIndex = 1;
+			label_NotSupported.Text = "Legacy projects don't support splash screens.";
+			label_NotSupported.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			label_NotSupported.Visible = false;
 			// 
 			// panel_Preview
 			// 
-			this.panel_Preview.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
-			this.panel_Preview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-			this.panel_Preview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.panel_Preview.Controls.Add(this.label_NotSupported);
-			this.panel_Preview.Controls.Add(this.label_Blank);
-			this.panel_Preview.Location = new System.Drawing.Point(7, 31);
-			this.panel_Preview.Margin = new System.Windows.Forms.Padding(6);
-			this.panel_Preview.Name = "panel_Preview";
-			this.panel_Preview.Size = new System.Drawing.Size(460, 230);
-			this.panel_Preview.TabIndex = 4;
+			panel_Preview.BackColor = System.Drawing.Color.FromArgb(48, 48, 48);
+			panel_Preview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			panel_Preview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			panel_Preview.Controls.Add(label_NotSupported);
+			panel_Preview.Controls.Add(label_Blank);
+			panel_Preview.Location = new System.Drawing.Point(7, 31);
+			panel_Preview.Margin = new System.Windows.Forms.Padding(6);
+			panel_Preview.Name = "panel_Preview";
+			panel_Preview.Size = new System.Drawing.Size(460, 230);
+			panel_Preview.TabIndex = 4;
 			// 
 			// sectionPanel
 			// 
-			this.sectionPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.sectionPanel.Controls.Add(this.button_Preview);
-			this.sectionPanel.Controls.Add(this.label_01);
-			this.sectionPanel.Controls.Add(this.button_Remove);
-			this.sectionPanel.Controls.Add(this.button_Change);
-			this.sectionPanel.Controls.Add(this.panel_Preview);
-			this.sectionPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.sectionPanel.Location = new System.Drawing.Point(0, 0);
-			this.sectionPanel.Name = "sectionPanel";
-			this.sectionPanel.SectionHeader = "Splash Screen Image";
-			this.sectionPanel.Size = new System.Drawing.Size(640, 270);
-			this.sectionPanel.TabIndex = 0;
+			sectionPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			sectionPanel.Controls.Add(panel1);
+			sectionPanel.Controls.Add(button_AdjustFrame);
+			sectionPanel.Controls.Add(button_Preview);
+			sectionPanel.Controls.Add(label_01);
+			sectionPanel.Controls.Add(button_Remove);
+			sectionPanel.Controls.Add(button_Change);
+			sectionPanel.Controls.Add(panel_Preview);
+			sectionPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			sectionPanel.Location = new System.Drawing.Point(0, 0);
+			sectionPanel.Name = "sectionPanel";
+			sectionPanel.SectionHeader = "Splash Screen Image";
+			sectionPanel.Size = new System.Drawing.Size(640, 270);
+			sectionPanel.TabIndex = 0;
+			// 
+			// panel1
+			// 
+			panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			panel1.Location = new System.Drawing.Point(476, 189);
+			panel1.Name = "panel1";
+			panel1.Size = new System.Drawing.Size(155, 1);
+			panel1.TabIndex = 6;
+			// 
+			// button_AdjustFrame
+			// 
+			button_AdjustFrame.Checked = false;
+			button_AdjustFrame.Location = new System.Drawing.Point(476, 154);
+			button_AdjustFrame.Margin = new System.Windows.Forms.Padding(3, 3, 6, 6);
+			button_AdjustFrame.Name = "button_AdjustFrame";
+			button_AdjustFrame.Size = new System.Drawing.Size(155, 26);
+			button_AdjustFrame.TabIndex = 5;
+			button_AdjustFrame.Text = "Customize frame...";
+			button_AdjustFrame.Click += button_AdjustFrame_Click;
 			// 
 			// SettingsSplashScreen
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-			this.Controls.Add(this.sectionPanel);
-			this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.MaximumSize = new System.Drawing.Size(640, 270);
-			this.MinimumSize = new System.Drawing.Size(640, 270);
-			this.Name = "SettingsSplashScreen";
-			this.Size = new System.Drawing.Size(640, 270);
-			this.panel_Preview.ResumeLayout(false);
-			this.sectionPanel.ResumeLayout(false);
-			this.ResumeLayout(false);
-
+			AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			BackColor = System.Drawing.Color.FromArgb(60, 63, 65);
+			Controls.Add(sectionPanel);
+			Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			MaximumSize = new System.Drawing.Size(640, 270);
+			MinimumSize = new System.Drawing.Size(640, 270);
+			Name = "SettingsSplashScreen";
+			Size = new System.Drawing.Size(640, 270);
+			panel_Preview.ResumeLayout(false);
+			sectionPanel.ResumeLayout(false);
+			ResumeLayout(false);
 		}
 
 		#endregion
@@ -155,5 +175,7 @@
 		private DarkUI.Controls.DarkLabel label_NotSupported;
 		private DarkUI.Controls.DarkSectionPanel sectionPanel;
 		private System.Windows.Forms.Panel panel_Preview;
+		private DarkUI.Controls.DarkButton button_AdjustFrame;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
