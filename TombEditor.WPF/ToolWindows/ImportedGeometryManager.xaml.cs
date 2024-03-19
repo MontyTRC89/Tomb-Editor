7922668b2d3aba6c6acb5eadc02bd955019ced31
davidmarr <116632612+davidmarr@users.noreply.github.com>
2024-03-19 21:08:29 +0100

    update

---
+++ TombEditor.WPF/ToolWindows/ImportedGeometryManager.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TombEditor.WPF.ToolWindows;
+
+/// <summary>
+/// Logica di interazione per ImportedGeometryManager.xaml
+/// </summary>
+public partial class ImportedGeometryManager : UserControl
+{
+    public ImportedGeometryManager()
+    {
+        InitializeComponent();
+    }
+}
+
