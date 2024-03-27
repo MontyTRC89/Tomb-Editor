fadedf179fb85170c32b1e718283daaadea03768
Kewin Kupilas <k.kupilas@outlook.com>
2024-03-27 16:43:00 +0000

    Fix hotkeys preventing text input

--- TombEditor.WPF/DynamicKeyBinding.cs
+++ TombEditor.WPF/DynamicKeyBinding.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows;
-using System.Windows.Forms;
+﻿using DarkUI.WPF.CustomControls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using WinForms = System.Windows.Forms;
 
 namespace TombEditor.WPF
 {
-	public sealed class DynamicKeyBinding : KeyBinding
+	public sealed class DynamicKeyBinding : InputBinding
 	{
 		public static readonly DependencyProperty InputGestureTextProperty = DependencyProperty.Register(nameof(InputGestureText), typeof(string), typeof(DynamicKeyBinding));
 
@@ -19,32 +22,58 @@ namespace TombEditor.WPF
 			if (e.Property == InputGestureTextProperty && e.NewValue is string text)
 			{
 				if (string.IsNullOrEmpty(text))
-					Gesture = new KeyGesture(Key.NoName);
-				else
 				{
-					var hotkey = Hotkey.FromString(text);
-
-					ModifierKeys modifiers = ModifierKeys.None;
-					Key key = Key.NoName;
-
-					if ((hotkey.Keys & Keys.Control) != Keys.None)
-						modifiers |= ModifierKeys.Control;
-
-					if ((hotkey.Keys & Keys.Shift) != Keys.None)
-						modifiers |= ModifierKeys.Shift;
-
-					if ((hotkey.Keys & Keys.Alt) != Keys.None)
-						modifiers |= ModifierKeys.Alt;
-
-					if (hotkey.MainKey != Keys.None)
-						key = KeyInterop.KeyFromVirtualKey((int)hotkey.MainKey);
-
-					Modifiers = modifiers;
-					Key = key;
+					Gesture = new KeyGestureEx();
+					return;
 				}
+
+				var hotkey = Hotkey.FromString(text);
+
+				ModifierKeys modifiers = ModifierKeys.None;
+				Key key = Key.NoName;
+
+				if ((hotkey.Keys & WinForms.Keys.Control) != WinForms.Keys.None)
+					modifiers |= ModifierKeys.Control;
+
+				if ((hotkey.Keys & WinForms.Keys.Shift) != WinForms.Keys.None)
+					modifiers |= ModifierKeys.Shift;
+
+				if ((hotkey.Keys & WinForms.Keys.Alt) != WinForms.Keys.None)
+					modifiers |= ModifierKeys.Alt;
+
+				if (hotkey.MainKey != WinForms.Keys.None)
+					key = KeyInterop.KeyFromVirtualKey((int)hotkey.MainKey);
+
+				Gesture = new KeyGestureEx(key, modifiers);
 			}
 
 			base.OnPropertyChanged(e);
+		}
+	}
+
+	public class KeyGestureEx : InputGesture
+	{
+		public Key Key { get; set; } = Key.NoName;
+		public ModifierKeys Modifiers { get; set; } = ModifierKeys.None;
+
+		public KeyGestureEx()
+		{ }
+
+		public KeyGestureEx(Key key, ModifierKeys modifiers)
+		{
+			Key = key;
+			Modifiers = modifiers;
+		}
+
+		public override bool Matches(object targetElement, InputEventArgs inputEventArgs)
+		{
+			if (inputEventArgs is not KeyEventArgs keyEventArgs)
+				return false;
+
+			if (inputEventArgs.OriginalSource is TextBoxBase or ListBoxItem)
+				return false;
+
+			return keyEventArgs.Key == Key && keyEventArgs.KeyboardDevice.Modifiers == Modifiers;
 		}
 	}
 }
