e78c84d0466d56cf859755ae4f78bcf78133c468
davidmarr <116632612+davidmarr@users.noreply.github.com>
2024-06-21 21:42:40 +0200

    Update LevelHandling.cs

--- TombIDE/TombIDE.Shared/SharedClasses/LevelHandling.cs
+++ TombIDE/TombIDE.Shared/SharedClasses/LevelHandling.cs
@@ -87,22 +87,16 @@ namespace TombIDE.Shared.SharedClasses
 			{
 				return new List<string>
 				{
-					"\n",
-					$"-- {dataFileName} level",
-					"",
-					$"{dataFileName} = TEN.Flow.Level()",
-					"",
+					$"\n-- {dataFileName} level\n",
+					$"{dataFileName} = TEN.Flow.Level()\n",
 					$"{dataFileName}.nameKey = \"{dataFileName}\"",
 					$"{dataFileName}.scriptFile = \"Scripts\\\\Levels\\\\{dataFileName}.lua\"",
 					$"{dataFileName}.ambientTrack = \"{ambientSoundID}\"",
 					$"{dataFileName}.horizon = " + (horizon ? "true" : "false"),
 					$"{dataFileName}.levelFile = \"Data\\\\{dataFileName}.ten\"",
-					$"{dataFileName}.loadScreenFile = \"Screens\\\\rome.jpg\"",
-					"",
-					$"TEN.Flow.AddLevel({dataFileName})",
-					$"	{dataFileName} = {{ \"{levelName}\" }}",
-					"",
-					"--------------------------------------------------"
+					$"{dataFileName}.loadScreenFile = \"Screens\\\\rome.jpg\"\n",
+					$"TEN.Flow.AddLevel({dataFileName})\n",
+					"--------------------------------------------------",
 				};
 			}
 
