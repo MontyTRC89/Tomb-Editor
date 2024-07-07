75c308adad3a05e17b23d4cca529567cc33fce9f
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2024-07-07 01:37:32 +0100

    Add optional offset to dynamic light nodes.

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Dynamic Lights.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Dynamic Lights.lua
@@ -3,11 +3,13 @@
 -- !Description "Add a dynamic light to a moveable's mesh."
 -- !Arguments "NewLine, Moveables, 70, Select moveable to attach light to." "Numerical, [ 0 | 100 | 0 ] , 30, Select mesh number of moveable. \nThis can be found in the Animation Editor within Wadtool."
 -- !Arguments "NewLine, Color, 70, Color of light." "Numerical, [ 0 | 100 | 0 ], 30, Light radius (in sectors)."
+-- !Arguments "NewLine, 100, Vector3, Offset (relative)"
 
-LevelFuncs.Engine.Node.MoveableLight = function(moveable, meshnumber, lightcolor, range)
+LevelFuncs.Engine.Node.MoveableLight = function(moveable, meshnumber, lightcolor, range, effectOffset)
 
-	local entity = TEN.Objects.GetMoveableByName(moveable):GetJointPosition(meshnumber)
-	TEN.Effects.EmitLight(entity, lightcolor, range)
+	local entityPos = TEN.Objects.GetMoveableByName(moveable):GetJointPosition(meshnumber)
+	local offset = entityPos + effectOffset
+	TEN.Effects.EmitLight(offset, lightcolor, range)
 end
 
 -- !Name "Add dynamic light to static"
@@ -15,10 +17,12 @@ end
 -- !Description "Add a dynamic light to a static object."
 -- !Arguments "NewLine, Statics, Select static to attach light to."
 -- !Arguments "NewLine, Color, 70, Color of light." "Numerical, 30, [ 0 | 100 | 0 ], Light radius (in sectors)."
+-- !Arguments "NewLine, 100, Vector3, Offset (relative)"
 
-LevelFuncs.Engine.Node.StaticLight = function(static,lightcolor,range)
+LevelFuncs.Engine.Node.StaticLight = function(static, lightcolor, range, effectOffset)
 
-	local entity = TEN.Objects.GetStaticByName(static):GetPosition()
-	TEN.Effects.EmitLight(entity, lightcolor, range)
+	local entityPos = TEN.Objects.GetStaticByName(static):GetPosition()
+	local offset = entityPos + effectOffset
+	TEN.Effects.EmitLight(offset, lightcolor, range)
 end
 
