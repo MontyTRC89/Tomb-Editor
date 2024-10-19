0a48bde2c86b7b5821893967ca74599ec46e8209
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2024-10-18 22:10:11 -0400

    Update Revolution.lua

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Revolution.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Revolution.lua
@@ -1,191 +1,236 @@
---Revolution Nodes by TrainWreck; Code references by Lwmte, Adngel. Shoutout to DaviDMRR, RichardTRLE and Stranger1992 for the help.
+--Custom Bar Nodes by TrainWreck and DaviDMRR and Adngel; Code references by Lwmte and TEN Nodes.
 
-local Timer = require("Engine.Timer")
+LevelVars.CustomBars = {}
+-- Construct custom bar
+LevelFuncs.Engine.Node.ConstructCustomBar = function(barName, startvalue, objectIDbg, spriteIDbg, colorbg, objectIDbar, spriteIDbar, colorbar, posX, posY, rot, scaleX, scaleY, alignMode, scaleMode, blendMode)
 
--- Construct timed transform data and start transform
-LevelFuncs.Engine.Node.ConstructRevolveData = function(objectName, objectCentre, dataType, radius, time, startAngle, endAngle, isLoop, isCCW, isSmooth, isRotate, isCRotate, SFX)
+	local dataName	= barName .. "_bar_data"
 
-    local dataName  = objectName .. "_revolve_data"
-    local revolutionAngle = 360  --one full revolution is 360 degrees
-    
-    if LevelVars[dataName] and LevelVars[dataName].Timer then
-        if Timer.Get(dataName):IsActive() then
-            return
-        end
-        Timer.Delete(LevelVars[dataName].Name)
-        LevelVars[dataName] = nil
-    end
+	if LevelVars.CustomBars[dataName] then
+		return
+	end
 
-    LevelVars[dataName] = {}
-    LevelVars[dataName].Progress   = 0
-    LevelVars[dataName].Interval   = 1 / (time * 30)
-    LevelVars[dataName].DataType   = dataType
-    LevelVars[dataName].Radius     = radius
-    LevelVars[dataName].ObjectName = objectName
-    LevelVars[dataName].CentreName = objectCentre
-    LevelVars[dataName].Name       = dataName
-    LevelVars[dataName].Timer      = Timer.Create(dataName, 1 / 30, true, false, LevelFuncs.Engine.Node.TransformRevolveData, dataName)
-    LevelVars[dataName].StartAngle = startAngle
-    LevelVars[dataName].EndAngle   = endAngle
-    LevelVars[dataName].CCW        = isCCW     -- Flips the direction of the loop if loop is ticked.
-    LevelVars[dataName].Loop       = isLoop    -- Loops the object continuously starting from start angle.
-    LevelVars[dataName].Smooth     = isSmooth  -- Used for smooth start and stop. If loop is used then the smooth start and stop is at the start angle.
-    LevelVars[dataName].Rotate     = isRotate  -- Rotate the object itself. Only used for Horizontal rotation
-    LevelVars[dataName].CRotate    = isCRotate -- to rotate the center object
-    LevelVars[dataName].Sfx        = SFX
-    LevelVars[dataName].OldValue   = startAngle
-    LevelVars[dataName].StopAtEnd  = false
-    
-    LevelVars[dataName].NewValue = LevelVars[dataName].Loop
-    and (startAngle + (LevelVars[dataName].CCW and -revolutionAngle or revolutionAngle))
-    or endAngle
-    
-    LevelVars[dataName].Timer:Start()
+	LevelVars.CustomBars[dataName]					= {}
+	LevelVars.CustomBars[dataName].Name				= dataName
+	LevelVars.CustomBars[dataName].FixedInterval	= 1/30
+	LevelVars.CustomBars[dataName].Progress			= startvalue / 1000 -- Set initial progress from start value (clamped to 0-1000)
+	LevelVars.CustomBars[dataName].Interval			= 1 / (30)	-- assuming 30 updates per second
+	LevelVars.CustomBars[dataName].ObjectIDbg		= objectIDbg
+	LevelVars.CustomBars[dataName].SpriteIDbg		= spriteIDbg
+	LevelVars.CustomBars[dataName].ColorBG			= colorbg
+	LevelVars.CustomBars[dataName].ObjectIDbar		= objectIDbar
+	LevelVars.CustomBars[dataName].SpriteIDbar		= spriteIDbar
+	LevelVars.CustomBars[dataName].ColorBar			= colorbar
+	LevelVars.CustomBars[dataName].PosX				= posX
+	LevelVars.CustomBars[dataName].PosY				= posY
+	LevelVars.CustomBars[dataName].ScaleX			= scaleX
+	LevelVars.CustomBars[dataName].ScaleY			= scaleY
+	LevelVars.CustomBars[dataName].Rot				= rot
+	LevelVars.CustomBars[dataName].Visible			= true
+	LevelVars.CustomBars[dataName].CurrentAlpha		= 1
+	LevelVars.CustomBars[dataName].TargetAlpha		= 255
+	LevelVars.CustomBars[dataName].AlignMode		= alignMode
+	LevelVars.CustomBars[dataName].ScaleMode		= scaleMode
+	LevelVars.CustomBars[dataName].BlendMode		= blendMode
+	LevelVars.CustomBars[dataName].OldValue			= startvalue  -- stores the current bar value
+	LevelVars.CustomBars[dataName].TargetValue		= startvalue  -- target value to reach
+	
 end
+
+
 
 -- Transform object parameter using previously saved timed transform data
-LevelFuncs.Engine.Node.TransformRevolveData = function(dataName)
-   
-    -- Smoothly loop the progress value
-    LevelVars[dataName].Progress = LevelVars[dataName].Loop
-    and (LevelVars[dataName].Progress + LevelVars[dataName].Interval) % 1
-    or math.min(LevelVars[dataName].Progress + LevelVars[dataName].Interval, 1)
-    
-    --function to normalize the angles to 360 degree
-    local function NormalizeAngle(angle)
-        return (angle % 360 + 360) % 360
-    end
-    
-    local factor = LevelVars[dataName].Smooth and LevelFuncs.Engine.Node.Smoothstep(LevelVars[dataName].Progress) or LevelVars[dataName].Progress
-    local newValue1 = LevelFuncs.Engine.Node.Lerp(LevelVars[dataName].OldValue, LevelVars[dataName].NewValue, factor)
-    local object2 = TEN.Objects.GetMoveableByName(LevelVars[dataName].ObjectName)
-    local object2pos = object2:GetPosition()
-    local center2 = TEN.Objects.GetMoveableByName(LevelVars[dataName].CentreName)
-    local center2pos = center2:GetPosition()
-    local center2rot = center2:GetRotation()
-    local radius2 = LevelVars[dataName].Radius
-    local angle = newValue1 * math.pi / 180
-    local x = center2pos.x
-    local y = center2pos.y
-    local z = center2pos.z
-    local normalizedNewValue1 = NormalizeAngle(newValue1)
-    local normalizedEndAngle = NormalizeAngle(LevelVars[dataName].EndAngle)
-    local tolerance = 5e-1 --0.5 tolerance. The tolerance to stop the object revolution. Its required incase of smooth rotation.
-    
-    if (LevelVars[dataName].DataType == 0) then
-        if (center2rot.y == 0) then
-            local ptx, pty = x + radius2 * math.sin(angle), y - radius2 * math.cos(angle)
-            object2:SetPosition(Vec3(ptx, pty, object2pos.z))
-            
-            if not IsSoundPlaying(LevelVars[dataName].Sfx) then
-                TEN.Sound.PlaySound(LevelVars[dataName].Sfx, object2pos)
-            end
-            
-            if LevelVars[dataName].CRotate then    
-                center2:SetRotation(Rotation(0, 0, newValue1))
-            end
-            
-        elseif (center2rot.y == 90) then
-            local ptz, pty = z + radius2 * math.sin(-angle), y - radius2 * math.cos(-angle)
-            object2:SetPosition(Vec3(object2pos.x, pty, ptz))
-            
-            if not IsSoundPlaying(LevelVars[dataName].Sfx) then
-                TEN.Sound.PlaySound(LevelVars[dataName].Sfx, object2pos)
-            end
-            
-            if LevelVars[dataName].CRotate then    
-                center2:SetRotation(Rotation(0, center2rot.y, newValue1))
-            end
-            
-        elseif (center2rot.y == -180) then
-            local ptx, pty = x + radius2 * math.sin(-angle), y - radius2 * math.cos(-angle)
-            object2:SetPosition(Vec3(ptx, pty, object2pos.z))
-            
-            if not IsSoundPlaying(LevelVars[dataName].Sfx) then
-                TEN.Sound.PlaySound(LevelVars[dataName].Sfx, object2pos)
-            end
-            
-            if LevelVars[dataName].CRotate then    
-                center2:SetRotation(Rotation(0, center2rot.y, newValue1))
-            end    
-            
-        elseif (center2rot.y == -90) then
-            local ptz, pty = z + radius2 * math.sin(angle), y - radius2 * math.cos(angle)
-            object2:SetPosition(Vec3(object2pos.x, pty, ptz))
-            
-            if not IsSoundPlaying(LevelVars[dataName].Sfx) then
-                TEN.Sound.PlaySound(LevelVars[dataName].Sfx, object2pos)
-            end
-            
-            if LevelVars[dataName].CRotate then    
-                center2:SetRotation(Rotation(0, center2rot.y, newValue1))
-            end
-        end
-    elseif (LevelVars[dataName].DataType == 1) then
-        local ptz, ptx = z + radius2 * math.sin(-angle), x + radius2 * math.cos(-angle)
-        object2:SetPosition(Vec3(ptx, object2pos.y, ptz))
-        
-        if not IsSoundPlaying(LevelVars[dataName].Sfx) then
-            TEN.Sound.PlaySound(LevelVars[dataName].Sfx, object2pos)
-        end
-        
-        if LevelVars[dataName].CRotate then    
-            center2:SetRotation(Rotation(0, newValue1, 0))
-        end
-        
-        if LevelVars[dataName].Rotate then    
-            object2:SetRotation(Rotation(0, newValue1, 0))
-        end
-    end
+LevelFuncs.Engine.UpdateCustomBars = function()
 
-    if LevelVars[dataName].StopAtEnd and  math.abs(normalizedNewValue1 - normalizedEndAngle) < tolerance then
-        Timer.Delete(LevelVars[dataName].Name)
-        LevelVars[dataName] = nil
-    elseif not LevelVars[dataName].Loop and LevelVars[dataName].Progress >= 1 then
-        Timer.Delete(LevelVars[dataName].Name)
-        LevelVars[dataName] = nil
-    end
+	for _, CustomBar in pairs (LevelVars.CustomBars) do
+
+		if CustomBar ~= nil then
+			-- Smoothly transition to target value
+			local currentValue = CustomBar.OldValue or 0
+			local targetValue = CustomBar.TargetValue or 0
+			local delta = CustomBar.FixedInterval
+
+			if currentValue ~= targetValue then
+				-- Update current value by delta (increment or decrement)
+				if currentValue < targetValue then
+					currentValue = math.min(currentValue + delta, targetValue)
+				else
+					currentValue = math.max(currentValue + delta, targetValue)
+				end
+
+				-- Update the bar's progress (0-1 scale)
+				CustomBar.OldValue = currentValue
+				CustomBar.Progress = currentValue / 1000
+			end
+			-- Smoothly transition alpha
+			if CustomBar.CurrentAlpha ~= CustomBar.TargetAlpha then
+				local alphaDelta = 50 -- Adjust speed of alpha transition
+				if CustomBar.CurrentAlpha < CustomBar.TargetAlpha then
+					CustomBar.CurrentAlpha = math.floor(math.min(CustomBar.CurrentAlpha + alphaDelta, CustomBar.TargetAlpha))
+				else
+					CustomBar.CurrentAlpha = math.floor(math.max(CustomBar.CurrentAlpha - alphaDelta, CustomBar.TargetAlpha))
+				end
+			end
+			-- Set up parameters for drawing
+			local pos = TEN.Vec2(CustomBar.PosX, CustomBar.PosY)
+			local scale = TEN.Vec2(CustomBar.ScaleX, CustomBar.ScaleY)
+			local rot = CustomBar.Rot
+			local alignM = LevelFuncs.Engine.Node.GetDisplaySpriteAlignMode(CustomBar.AlignMode)
+			local scaleM = LevelFuncs.Engine.Node.GetDisplaySpriteScaleMode(CustomBar.ScaleMode)
+			local blendID = LevelFuncs.Engine.Node.GetBlendMode(CustomBar.BlendMode)
+			
+			-- Adjust color with alpha blending
+			local bgColor =Color(CustomBar.ColorBG.r,CustomBar.ColorBG.g,CustomBar.ColorBG.b,CustomBar.CurrentAlpha)
+			local barColor = Color(CustomBar.ColorBar.r,CustomBar.ColorBar.g,CustomBar.ColorBar.b,CustomBar.CurrentAlpha)
+			
+			if CustomBar.CurrentAlpha == 0 then
+				LevelVars.CustomBars[dataName].Visible = false
+			end
+			
+			if CustomBar.Visible and CustomBar.CurrentAlpha > 0 then
+				-- Draw background sprite
+				local bgSprite = TEN.DisplaySprite(CustomBar.ObjectIDbg, CustomBar.SpriteIDbg, pos, rot, scale, bgColor)
+				bgSprite:Draw(0, alignM, scaleM, blendID)
+
+				-- Draw foreground sprite (the bar itself) proportional to Progress
+				local barScale = TEN.Vec2(CustomBar.ScaleX * CustomBar.Progress, CustomBar.ScaleY)
+				local barSprite = TEN.DisplaySprite(CustomBar.ObjectIDbar, CustomBar.SpriteIDbar, pos, rot, barScale, barColor)
+				barSprite:Draw(1, alignM, scaleM, blendID)
+				
+				--- Debugging code
+				local barValue = math.floor(CustomBar.OldValue)
+				myTextString = "Bar Value: " .. barValue
+				myText = DisplayString(myTextString, CustomBar.PosX, CustomBar.PosY-10, CustomBar.ColorBar)
+				ShowString(myText,1/30)
+			end
+		end
+	end
 end
 
---- Delete timed transform data
-LevelFuncs.Engine.Node.DeleteTimedData = function(objectName, endAngle)
-    local dataName = objectName .. "_revolve_data"
+-- !Name "Draw custom bar"
+-- !Section "Custom bars"
+-- !Conditional "False"
+-- !Description "Draw a custom bar."
+-- !Arguments "NewLine, String, 50, [ NoMultiline ], Bar name"
+-- !Arguments "Number, 20, {0}, [ 0 | 1000 | 2 ], Start value of bar"
+-- !Arguments "Newline, SpriteSlots, 61, Background sprite sequence object ID,{TEN.Objects.ObjID.BAR_BORDER_GRAPHIC}"
+-- !Arguments "Number, 19, [ 0 | 9999 | 0 ], Sprite ID for background in sprite sequence\nRange[0 to 9999]"
+-- !Arguments "Color, 20, Color of background sprite"
+-- !Arguments "Newline, SpriteSlots, 61, Bar sprite sequence object ID, {TEN.Objects.ObjID.SFX_BAR_TEXTURE}"
+-- !Arguments "Number, 19, [ 0 | 9999 | 0 ], Sprite ID for bar in sprite sequence\nRange[0 to 9999]"
+-- !Arguments "Color, 20, Color of bar sprite"
+-- !Arguments "Newline, Number, 20, [ -1000 | 1000 | 2 ], Position X (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ -1000 | 1000 | 2 ], Position Y (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ 0 | 360 | 2 ], Rotation\nRange [0 to 360]"
+-- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale X (%)\nRange [0 to 1000]"
+-- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale Y (%)\nRange [0 to 1000]"
+-- !Arguments "NewLine, Enumeration, 35, [ Center | Center Top | Center Bottom | Center Left | Center Right | Top Left | Top Right | Bottom Left | Bottom Right ], {3}, Align mode"
+-- !Arguments "Enumeration, 22, [ Fit | Fill | Stretch ], Scale mode"
+-- !Arguments "Enumeration, 28, [ Opaque | Alpha Test | Additive | No Z Test | Subtractive | Wireframe | Exclude | Screen | Lighten | Alphablend ], {9}, Blend mode"
 
-    if LevelVars[dataName] and LevelVars[dataName].Timer and Timer.Get(dataName):IsActive() then
-        LevelVars[dataName].EndAngle = endAngle
-        LevelVars[dataName].StopAtEnd = true
-    end
+LevelFuncs.Engine.Node.DisplayBars = function(barName, startvalue, objectIDbg, spriteIDbg, colorbg, objectIDbar, spriteIDbar, colorbar, posX, posY, rot, scaleX, scaleY, alignMode, scaleMode, blendMode)
+-- Wrap another node function call into do/end to prevent wrong parsing
+	do LevelFuncs.Engine.Node.ConstructCustomBar(barName, startvalue, objectIDbg, spriteIDbg, colorbg, objectIDbar, spriteIDbar, colorbar, posX, posY, rot, scaleX, scaleY, alignMode, scaleMode, blendMode)
+	end
 end
 
--- !Name "Revolve an object"
--- !Section "Timespan actions"
--- !Description "Rotate an object around another a moveable over specified timespan."
--- !Arguments "NewLine, Moveables, Moveable to rotate"
--- !Arguments "NewLine, Moveables, CentrePoint"
--- !Arguments "NewLine, Enumeration, [ Vertical | Horizontal], 20, Select Axis of Rotation."
--- !Arguments "Numerical, [ 1 | 65535 | 2 | 1 | 1 ], {1}, 20, Radius of Rotation"
--- !Arguments "Numerical, [ 0.1 | 65535 | 2 | 0.1 | 1 ], {1}, 20, Time (in seconds)"
--- !Arguments "Numerical, [ -360 | 360 | 2 | 1 | 1 ], {0}, 20, Start Angle"
--- !Arguments "Numerical, [ -360 | 360 | 2 | 1 | 1 ], {360}, 20, End Angle. The angle the object will stop if loop is not ticked."
--- !Arguments "NewLine, 33, Boolean, Loop"
--- !Arguments "33, Boolean, Flip Loop"
--- !Arguments "33, Boolean, Smooth"
--- !Arguments "NewLine, 65, Boolean, Rotate Object (Y axis when Horizontal selected)"
--- !Arguments "35, Boolean, Rotate Center Object"
--- !Arguments "NewLine, SoundEffects,{635}"
+-- !Name "Change custom bar value over time"
+-- !Section "Custom bars"
+-- !Description "Change bar value over time."
+-- !Arguments "NewLine, String, 50, [ NoMultiline ], Bar name"
+-- !Arguments "Numerical, [ -1000 | 1000 | 2 | 1 | 1 ], {1}, 25, Value"
+-- !Arguments "Numerical, [ 0.1 | 65535 | 2 | 0.1 | 1 ], {1}, 25, Time (in seconds)"
 
-LevelFuncs.Engine.Node.ChangeMoveableRevolutionOverTimespan = function(moveableName, centrepoint, option, radius, time, startAngle, endAngle, isLoop, isCCW, isSmooth, isRotate, isCRotate, SFX)
-    -- Wrap another node function call into do/end to prevent wrong parsing
-    do LevelFuncs.Engine.Node.ConstructRevolveData(moveableName, centrepoint, option, radius, time, startAngle, endAngle, isLoop, isCCW, isSmooth, isRotate,  isCRotate, SFX) end
+LevelFuncs.Engine.Node.ChangeBarValueOverTimespan = function(barName, value, time)
+
+	local dataName = barName .. "_bar_data"
+
+	-- Check if bar data and timer exist
+	if LevelVars.CustomBars[dataName] then
+
+		-- Get the current target value or old value if no target value exists
+		local currentValue = LevelVars.CustomBars[dataName].OldValue
+		local currentTarget = LevelVars.CustomBars[dataName].TargetValue or currentValue
+
+		-- Calculate new target value by adding the relative 'value' and clamp between 0 and 1000
+		local newTargetValue = math.max(0, math.min(1000, currentTarget + value))
+
+		-- Set the new target value
+		LevelVars.CustomBars[dataName].TargetValue = newTargetValue
+
+		-- Calculate total frames based on time and FPS (30 FPS)
+		local totalFrames = time * 30
+
+		-- Calculate the fixed interval for the entire transition
+		LevelVars.CustomBars[dataName].FixedInterval = (newTargetValue - currentValue) / totalFrames
+	end
 end
 
--- !Name "Stop the revolution of an object"
--- !Section "Timespan actions"
--- !Description "Stop an already active object rotation."
--- !Arguments "NewLine, Moveables, Moveable to stop rotation"
--- !Arguments "NewLine, Numerical, [ -360 | 360 | 2 | 1 | 1 ], {360}, 20, End Angle. This is the angle the object will stop."
+-- !Name "Delete custom bar"
+-- !Section "Custom bars"
+-- !Description "Delete a custom bar."
+-- !Arguments "NewLine, String, 100, [ NoMultiline ], Bar name"
 
-LevelFuncs.Engine.Node.StopMoveableRotation = function(moveableName, endAngle)
-    -- Wrap another node function call into do/end to prevent wrong parsing
-    do LevelFuncs.Engine.Node.DeleteTimedData(moveableName, endAngle) end
+LevelFuncs.Engine.Node.DeleteCustomBar = function(barName)
+	local dataName = barName .. "_bar_data"
+
+	if LevelVars.CustomBars[dataName] then
+		LevelVars.CustomBars[dataName] = nil
+	end
+end
+
+-- !Name "Show/Hide custom bar"
+-- !Section "Custom bars"
+-- !Description "Hides or Shows the custom bar."
+-- !Arguments "NewLine, String, 70, [ NoMultiline ], Bar name"
+-- !Arguments "Enumeration, 30, [ Not visible | Visible ], Visibility"
+
+LevelFuncs.Engine.Node.BarVisibility = function(barName, value)
+	local dataName = barName .. "_bar_data"
+
+	if LevelVars.CustomBars[dataName] then
+		if value == 0 then
+			LevelVars.CustomBars[dataName].TargetAlpha = 0
+		elseif value == 1 then
+			LevelVars.CustomBars[dataName].TargetAlpha = 255
+			LevelVars.CustomBars[dataName].Visible = true
+		end
+	end
+end
+
+-- !Name "If custom bar is visible..."
+-- !Section "Custom bars"
+-- !Description "Checks if specified custom bar is visible."
+-- !Conditional "True"
+-- !Arguments "NewLine, String, 100, [ NoMultiline ], Bar name"
+
+LevelFuncs.Engine.Node.TestBarVisibility = function(barName)
+	
+	local dataName = barName .. "_bar_data"
+	if LevelVars.CustomBars[dataName] then
+		if LevelVars.CustomBars[dataName].Visible then
+			return true
+		else
+			return false
+		end
+	end
+end
+
+-- !Name "If custom bar value is..."
+-- !Section "Custom bars"
+-- !Description "Checks if specified bar value complies to specified compare function."
+-- !Conditional "True"
+-- !Arguments "NewLine, String, 50, [ NoMultiline ], Bar name"
+-- !Arguments "CompareOperator, 25" "Numerical, 25, [ 0 | 1000 | 2 ], Bar value"
+
+LevelFuncs.Engine.Node.TestBarValue = function(barName, operator, value)
+	
+	local dataName = barName .. "_bar_data"
+	
+	if (LevelVars.CustomBars[dataName] == nil) then
+		return LevelFuncs.Engine.Node.CompareValue(0, value, operator)
+	else
+		return LevelFuncs.Engine.Node.CompareValue(LevelVars.CustomBars[dataName].OldValue, value, operator)
+	end
+	
 end
