c3dd80dfdb6909e41c7f1b56d07c519be28d4162
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2025-01-09 17:24:50 -0500

    Add files via upload

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Diary.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Diary.lua
@@ -3,7 +3,7 @@ local CustomDiary = require("Engine.CustomDiary")
 -- !Name "Create a diary from the template file"
 -- !Section "Diary"
 -- !Description "Create diary from template file."
--- !Arguments "NewLine, String, Name of the file to import diary entries., {DiarySetup}"
+-- !Arguments "NewLine, String, [ NoMultiline ], {DiarySetup}, Name of the file to import diary entries."
 
 LevelFuncs.Engine.Node.DiaryImport = function(fileName)
 	CustomDiary.ImportDiary(fileName)
@@ -11,7 +11,7 @@ end
 
 -- !Name "Start / Stop the diary feature"
 -- !Section "Diary"
--- !Description "Starts or stops the diary feature. To be used with start in a new level to enable the diary."
+-- !Description "Starts or stops the diary feature. To be used at start of a new level to enable the diary."
 -- !Arguments "Enumeration, 30, [ Start | Stop ], Status"
 
 LevelFuncs.Engine.Node.DiaryStatus = function(value)
@@ -25,10 +25,156 @@ LevelFuncs.Engine.Node.DiaryStatus = function(value)
 	end
 end
 
+-- !Name "Create a diary"
+-- !Section "Diary"
+-- !Description "Start a new diary with specified name."
+-- !Arguments "NewLine, 100, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Object to call the diary with in Lara's inventory."
+-- !Arguments "Newline, SpriteSlots, 60, {TEN.Objects.ObjID.DIARY_SPRITES}, Background sprite sequence object ID"
+-- !Arguments "Number, 20, [ 0 | 9999 | 0 ],{0}, Sprite ID for background in sprite sequence\nRange[0 to 9999]"
+-- !Arguments "Color, 20, {TEN.Color(255,255,255)}, Color of the diary sprite"
+-- !Arguments "Newline, Number, 20, [ -1000 | 1000 | 2 ], {50}, Position X (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ -1000 | 1000 | 2 ], {50}, Position Y (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ 0 | 360 | 2 ], Rotation\nRange [0 to 360]"
+-- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale X (%)\nRange [0 to 1000]"
+-- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale Y (%)\nRange [0 to 1000]"
+-- !Arguments "NewLine, Enumeration, 35, [ Center | Center Top | Center Bottom | Center Left | Center Right | Top Left | Top Right | Bottom Left | Bottom Right ], {0}, Align mode"
+-- !Arguments "Enumeration, 22, [ Fit | Fill | Stretch ], Scale mode"
+-- !Arguments "Enumeration, 28, [ Opaque | Alpha Test | Additive | No Z Test | Subtractive | Wireframe | Exclude | Screen | Lighten | Alphablend ], {9}, Blend mode"
+-- !Arguments "Number, 15, [ 0 | 255 | 0 ], {255}, Alpha Blend for Diary sprite"
+-- !Arguments "NewLine, SoundEffects, 50, Page Flip Sound,{369}" "SoundEffects, 50, Exit Sound,{369}"
+
+LevelFuncs.Engine.Node.CreateDiary = function(object, objectIDbg, spriteIDbg, colorbg, posX, posY, rot, scaleX, scaleY, alignMode, scaleMode, blendMode, alpha, pageSound, exitSound)
+	local alignM = LevelFuncs.Engine.Node.GetDisplaySpriteAlignMode(alignMode)
+    local scaleM = LevelFuncs.Engine.Node.GetDisplaySpriteScaleMode(scaleMode)
+    local blendID = LevelFuncs.Engine.Node.GetBlendMode(blendMode)
+	local pos = TEN.Vec2(posX, posY)
+	local scale = TEN.Vec2(scaleX, scaleY)
+
+	CustomDiary.Create(object, objectIDbg, spriteIDbg, colorbg, pos, rot, scale, alignM, scaleM, blendID, alpha, pageSound, exitSound)
+	
+end
+
+-- !Name "Add background image to diary"
+-- !Section "Diary"
+-- !Description "Add post process effect to the diary background."
+-- !Arguments "NewLine, 100, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Object to call the diary with in Lara's inventory."
+-- !Arguments "Newline, SpriteSlots, 60, {TEN.Objects.ObjID.DIARY_SPRITES}, Background sprite sequence object ID"
+-- !Arguments "Number, 20, [ 0 | 9999 | 0 ], Sprite ID for background in sprite sequence\nRange[0 to 9999],{1}"
+-- !Arguments "Color, 20, {TEN.Color(255,0,0)}, Color of background sprite"
+-- !Arguments "Newline, Number, 20, [ -1000 | 1000 | 2 ], {50}, Position X (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ -1000 | 1000 | 2 ], {50}, Position Y (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ 0 | 360 | 2 ], Rotation\nRange [0 to 360]"
+-- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale X (%)\nRange [0 to 1000]"
+-- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale Y (%)\nRange [0 to 1000]"
+-- !Arguments "NewLine, Enumeration, 35, [ Center | Center Top | Center Bottom | Center Left | Center Right | Top Left | Top Right | Bottom Left | Bottom Right ], {0}, Align mode"
+-- !Arguments "Enumeration, 22, [ Fit | Fill | Stretch ], {2}, Scale mode"
+-- !Arguments "Enumeration, 28, [ Opaque | Alpha Test | Additive | No Z Test | Subtractive | Wireframe | Exclude | Screen | Lighten | Alphablend ], {9}, Blend mode"
+-- !Arguments "Number, 15, {128}, [ 0 | 255 | 2 ], Alpha [0 to 255]"
+
+LevelFuncs.Engine.Node.DiaryBackgroundImage = function(object, objectIDbg, spriteIDbg, colorbg, posX, posY, rot, scaleX, scaleY, alignMode, scaleMode, blendMode, alpha) 
+
+    local dataName = object .. "_diarydata"
+	local blendID = LevelFuncs.Engine.Node.GetBlendMode(blendMode)
+	local alignM = LevelFuncs.Engine.Node.GetDisplaySpriteAlignMode(alignMode)
+    local scaleM = LevelFuncs.Engine.Node.GetDisplaySpriteScaleMode(scaleMode)
+	
+	if GameVars.Engine.Diaries[dataName] then
+		local pos = TEN.Vec2(posX, posY)
+		local scale = TEN.Vec2(scaleX, scaleY)
+
+		local diary = CustomDiary.Get(object)
+		diary:AddBackground(objectIDbg, spriteIDbg, colorbg, pos, rot, scale, alignM, scaleM, blendID, alpha)
+	end
+end
+
+-- !Name "Customize notifications for the diary"
+-- !Section "Diary"
+-- !Description "Customize notification systems for the diary."
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to add notification." "Numerical, 20, [ 1 | 65535 ], {3}, Notification Time"
+-- !Arguments "Newline, SpriteSlots, 60, {TEN.Objects.ObjID.DIARY_SPRITES}, Sprite sequence object ID for notification"
+-- !Arguments "Number, 20, [ 0 | 9999 | 0 ], Sprite ID for background in sprite sequence\nRange[0 to 9999],{2}"
+-- !Arguments "Color, 20, {TEN.Color(255,255,255)}, Color of background sprite"
+-- !Arguments "Newline, Number, 20, [ -1000 | 1000 | 2 ], {95}, Position X (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ -1000 | 1000 | 2 ],{95}, Position Y (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
+-- !Arguments "Number, 20, [ 0 | 360 | 2 ], Rotation\nRange [0 to 360]"
+-- !Arguments "Number, 20, {5}, [ 0 | 1000 | 2 ], Scale X (%)\nRange [0 to 1000]"
+-- !Arguments "Number, 20, {5}, [ 0 | 1000 | 2 ], Scale Y (%)\nRange [0 to 1000]"
+-- !Arguments "NewLine, Enumeration, 35, [ Center | Center Top | Center Bottom | Center Left | Center Right | Top Left | Top Right | Bottom Left | Bottom Right ], {0}, Align mode"
+-- !Arguments "Enumeration, 22, [ Fit | Fill | Stretch ], Scale mode"
+-- !Arguments "Enumeration, 28, [ Opaque | Alpha Test | Additive | No Z Test | Subtractive | Wireframe | Exclude | Screen | Lighten | Alphablend ], {9}, Blend mode"
+-- !Arguments "NewLine, SoundEffects, 100, Notification Sound,{369}" 
+LevelFuncs.Engine.Node.DiaryCustomizeNotifications = function(object, notificationTime, objectID, spriteID, color, posX, posY, rot, scaleX, scaleY, alignMode, scaleMode, blendMode, notificationSound)
+	
+	local dataName = object .. "_diarydata"
+	local alignM = LevelFuncs.Engine.Node.GetDisplaySpriteAlignMode(alignMode)
+    local scaleM = LevelFuncs.Engine.Node.GetDisplaySpriteScaleMode(scaleMode)
+    local blendID = LevelFuncs.Engine.Node.GetBlendMode(blendMode)
+	local pos = TEN.Vec2(posX, posY)
+	local scale = TEN.Vec2(scaleX, scaleY)
+
+	if GameVars.Engine.Diaries[dataName] then
+		local diary = CustomDiary.Get(object)
+		diary:CustomizeNotification(notificationTime, objectID, spriteID, color, pos, rot, scale, alignM, scaleM, blendID, notificationSound)
+	end
+
+end
+
+-- !Name "Customize Page Numbers for the diary"
+-- !Section "Diary"
+-- !Description "Add a new entry with to a diary."
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to customize page numbers." "Numerical, 20, Entry Type, [ 1 | 2 ], {2}, Type 1 is just page number\nType 2 is formatted as\n (prefix page number separator total page numbers)"
+-- !Arguments "NewLine, String, 35, [ NoMultiline ], Prefix" "String, 35, [ NoMultiline ], Separator" "Numerical, 15, {98}, X position, [ 0 | 100 ]" "Numerical, 15, {95}, Y position, [ 0 | 100 ]"
+-- !Arguments "NewLine, Enumeration, 20, [ Left | Center | Right ], {2}, Horizontal alignment"
+-- !Arguments "Enumeration, 40, [ Flat | Shadow | Blinking | Shadow + Blinking ], {1}, Effects"
+-- !Arguments "Numerical, 20, {0.5}, [ 0 | 9 | 2 | 0.1 ], Scale" "Color, 20, {TEN.Color(255,255,255)}, Text color"
+
+LevelFuncs.Engine.Node.DiaryCustomizePageNumbers = function(object, type, prefix, separator, textX, textY, textAlignment, textEffects, textScale, textColor)
+	
+	local dataName = object .. "_diarydata"
+	if GameVars.Engine.Diaries[dataName] then
+		local pos = TEN.Vec2(textX, textY)
+		local options = {}
+		if (textEffects == 1 or textEffects == 3) then table.insert(options, TEN.Strings.DisplayStringOption.SHADOW) end
+		if (textEffects == 2 or textEffects == 3) then table.insert(options, TEN.Strings.DisplayStringOption.BLINK) end
+		if (textAlignment == 1) then table.insert(options, TEN.Strings.DisplayStringOption.CENTER) end
+		if (textAlignment == 2) then table.insert(options, TEN.Strings.DisplayStringOption.RIGHT) end
+
+		local diary = CustomDiary.Get(object)
+		diary:CustomizePageNumbers(type, prefix, separator, pos, options, textScale, textColor)
+	end
+	
+end
+
+-- !Name "Customize Controls for the diary"
+-- !Section "Diary"
+-- !Description "Add a new entry with to a diary."
+-- !Arguments "NewLine, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to customize controls for."
+-- !Arguments "NewLine, Numerical, 33, [ 0 | 100 ], {5}, X position" "Numerical, 33, [ 0 | 100 ] {95}, Y position"
+-- !Arguments "NewLine, Enumeration, 20, [ Left | Center | Right ], Horizontal alignment"
+-- !Arguments "Enumeration, 40, [ Flat | Shadow | Blinking | Shadow + Blinking ], {1}, Effects"
+-- !Arguments "Numerical, 20, {1}, [ 0 | 9 | 2 | 0.1 ], {0.5}, Scale" "Color, 20, {TEN.Color(255,255,255)}, Text color"
+
+LevelFuncs.Engine.Node.DiaryCustomizeControls = function(object, textX, textY, textAlignment, textEffects, textScale, textColor)
+	
+	local dataName = object .. "_diarydata"
+	if GameVars.Engine.Diaries[dataName] then
+		local pos = TEN.Vec2(textX, textY)
+		local options = {}
+		if (textEffects == 1 or textEffects == 3) then table.insert(options, TEN.Strings.DisplayStringOption.SHADOW) end
+		if (textEffects == 2 or textEffects == 3) then table.insert(options, TEN.Strings.DisplayStringOption.BLINK) end
+		if (textAlignment == 1) then table.insert(options, TEN.Strings.DisplayStringOption.CENTER) end
+		if (textAlignment == 2) then table.insert(options, TEN.Strings.DisplayStringOption.RIGHT) end
+
+		local diary = CustomDiary.Get(object)
+		diary:CustomizeControls(pos, options, textScale, textColor)
+	end
+	
+end
+
 -- !Name "Add a text entry to the specified page of the diary"
 -- !Section "Diary"
 -- !Description "Add a new entry with to a diary."
--- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], Diary Object to add an entry., {TEN.Objects.ObjID.DIARY_ITEM}" "Numerical, 20, Diary Page, [ 1 | 65535 ], {1}"
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to add an entry." "Numerical, 20, [ 1 | 65535 ], {1}, Page Number to text entry."
 -- !Arguments "NewLine, String, 70, [ Multiline ], Diary entry" "Numerical, 15, X position, [ 0 | 100 ]" "Numerical, 15, Y position, [ 0 | 100 ]"
 -- !Arguments "NewLine, Enumeration, 20, [ Left | Center | Right ], Horizontal alignment"
 -- !Arguments "Enumeration, 40, [ Flat | Shadow | Blinking | Shadow + Blinking ], Effects"
@@ -47,7 +193,7 @@ LevelFuncs.Engine.Node.DiaryAddTextEntry = function(object, pageIndex, text, tex
 		if (textAlignment == 2) then table.insert(options, TEN.Strings.DisplayStringOption.RIGHT) end
 
 		local diary = CustomDiary.Get(object)
-		diary:addTextEntry(pageIndex, text, pos, options, textScale, textColor)
+		diary:AddTextEntry(pageIndex, text, pos, options, textScale, textColor)
 	end
 	
 end
@@ -55,16 +201,16 @@ end
 -- !Name "Add an image entry to the specified page of the diary"
 -- !Section "Diary"
 -- !Description "Add a new entry with to a diary."
--- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], Diary Object to add an entry., {TEN.Objects.ObjID.DIARY_ITEM}" "Numerical, 20, Diary Page, [ 1 | 65535 ], {1}"
--- !Arguments "Newline, SpriteSlots, 60, Sprite sequence object ID, {TEN.Objects.ObjID.DIARY_ENTRY_SPRITES}"
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to add an entry." "Numerical, 20, [ 1 | 65535 ], {1}, Page Number to add image entry."
+-- !Arguments "Newline, SpriteSlots, 60, {TEN.Objects.ObjID.DIARY_ENTRY_SPRITES}, Sprite sequence object ID"
 -- !Arguments "Number, 20, [ 0 | 9999 | 0 ], Sprite ID in sprite sequence\nRange[0 to 9999],{0}"
--- !Arguments "Color, 20, Color of background sprite"
+-- !Arguments "Color, 20, {TEN.Color(255,255,255)}, Color of image entry sprite"
 -- !Arguments "Newline, Number, 20, [ -1000 | 1000 | 2 ], Position X (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
 -- !Arguments "Number, 20, [ -1000 | 1000 | 2 ], Position Y (%)\nRange [-1000 to 1000]\nVisible range [0 to 100]"
 -- !Arguments "Number, 20, [ 0 | 360 | 2 ], Rotation\nRange [0 to 360]"
 -- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale X (%)\nRange [0 to 1000]"
 -- !Arguments "Number, 20, {100}, [ 0 | 1000 | 2 ], Scale Y (%)\nRange [0 to 1000]"
--- !Arguments "NewLine, Enumeration, 35, [ Center | Center Top | Center Bottom | Center Left | Center Right | Top Left | Top Right | Bottom Left | Bottom Right ], {3}, Align mode"
+-- !Arguments "NewLine, Enumeration, 35, [ Center | Center Top | Center Bottom | Center Left | Center Right | Top Left | Top Right | Bottom Left | Bottom Right ], {0}, Align mode"
 -- !Arguments "Enumeration, 22, [ Fit | Fill | Stretch ], Scale mode"
 -- !Arguments "Enumeration, 28, [ Opaque | Alpha Test | Additive | No Z Test | Subtractive | Wireframe | Exclude | Screen | Lighten | Alphablend ], {9}, Blend mode"
 
@@ -79,7 +225,7 @@ LevelFuncs.Engine.Node.DiaryAddImageEntry = function(object, pageIndex, objectID
 
 	if GameVars.Engine.Diaries[dataName] then
 		local diary = CustomDiary.Get(object)
-		diary:addImageEntry(pageIndex, objectIDbg, spriteIDbg, colorbg, pos, rot, scale, alignM, scaleM, blendID)
+		diary:AddImageEntry(pageIndex, objectIDbg, spriteIDbg, colorbg, pos, rot, scale, alignM, scaleM, blendID)
 	end
 
 end
@@ -87,44 +233,44 @@ end
 -- !Name "Add/Update narration to a diary page."
 -- !Section "Diary"
 -- !Description "Add narration soundtrack to specified page number."
--- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], Diary Object to unlock pages., {TEN.Objects.ObjID.DIARY_ITEM}" "Numerical, 20, Page Number to add narration, [ 1 | 65535 ], {1}"
--- !Arguments "NewLine, SoundTracks, Name of the audiotrack to check"
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to add narration to." "Numerical, 20, [ 1 | 65535 ], {1}, Page Number to add narration."
+-- !Arguments "NewLine, SoundTracks, Name of the audiotrack to add to page"
 
 LevelFuncs.Engine.Node.DiaryNarrationPages = function(object, pageIndex, trackName)
 	local dataName = object .. "_diarydata"
 	if GameVars.Engine.Diaries[dataName] then
 		local diary = CustomDiary.Get(object)
-		diary:addNarration(pageIndex, trackName)
+		diary:AddNarration(pageIndex, trackName)
 	end
 end
 
 -- !Name "Unlock diary pages till..."
 -- !Section "Diary"
 -- !Description "Unlock the diary pages til specified page number."
--- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], Diary Object to unlock pages., {TEN.Objects.ObjID.DIARY_ITEM}" 
--- !Arguments "Numerical, 20, Page Number to unlock till, [ 1 | 65535 ], {1}"
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to unlock pages." 
+-- !Arguments "Numerical, 20, [ 1 | 65535 ], {1}, Page Number to unlock till"
 -- !Arguments "NewLine, Boolean, Notification"
 LevelFuncs.Engine.Node.DiaryUnlockPages = function(object, index, notification)
 	
 	local dataName = object .. "_diarydata"
 	if GameVars.Engine.Diaries[dataName] then
 		local diary = CustomDiary.Get(object)
-		diary:unlockPages(index, notification)
+		diary:UnlockPages(index, notification)
 	end
 end
 
 -- !Name "Clear diary page"
 -- !Section "Diary"
 -- !Description "This function clears the specified diary page."
--- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], Diary Object to clear the page for, {TEN.Objects.ObjID.DIARY_ITEM}" 
--- !Arguments "Numerical, 20, Page Number to clear, [ 1 | 65535 ], {1}"
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary Object to clear the page for" 
+-- !Arguments "Numerical, 20, [ 1 | 65535 ], {1}, Page Number to clear"
 
 LevelFuncs.Engine.Node.DiaryClearPage = function(object, index)
 	
 	local dataName = object .. "_diarydata"
 	if GameVars.Engine.Diaries[dataName] then
 		local diary = CustomDiary.Get(object)
-		diary:clearPage(index)
+		diary:ClearPage(index)
 	end
 
 end
@@ -132,21 +278,21 @@ end
 -- !Name "Show Diary at specified page"
 -- !Section "Diary"
 -- !Description "This function displays the diary at the specified page."
--- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], Diary to display., {TEN.Objects.ObjID.DIARY_ITEM}" 
--- !Arguments "Numerical, 20, Page Number to display, [ 1 | 65535 ], {1}"
+-- !Arguments "NewLine, 80, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary to display." 
+-- !Arguments "Numerical, 20, [ 1 | 65535 ], {1}, Page Number to display"
 LevelFuncs.Engine.Node.ShowDiary = function(object, pageIndex)
 	
 	local dataName = object .. "_diarydata"
 	if GameVars.Engine.Diaries[dataName] then
 		local diary = CustomDiary.Get(object)
-		diary:showDiary(pageIndex)
+		diary:ShowDiary(pageIndex)
 	end
 end
 
 -- !Name "Clear a diary"
 -- !Section "Diary"
 -- !Description "Clear a diary. Please do this once a diary has served its purpose. \n It helps reduce the savegame size."
--- !Arguments "NewLine, WadSlots, [ _ITEM ], Diary to clear., {TEN.Objects.ObjID.DIARY_ITEM}"
+-- !Arguments "NewLine, WadSlots, [ _ITEM ], {TEN.Objects.ObjID.DIARY_ITEM}, Diary to clear."
 
 LevelFuncs.Engine.Node.ClearDiary = function(object)
 
