4d379b7200d6f4eb4a9680282f8208f856a39ec0
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-10-22 21:59:41 +0200

    Update GlobalAssemblyInfo.cs

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.9.2")]
-[assembly: AssemblyFileVersion("1.9.2")]
+[assembly: AssemblyVersion("1.10.0")]
+[assembly: AssemblyFileVersion("1.10.0")]
