cbef7f2abfd2fbb0ff28598aef4f299c12db5fa5
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2026-04-11 17:28:05 -0400

    Add statistics nodes Add slot for sprites

---
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Statistics.lua
@@ -0,0 +1,43 @@
+local Statistics = require("Engine.RingInventory.Statistics")
+local count = 0
+
+-- !Name "Shows end of level statistics"
+-- !Section "User interface"
+-- !Description "Show end of level statistics."
+-- !Arguments "NewLine, Numerical, 20, [ 0 | 256 ], Next level"
+-- !Arguments "80, SoundTracks, Name of the audiotrack to play"
+LevelFuncs.Engine.Node.ShowStatistics = function(level, track)
+	
+    if count == 0 then
+        Statistics.SetType(false)
+        TEN.Sound.PlayAudioTrack(track)
+        Statistics.SetEndStatistics(true)
+    end
+
+    count = count + 1
+    if count == 3 then
+        count = 0
+        TEN.Flow.EndLevel(level)
+    end
+	
+end
+
+-- !Name "Shows end of game statistics"
+-- !Section "User interface"
+-- !Description "Show end of game statistics."
+-- !Arguments "NewLine, 100, SoundTracks, Name of the audiotrack to play"
+LevelFuncs.Engine.Node.ShowEndGameStatistics = function(track)
+	
+    if count == 0 then
+        Statistics.SetType(true)
+        TEN.Sound.PlayAudioTrack(track)
+        Statistics.SetEndStatistics(true)
+    end
+
+    count = count + 1
+    if count == 3 then
+        count = 0
+        TEN.Flow.EndLevel(999)
+    end
+
+end
