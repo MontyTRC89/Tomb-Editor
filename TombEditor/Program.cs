30a0066cce19cc4040c64bf5fc1dfc71c2436b29
MontyTRC89 <montyhammet@hotmail.it>
2026-05-27 15:37:33 +0200

    Use SVG icons in WPF views and enable PerMonitorV2 DPI awareness

--- TombEditor/Program.cs
+++ TombEditor/Program.cs
@@ -90,7 +90,7 @@ namespace TombEditor
                     // Setup application
                     Application.EnableVisualStyles();
                     Application.SetDefaultFont(new System.Drawing.Font("Segoe UI", 8.25f));
-                    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                    Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
                     Application.SetCompatibleTextRenderingDefault(false);
                     Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
                     Application.ThreadException += (sender, e) =>
