425eccc9698508b750baf8c95415035b142aab77
MontyTRC89 <montyhammet@hotmail.it>
2026-06-03 10:38:45 +0200

    Level settings WPF: Misc, Sky/Font, Static meshes tabs

--- TombEditor/Features/Dialogs/LevelSettings/LevelSettingsWindowViewModel.cs
+++ TombEditor/Features/Dialogs/LevelSettings/LevelSettingsWindowViewModel.cs
@@ -1,15 +1,21 @@
 #nullable enable
 
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Numerics;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MvvmDialogs;
+using TombLib.Controls;
 using TombLib.LevelData;
 using TombLib.Utils;
+using TombLib.Wad.Catalog;
 using TombLib.WPF;
 using static TombLib.LevelData.TRVersion;
 using LevelSettingsData = TombLib.LevelData.LevelSettings;
+using WpfColor = System.Windows.Media.Color;
 
 namespace TombEditor.Features.Dialogs.LevelSettings
 {
@@ -39,6 +45,8 @@ namespace TombEditor.Features.Dialogs.LevelSettings
                 .Select(t => LevelSettingsData.VariableCreate(t))
                 .Select(name => new PathVariableRow(name, _settings.ParseVariables(name)))
                 .ToList();
+
+            BuildStaticMeshMerges();
         }
 
         // Game tab.
@@ -54,8 +62,11 @@ namespace TombEditor.Features.Dialogs.LevelSettings
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(IsTRNG));
                 OnPropertyChanged(nameof(IsTombEngine));
+                OnPropertyChanged(nameof(IsNotTombEngine));
                 OnPropertyChanged(nameof(IsTR4Native));
                 OnPropertyChanged(nameof(IsTR5));
+                OnPropertyChanged(nameof(Supports16BitDithering));
+                OnPropertyChanged(nameof(SupportsFontAndSky));
             }
         }
 
@@ -77,8 +88,93 @@ namespace TombEditor.Features.Dialogs.LevelSettings
 
         public bool IsTRNG => _settings.GameVersion == Game.TRNG;
         public bool IsTombEngine => _settings.GameVersion == Game.TombEngine;
+        public bool IsNotTombEngine => _settings.GameVersion != Game.TombEngine;
         public bool IsTR4Native => _settings.GameVersion.Native() == Game.TR4;
         public bool IsTR5 => _settings.GameVersion == Game.TR5;
+        public bool Supports16BitDithering => _settings.GameVersion.Supports16BitDithering();
+        public bool SupportsFontAndSky => _settings.GameVersion.SupportsFontAndSkySettings();
+
+        // Sky & font tab.
+
+        public IReadOnlyList<Tr5WeatherType> Tr5WeatherTypes { get; } = Enum.GetValues(typeof(Tr5WeatherType)).Cast<Tr5WeatherType>().ToList();
+        public IReadOnlyList<Tr5LaraType> Tr5LaraTypes { get; } = Enum.GetValues(typeof(Tr5LaraType)).Cast<Tr5LaraType>().ToList();
+
+        public Tr5WeatherType Tr5WeatherType { get => _settings.Tr5WeatherType; set => SetSetting(_settings.Tr5WeatherType, value, v => _settings.Tr5WeatherType = v); }
+        public Tr5LaraType Tr5LaraType { get => _settings.Tr5LaraType; set => SetSetting(_settings.Tr5LaraType, value, v => _settings.Tr5LaraType = v); }
+
+        public bool FontUseCustom
+        {
+            get => !string.IsNullOrEmpty(_settings.FontTextureFilePath);
+            set => ToggleCustomResource(value, "Font.pc", () => _settings.FontTextureFilePath, p => _settings.FontTextureFilePath = p, nameof(FontUseCustom), nameof(FontTextureFilePath));
+        }
+        public string FontTextureFilePath { get => _settings.FontTextureFilePath ?? string.Empty; set => SetOptionalPath(value, () => _settings.FontTextureFilePath, p => _settings.FontTextureFilePath = p, nameof(FontTextureFilePath)); }
+
+        public bool SkyUseCustom
+        {
+            get => !string.IsNullOrEmpty(_settings.SkyTextureFilePath);
+            set => ToggleCustomResource(value, "pcsky.raw", () => _settings.SkyTextureFilePath, p => _settings.SkyTextureFilePath = p, nameof(SkyUseCustom), nameof(SkyTextureFilePath));
+        }
+        public string SkyTextureFilePath { get => _settings.SkyTextureFilePath ?? string.Empty; set => SetOptionalPath(value, () => _settings.SkyTextureFilePath, p => _settings.SkyTextureFilePath = p, nameof(SkyTextureFilePath)); }
+
+        public bool Tr5SpritesUseCustom
+        {
+            get => !string.IsNullOrEmpty(_settings.Tr5ExtraSpritesFilePath);
+            set => ToggleCustomResource(value, "Extra.Tr5.pc", () => _settings.Tr5ExtraSpritesFilePath, p => _settings.Tr5ExtraSpritesFilePath = p, nameof(Tr5SpritesUseCustom), nameof(Tr5ExtraSpritesFilePath));
+        }
+        public string Tr5ExtraSpritesFilePath { get => _settings.Tr5ExtraSpritesFilePath ?? string.Empty; set => SetOptionalPath(value, () => _settings.Tr5ExtraSpritesFilePath, p => _settings.Tr5ExtraSpritesFilePath = p, nameof(Tr5ExtraSpritesFilePath)); }
+
+        // Misc tab.
+
+        public IReadOnlyList<string> SampleRates { get; } = new[] { "11025", "22050", "44100", "48000" };
+        public IReadOnlyList<string> LightQualities { get; } = new[] { "Low", "Medium", "High" };
+
+        public double TexturePadding { get => _settings.TexturePadding; set => SetSetting<double>(_settings.TexturePadding, value, v => _settings.TexturePadding = (int)v); }
+        public bool Dither16BitTextures { get => _settings.Dither16BitTextures; set => SetSetting(_settings.Dither16BitTextures, value, v => _settings.Dither16BitTextures = v); }
+        public bool CompressTextures { get => _settings.CompressTextures; set => SetSetting(_settings.CompressTextures, value, v => _settings.CompressTextures = v); }
+        public bool AgressiveTexturePacking { get => _settings.AgressiveTexturePacking; set => SetSetting(_settings.AgressiveTexturePacking, value, v => _settings.AgressiveTexturePacking = v); }
+        public bool AgressiveFloordataPacking { get => _settings.AgressiveFloordataPacking; set => SetSetting(_settings.AgressiveFloordataPacking, value, v => _settings.AgressiveFloordataPacking = v); }
+        public bool Room32BitLighting { get => _settings.Room32BitLighting; set => SetSetting(_settings.Room32BitLighting, value, v => _settings.Room32BitLighting = v); }
+        public bool RemapAnimatedTextures { get => _settings.RemapAnimatedTextures; set => SetSetting(_settings.RemapAnimatedTextures, value, v => _settings.RemapAnimatedTextures = v); }
+        public bool RearrangeVerticalRooms { get => _settings.RearrangeVerticalRooms; set => SetSetting(_settings.RearrangeVerticalRooms, value, v => _settings.RearrangeVerticalRooms = v); }
+        public bool RemoveUnusedObjects { get => _settings.RemoveUnusedObjects; set => SetSetting(_settings.RemoveUnusedObjects, value, v => _settings.RemoveUnusedObjects = v); }
+        public bool OverrideIndividualLightQualitySettings { get => _settings.OverrideIndividualLightQualitySettings; set => SetSetting(_settings.OverrideIndividualLightQualitySettings, value, v => _settings.OverrideIndividualLightQualitySettings = v); }
+        public bool AutoAssignSoundsIfNoSelection { get => _settings.AutoAssignSoundsIfNoSelection; set => SetSetting(_settings.AutoAssignSoundsIfNoSelection, value, v => _settings.AutoAssignSoundsIfNoSelection = v); }
+
+        public bool EnableCustomSampleRate
+        {
+            get => _settings.EnableCustomSampleRate;
+            set { if (SetSetting(_settings.EnableCustomSampleRate, value, v => _settings.EnableCustomSampleRate = v)) OnPropertyChanged(nameof(CanEditSampleRate)); }
+        }
+        public bool CanEditSampleRate => _settings.EnableCustomSampleRate;
+
+        public int DefaultLightQualityIndex
+        {
+            get => _settings.DefaultLightQuality == LightQuality.Default ? 0 : (int)_settings.DefaultLightQuality - 1;
+            set { _settings.DefaultLightQuality = (LightQuality)(value + 1); OnPropertyChanged(); }
+        }
+
+        public string SelectedSampleRate
+        {
+            get => _settings.CustomSampleRate.ToString();
+            set { if (int.TryParse(value, out int rate)) { _settings.CustomSampleRate = rate; OnPropertyChanged(); } }
+        }
+
+        public WpfColor AmbientLightColor => (_settings.DefaultAmbientLight * 0.5f).ToWPFColor();
+
+        [RelayCommand]
+        private void PickAmbientLight()
+        {
+            using var dialog = new RealtimeColorDialog
+            {
+                Color = (_settings.DefaultAmbientLight * 0.5f).ToWinFormsColor(),
+                FullOpen = true
+            };
+            if (dialog.ShowDialog(Owner) != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            _settings.DefaultAmbientLight = dialog.Color.ToFloat3Color() * 2.0f;
+            OnPropertyChanged(nameof(AmbientLightColor));
+        }
 
         // Browse commands.
 
@@ -106,14 +202,68 @@ namespace TombEditor.Features.Dialogs.LevelSettings
         private void BrowseLua()
             => SetIfBrowsed(LevelFileDialog.BrowseFile(Owner, _settings, _settings.TenLuaScriptFile, "Select the Lua script file", new[] { new FileFormat("Lua script", "lua") }, VariableType.LevelDirectory, false), nameof(TenLuaScriptFile), v => TenLuaScriptFile = v);
 
+        [RelayCommand]
+        private void BrowseFont() => BrowseResource(() => _settings.FontTextureFilePath, p => _settings.FontTextureFilePath = p, "Select a font texture", nameof(FontTextureFilePath), nameof(FontUseCustom));
+
+        [RelayCommand]
+        private void BrowseSky() => BrowseResource(() => _settings.SkyTextureFilePath, p => _settings.SkyTextureFilePath = p, "Select a sky texture", nameof(SkyTextureFilePath), nameof(SkyUseCustom));
+
+        [RelayCommand]
+        private void BrowseTr5Sprites() => BrowseResource(() => _settings.Tr5ExtraSpritesFilePath, p => _settings.Tr5ExtraSpritesFilePath = p, "Select a TR5 extra sprites texture", nameof(Tr5ExtraSpritesFilePath), nameof(Tr5SpritesUseCustom));
+
+        // Static meshes tab.
+
+        public ObservableCollection<StaticMeshMergeRow> StaticMeshMerges { get; } = new();
+
+        [RelayCommand]
+        private void SelectAllStatics() => ToggleStatics(true, bypassShatterable: false);
+
+        [RelayCommand]
+        private void SelectAllButShatterStatics() => ToggleStatics(true, bypassShatterable: true);
+
+        [RelayCommand]
+        private void DeselectAllStatics() => ToggleStatics(false, bypassShatterable: false);
+
+        private void ToggleStatics(bool value, bool bypassShatterable)
+        {
+            foreach (var row in StaticMeshMerges)
+            {
+                if (value && bypassShatterable && TrCatalog.IsStaticShatterable(_settings.GameVersion, row.MeshId))
+                {
+                    row.Merge = false;
+                    continue;
+                }
+
+                row.Merge = value;
+                if (!value)
+                    row.InterpretShadesAsEffect = false;
+            }
+        }
+
+        private void BuildStaticMeshMerges()
+        {
+            StaticMeshMerges.Clear();
+            foreach (var staticMesh in _settings.WadGetAllStatics())
+            {
+                uint typeId = staticMesh.Value.Id.TypeId;
+                var existing = _settings.AutoStaticMeshMerges.FirstOrDefault(e => e.meshId.Equals(typeId));
+                StaticMeshMerges.Add(new StaticMeshMergeRow(existing ?? new AutoStaticMeshMergeEntry(typeId, false, false, false, false, _settings)));
+            }
+        }
+
         // Dialog commands.
 
         [RelayCommand]
-        private void Apply() => _editor.UpdateLevelSettings(_settings.Clone());
+        private void Apply()
+        {
+            CommitCollections();
+            _editor.UpdateLevelSettings(_settings.Clone());
+        }
 
         [RelayCommand]
         private void Ok()
         {
+            CommitCollections();
             _editor.UpdateLevelSettings(_settings.Clone());
             DialogResult = true;
         }
@@ -125,12 +275,51 @@ namespace TombEditor.Features.Dialogs.LevelSettings
 
         private static System.Windows.Forms.IWin32Window Owner => WinFormsDialogHelper.GetOpenFormOwner();
 
-        private void SetSetting<T>(T current, T value, System.Action<T> setter, [System.Runtime.CompilerServices.CallerMemberName] string? propertyName = null)
+        private void CommitCollections()
+        {
+            _settings.AutoStaticMeshMerges.Clear();
+            foreach (var row in StaticMeshMerges)
+                if (row.Merge)
+                    _settings.AutoStaticMeshMerges.Add(row.Entry.Clone()); // Clone(), otherwise merge entries won't update
+        }
+
+        private string GetLevelResourcePath(string file)
+            => LevelSettingsData.VariableCreate(VariableType.LevelDirectory) + LevelSettingsData.Dir + file;
+
+        private void ToggleCustomResource(bool useCustom, string defaultFile, Func<string?> get, Action<string?> set, string customProp, string pathProp)
+        {
+            if (useCustom == !string.IsNullOrEmpty(get()))
+                return;
+            set(useCustom ? GetLevelResourcePath(defaultFile) : null);
+            OnPropertyChanged(customProp);
+            OnPropertyChanged(pathProp);
+        }
+
+        private void SetOptionalPath(string value, Func<string?> get, Action<string?> set, string pathProp)
+        {
+            if (string.IsNullOrEmpty(get()) || string.IsNullOrEmpty(value) || get() == value)
+                return; // auto mode, or no real change
+            set(value);
+            OnPropertyChanged(pathProp);
+        }
+
+        private void BrowseResource(Func<string?> get, Action<string?> set, string title, string pathProp, string customProp)
+        {
+            string? result = LevelFileDialog.BrowseFile(Owner, _settings, get(), title, LevelSettingsData.FileFormatsLoadRawExtraTexture, VariableType.LevelDirectory, false);
+            if (result == null)
+                return;
+            set(result);
+            OnPropertyChanged(pathProp);
+            OnPropertyChanged(customProp);
+        }
+
+        private bool SetSetting<T>(T current, T value, System.Action<T> setter, [System.Runtime.CompilerServices.CallerMemberName] string? propertyName = null)
         {
             if (EqualityComparer<T>.Default.Equals(current, value))
-                return;
+                return false;
             setter(value);
             OnPropertyChanged(propertyName);
+            return true;
         }
 
         private static void SetIfBrowsed(string? result, string propertyName, System.Action<string> apply)
@@ -140,5 +329,20 @@ namespace TombEditor.Features.Dialogs.LevelSettings
         }
 
         public sealed record PathVariableRow(string Variable, string Value);
+
+        public partial class StaticMeshMergeRow : ObservableObject
+        {
+            public AutoStaticMeshMergeEntry Entry { get; }
+
+            public StaticMeshMergeRow(AutoStaticMeshMergeEntry entry) => Entry = entry;
+
+            public string StaticMesh => Entry.StaticMesh;
+            public uint MeshId => Entry.meshId;
+
+            public bool Merge { get => Entry.Merge; set { if (Entry.Merge == value) return; Entry.Merge = value; OnPropertyChanged(); } }
+            public bool InterpretShadesAsEffect { get => Entry.InterpretShadesAsEffect; set { if (Entry.InterpretShadesAsEffect == value) return; Entry.InterpretShadesAsEffect = value; OnPropertyChanged(); } }
+            public bool TintAsAmbient { get => Entry.TintAsAmbient; set { if (Entry.TintAsAmbient == value) return; Entry.TintAsAmbient = value; OnPropertyChanged(); } }
+            public bool ClearShades { get => Entry.ClearShades; set { if (Entry.ClearShades == value) return; Entry.ClearShades = value; OnPropertyChanged(); } }
+        }
     }
 }
