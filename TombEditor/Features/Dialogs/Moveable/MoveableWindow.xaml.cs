465c106ee67d0864671e20d0ceba6f85f8d70bdf
MontyTRC89 <montyhammet@hotmail.it>
2026-06-04 13:55:37 +0200

    MoveableWindow: drop redundant HookModalAutoClose (closes via md:DialogServiceViews.IsRegistered); keep window-config persistence

--- TombEditor/Features/Dialogs/Moveable/MoveableWindow.xaml.cs
+++ TombEditor/Features/Dialogs/Moveable/MoveableWindow.xaml.cs
@@ -10,7 +10,6 @@ public partial class MoveableWindow : Window
 	public MoveableWindow()
 	{
 		InitializeComponent();
-		this.HookModalAutoClose();
 		Loaded += (_, _) => WindowConfiguration.ConfigureWindow(this, Editor.Instance.Configuration, "FormMoveable");
 	}
 }
