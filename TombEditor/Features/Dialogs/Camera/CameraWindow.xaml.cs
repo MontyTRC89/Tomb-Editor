787cce4a8aabf6c6f854fa40a10057ddfc3f55c0
MontyTRC89 <montyhammet@hotmail.it>
2026-06-04 13:33:46 +0200

    CameraWindow: align to shared-window template (trivial code-behind, close via md:DialogServiceViews.IsRegistered, drop redundant HookModalAutoClose)

--- TombEditor/Features/Dialogs/Camera/CameraWindow.xaml.cs
+++ TombEditor/Features/Dialogs/Camera/CameraWindow.xaml.cs
@@ -1,15 +1,9 @@
-#nullable enable
-
 using System.Windows;
-using TombLib.WPF;
 
 namespace TombEditor.Features.Dialogs.Camera;
 
 public partial class CameraWindow : Window
 {
 	public CameraWindow()
-	{
-		InitializeComponent();
-		this.HookModalAutoClose();
-	}
+		=> InitializeComponent();
 }
