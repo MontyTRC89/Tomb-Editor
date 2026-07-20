MontyTRC89/Tomb-Editor

commit b9de93159d39da7f1d222ee312097fccc15e72ea

Update TRX presets (#1256)
* Fix TRX sound map size

This resolves being unable to use sound IDs above the OG sound map size
without also having to define all other lower IDs.

* Update TRX presets

This updates the TR1X and TR2X presets to 1.9.3.

TombLib/TombLib/LevelData/Compilers/Wad.cs
@@ -821,7 +821,7 @@ namespace TombLib.LevelData.Compilers
             if (_level.IsNG)
                 _soundMapSize = _limits[Limit.NG_SoundMapSize];
             else if (_level.IsTRX)
-                _soundMapSize = Math.Max(_finalSoundInfosList.Count, _limits[Limit.SoundMapSize]);
+                _soundMapSize = Math.Max(_finalSoundInfosList.Max(s => s.Id) + 1, _limits[Limit.SoundMapSize]);
             else
                 _soundMapSize = _limits[Limit.SoundMapSize];
 

TombIDE/TombIDE.ProjectMaster/Services/ArchiveCreation/TRXArchiveService.cs
@@ -17,7 +17,8 @@ public sealed class TRXArchiveService : GameArchiveServiceBase
 		Path.Combine(engineDirectory, "music"),
 		Path.Combine(engineDirectory, "cfg"),
 		Path.Combine(engineDirectory, "data"),
-		Path.Combine(engineDirectory, "shaders")
+		Path.Combine(engineDirectory, "shaders"),
+		Path.Combine(engineDirectory, "game"),
 	];
 
 	protected override IReadOnlyList<string> GetImportantFiles(string engineDirectory) => [

TombIDE/TombIDE.ProjectMaster/Services/EngineUpdate/TRXUpdateService.cs
@@ -17,7 +17,7 @@ namespace TombIDE.ProjectMaster.Services.EngineUpdate;
 /// </summary>
 public sealed class TRXUpdateService : IEngineUpdateService
 {
-	private static readonly Version MinAutoUpdateVersion = new(1, 3, 1);
+	private static readonly Version MinAutoUpdateVersion = new(1, 9, 0);
 
 	private readonly IFileExtractionService _fileExtractionService;
 	private readonly TRVersion.Game _gameVersion;
@@ -46,7 +46,7 @@ public sealed class TRXUpdateService : IEngineUpdateService
 	{
 		if (currentVersion < MinAutoUpdateVersion)
 		{
-			blockReason = "Cannot Auto-Update engine. TRX 1.3 introduced breaking changes, which require manual migration.";
+			blockReason = "Cannot Auto-Update engine. TRX 1.9 introduced breaking changes, which require manual migration.";
 			return false;
 		}
 
@@ -67,7 +67,7 @@ public sealed class TRXUpdateService : IEngineUpdateService
 		DialogResult result = MessageBox.Show(owner,
 			"This update will replace the following directories and files:\n\n" +
 
-			"- Engine/shaders/\n" +
+			"- Engine/cfg/shaders/\n" +
 			"- Engine/TRX.exe\n\n" +
 
 			"If any of these directories / files are important to you, please update the engine manually or create a copy of these files before performing this update.\n\n" +
@@ -95,7 +95,7 @@ public sealed class TRXUpdateService : IEngineUpdateService
 			string enginePresetPath = Path.Combine(DefaultPaths.PresetsDirectory, presetArchiveName);
 			using var engineArchive = new ZipArchive(File.OpenRead(enginePresetPath));
 
-			var shaders = engineArchive.Entries.Where(entry => entry.FullName.StartsWith("Engine/shaders")).ToList();
+			var shaders = engineArchive.Entries.Where(entry => entry.FullName.StartsWith("Engine/cfg/shaders")).ToList();
 			_fileExtractionService.ExtractEntries(shaders, project.DirectoryPath);
 
 			var executables = engineArchive.Entries.Where(entry => entry.FullName.EndsWith(".exe")).ToList();

commit 8ebc49244511c4d2dfd6c37bc6f063d2e535164d

Fix TRX sound map
This fixes trying to call Max on an empty sound info list.
Unreleased regression in b9de93159.

TombLib/TombLib/LevelData/Compilers/Wad.cs
@@ -821,7 +821,7 @@ namespace TombLib.LevelData.Compilers
             if (_level.IsNG)
                 _soundMapSize = _limits[Limit.NG_SoundMapSize];
             else if (_level.IsTRX)
-                _soundMapSize = Math.Max(_finalSoundInfosList.Max(s => s.Id) + 1, _limits[Limit.SoundMapSize]);
+                _soundMapSize = Math.Max(_finalSoundInfosList.Select(s => s.Id).DefaultIfEmpty(-1).Max() + 1, _limits[Limit.SoundMapSize]);
             else
                 _soundMapSize = _limits[Limit.SoundMapSize];
 