1555ae08800a7300578a2a33ef56a0587e5337dd
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2026-08-16 23:04:52 +0100

    Add fog bulb nodes and point light to volumes

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Dynamic Lights.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Dynamic Lights.lua
@@ -37,6 +37,24 @@ LevelFuncs.Engine.Node.StaticLight = function(static, lightcolor, range, effectO
 	TEN.Effects.EmitLight(offset, lightcolor, range, shadow, name)
 end
 
+-- !Name "Add point light to volume"
+-- !Section "Dynamic lights"
+-- !Description "Add a point light to a volume."
+-- !Arguments "NewLine, Volumes, 100, Select volume to attach light to."
+-- !Arguments "NewLine, Color, 20, { TEN.Color(128,128,128) }, Light color." 
+-- !Arguments "Numerical, 20, [ 0 | 100 | 0 ], { 20 }, (Optional) Light radius (in clicks of 256 world units)."
+-- !Arguments "Vector3 , 60, [ -1000000 | 1000000 |  | 1 | 32 ], { TEN.Vec3(.1,.1,.1) }, Offset (relative)"
+-- !Arguments "NewLine, Boolean, 50, Casts Dynamic Shadow"
+-- !Arguments "String, 50, [ NoMultiline ], (Optional) A unique name for the light.\nTo enable interpolation in high framerate mode the light must have a unique name.\nIf the source moveable does not move significantly this field is not required." 
+
+LevelFuncs.Engine.Node.VolumeLight = function(volume, lightcolor, range, effectOffset, shadow, name)
+
+	local entityPos = TEN.Objects.GetVolumeByName(volume):GetPosition()
+	effectOffset = effectOffset or Vec3(0, 0, 0)
+	local offset = (entityPos) + (effectOffset)
+	TEN.Effects.EmitLight(offset, lightcolor, range, shadow, name)
+end
+
 -- !Name "Add spotlight to a moveable"
 -- !Section "Dynamic lights"
 -- !Description "Adds a spotlight to a moveable."
@@ -56,6 +74,58 @@ LevelFuncs.Engine.Node.MoveableSpotLight = function(moveable, meshnumber, name, 
 	local offset = entityPos + (effectOffset or Vec3(0, 0, 0))
 	local direction = TEN.Objects.GetMoveableByName(moveable):GetJointRotation(meshnumber):Direction()
 
-	EmitSpotLight(offset, direction, color, radius, falloff, distance, shadow, name)
+	TEN.Effects.EmitSpotLight(offset, direction, color, radius, falloff, distance, shadow, name)
+end
 
+-- !Name "Add fog bulb to moveable"
+-- !Section "Dynamic lights"
+-- !Description "Add a fog bulb to a moveable."
+-- !Arguments "NewLine, Moveables, 80, Select moveable to attach effect to."
+-- !Arguments "Numerical, 20, [ 0 | 100 | 0 ] , Select mesh number of moveable. \nThis can be found in the Animation Editor within Wadtool."
+-- !Arguments "NewLine, Color, 50, { TEN.Color(128,128,128) }, Light color." 
+-- !Arguments "Numerical, 25, [ 0 | 100 | 0 ], { 20 }, (Optional) Light radius (in clicks of 256 world units)."
+-- !Arguments "Numerical, 25, [ 0 | 255 | 0 ], { 128 }, (Optional) Effect density."
+-- !Arguments "NewLine, Vector3 , 50, [ -1000000 | 1000000 |  | 1 | 32 ], { TEN.Vec3(.1,.1,.1) }, Offset (relative)"
+-- !Arguments "String, 50, [ NoMultiline ], (Optional) A unique name for the light.\nTo enable interpolation in high framerate mode the light must have a unique name.\nIf the source moveable does not move significantly this field is not required." 
+
+LevelFuncs.Engine.Node.MoveableFogBulb = function(moveable, meshnumber, lightcolor, radius, density, effectOffset, name)
+
+	local entityPos = TEN.Objects.GetMoveableByName(moveable):GetJointPosition(meshnumber)
+	effectOffset = effectOffset or Vec3(0, 0, 0)
+	local offset = (entityPos) + (effectOffset)
+	TEN.Effects.EmitFogBulb(offset, radius, density, lightcolor, name)
+end
+
+-- !Name "Add fog bulb to static"
+-- !Section "Dynamic lights"
+-- !Description "Add a fog bulb light to a static object."
+-- !Arguments "NewLine, Statics, 100, Select static to attach effect to."
+-- !Arguments "NewLine, Color, 50, { TEN.Color(128,128,128) }, Light color." 
+-- !Arguments "Numerical, 25, [ 0 | 100 | 0 ], { 20 }, (Optional) Light radius (in clicks of 256 world units)."
+-- !Arguments "Numerical, 25, [ 0 | 255 | 0 ], { 128 }, (Optional) Effect density."
+-- !Arguments "NewLine, Vector3 , 50, [ -1000000 | 1000000 |  | 1 | 32 ], { TEN.Vec3(.1,.1,.1) }, Offset (relative)"
+-- !Arguments "String, 50, [ NoMultiline ], (Optional) A unique name for the light.\nTo enable interpolation in high framerate mode the light must have a unique name.\nIf the source moveable does not move significantly this field is not required." 
+
+LevelFuncs.Engine.Node.StaticFogBulb = function(static, lightcolor, radius, density, effectOffset, name)
+
+	local entityPos = TEN.Objects.GetStaticByName(static):GetPosition()
+	local offset = (entityPos) + (effectOffset)
+	TEN.Effects.EmitFogBulb(offset, radius, density, lightcolor, name)
+end
+
+-- !Name "Add fog bulb to volume"
+-- !Section "Dynamic lights"
+-- !Description "Add a fog bulb light to volume."
+-- !Arguments "NewLine, Volumes, 100, Select volume to attach effect to."
+-- !Arguments "NewLine, Color, 50, { TEN.Color(128,128,128) }, Light color." 
+-- !Arguments "Numerical, 25, [ 0 | 100 | 0 ], { 20 }, (Optional) Light radius (in clicks of 256 world units)."
+-- !Arguments "Numerical, 25, [ 0 | 255 | 0 ], { 128 }, (Optional) Effect density."
+-- !Arguments "NewLine, Vector3 , 50, [ -1000000 | 1000000 |  | 1 | 32 ], { TEN.Vec3(.1,.1,.1) }, Offset (relative)"
+-- !Arguments "String, 50, [ NoMultiline ], (Optional) A unique name for the light.\nTo enable interpolation in high framerate mode the light must have a unique name.\nIf the source moveable does not move significantly this field is not required." 
+
+LevelFuncs.Engine.Node.VolumeFogBulb = function(volume, lightcolor, radius, density, effectOffset, name)
+
+	local entityPos = TEN.Objects.GetVolumeByName(volume):GetPosition()
+	local offset = (entityPos) + (effectOffset)
+	TEN.Effects.EmitFogBulb(offset, radius, density, lightcolor, name)
 end
