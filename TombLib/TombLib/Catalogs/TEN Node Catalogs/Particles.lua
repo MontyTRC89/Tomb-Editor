58402f68f3bbfcb31526096c909ce80fa9d733a0
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2026-08-18 04:14:56 +0100

    Use TEN's PrintLog function for error message.

--- TombLib/TombLib/Catalogs/TEN Node Catalogs/Particles.lua
+++ TombLib/TombLib/Catalogs/TEN Node Catalogs/Particles.lua
@@ -140,7 +140,7 @@ LevelFuncs.Engine.Node.EmitAirBubbleMoveable = function(mov, size, osc)
 	local origin = moveable:GetPosition()
 
 	if (moveableRoom:GetFlag(TEN.Objects.RoomFlagID.WATER) == false) then
-		print("Moveable '" .. mov .. "' must be placed underwater to emit air bubbles.")
+		PrintLog("Moveable '" .. mov .. "' must be placed underwater to emit air bubbles.", TEN.Util.LogLevel.WARNING, false)
 		return
 	end
 
